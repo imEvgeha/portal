65b3a67ec8e6f43b91ccbf0c2a8a432f6666c8ed
Oleh Tsyupa <oleh.tsyupa@vubiquity.com>
2018-10-22 13:07:18 +0200

    Change base url for upload files

--- src/containers/dashboard/TmpUploadService.js
+++ src/containers/dashboard/TmpUploadService.js
@@ -1,11 +1,11 @@
 import Http from '../../util/Http';
 
-const http = Http.create({baseURL: 'http://usla-amm-d001.dev.vubiquity.com:8082'});
+const http = Http.create({baseURL: 'http://usla-amm-d001.dev.vubiquity.com:8082/avails-api/v1'});
 
 export const tmpUploadService = {
     uploadAvail: (file) => {
         const formData = new FormData();
         formData.append('avail', file);
-        return http.post('/avails', formData,  {headers: {'Content-Type': 'multipart/form-data'}});
+        return http.post('/avails/upload', formData,  {headers: {'Content-Type': 'multipart/form-data'}});
     },
 };
