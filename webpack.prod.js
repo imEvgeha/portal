cb30171ec30e58e5e6831f96c89bfe70350a543a
Saeed Jalali <sjalali@vubiquity.com>
2018-11-06 10:20:52 -0800

    add rule per Krz.

--- webpack.prod.js
+++ webpack.prod.js
@@ -25,6 +25,18 @@ module.exports = {
             {
                 test: /\.scss/,
                 loaders: ['style-loader', 'css-loader', 'sass-loader']
+            }, {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [
+                    'file-loader',
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            bypassOnDebug: true, // webpack@1.x
+                            disable: true, // webpack@2.x and newer
+                        },
+                    },
+                ],
             }
         ],
     },
