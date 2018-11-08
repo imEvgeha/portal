5d0a40c7f54b07e6ad2765501fba82cb8375f5fa
Hakan Genc <HakanGenc@pl.micazook.com>
2018-11-08 12:01:25 +0100

    Editable datepicker

---
+++ src/components/fields/EditableDatePicker.js
@@ -0,0 +1,136 @@
+import React, { Component } from 'react';
+import DatePicker from 'react-datepicker';
+import { Button } from 'reactstrap';
+import FontAwesome from 'react-fontawesome';
+import moment from 'moment';
+import t from 'prop-types';
+
+class EditableDatePicker extends Component {
+
+    static propTypes = {
+        validate: t.func,
+        value: t.string,
+        displayName: t.string,
+        name: t.string,
+        onChange: t.func,
+    };
+
+    constructor(props) {
+        let vodDate = props.value;
+        console.log(vodDate + ' ' + ( vodDate ? vodDate : moment()) + ' ' + (  moment(vodDate).isValid() ? moment(vodDate) : moment()));
+        super(props);
+        this.state = {
+            date: vodDate ? vodDate : moment(),
+            datePickerStatus: false,
+            errorMessage: '',
+            submitStatus: false,
+            prevDate: vodDate
+        };
+
+        this.handleShowDatePicker = this.handleShowDatePicker.bind(this);
+        this.handleChangeDate = this.handleChangeDate.bind(this);
+        this.handleCancelDatePicker = this.handleCancelDatePicker.bind(this);
+        this.handleDatepickerRawChange = this.handleDatepickerRawChange.bind(this);
+        this.submit = this.submit.bind(this);
+    } 
+
+    handleShowDatePicker(e) {
+        e.preventDefault();
+        this.setState({
+            datePickerStatus: true,
+            date: moment(this.state.date).isValid() ? moment(this.state.date) : moment(),
+        });
+    }
+    handleCancelDatePicker(e) {
+        e.preventDefault();
+        let date = moment(this.props.value);
+        let newPrevDate = date;
+        this.setState({
+            date: newPrevDate,
+            datePickerStatus: false,
+        });
+    }
+
+    handleChangeDate(date) {
+        this.setState({ date: date, submitStatus: false, errorMessage: '' });
+    }
+
+    handleDatepickerRawChange(date) {
+        if (moment(date).isValid() || !date) {
+            this.handleChangeDate(date);
+        } else {
+            this.setState({ errorMessage: 'Invalid date: ' + this.props.displayName, submitStatus: true });
+        }
+    }
+
+    submit(date) {
+        let validationError = this.props.validate(date);
+        this.setState({
+            errorMessage: validationError
+        });
+        if (validationError === undefined) {
+            this.props.onChange(date);
+            this.setState({
+                datePickerStatus: false
+            });
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                {
+                    this.state.datePickerStatus ?
+                        <div>
+                            <div className="dPicker">
+                                <DatePicker
+                                    className={this.state.errorMessage ? 'text-danger' : ''}
+                                    placeholderText="Enter date" 
+                                    selected={this.state.date}
+                                    onChangeRaw={(event) => this.handleDatepickerRawChange(event.target.value)}
+                                    onChange={this.handleChangeDate}
+                                    todayButton={'Today'} />
+                            </div>
+                            <div style={{ float: 'left' }}>
+                                <Button
+                                    className="dPButton"
+                                    disabled={this.state.submitStatus}
+                                    onClick={() => this.submit(this.state.date)}
+                                    color="success"><FontAwesome name='check' />
+                                </Button>
+                                <Button
+                                    className="dPButton"
+                                    onClick={this.handleCancelDatePicker}
+                                    color="danger"><FontAwesome name='times' />
+                                </Button>
+                            </div>
+                            {
+                                this.state.errorMessage &&
+                                <p style={{ color: 'red' }}>
+                                    <br />
+                                    <br />
+                                    {this.state.errorMessage}
+                                </p>
+                            }
+                        </div>
+                        :
+                        this.props.value ?
+                            <span 
+                                onClick={this.handleShowDatePicker} 
+                                className="displayDate">
+                                    {moment(this.state.date).format('L')}
+                            </span>
+                            :
+                            <span 
+                                style={{ color: '#808080', cursor: 'pointer' }} 
+                                onClick={this.handleShowDatePicker}> 
+                                    {'Enter ' + this.props.displayName}
+                                </span>
+                }
+
+            </div>
+        );
+    }
+}
+
+export default EditableDatePicker;
