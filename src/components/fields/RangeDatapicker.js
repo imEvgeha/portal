ed94b955bb525e4896eca35b7e8720df18688713
krzysztof <krzysztof.sikora@vubiquity.co.uk>
2018-11-15 13:40:05 +0100

    PORT-111 small fix

--- src/components/fields/RangeDatapicker.js
+++ src/components/fields/RangeDatapicker.js
@@ -116,6 +116,7 @@ export default class RangeDatapicker extends React.Component {
             }
         } else {
             this.setState({invalidRange: '', invalidEndDate: ''});
+            this.props.onValidate('');
         }
     }
 
