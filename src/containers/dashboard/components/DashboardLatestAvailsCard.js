45d9a8b2bd39de35b49b7ede6d137715dbe2e701
Adrian Buse <adrian.buse2@vubiquity.co.uk>
2019-01-14 15:55:52 +0100

    PORT-180 style improvements

--- src/containers/dashboard/components/DashboardLatestAvailsCard.js
+++ src/containers/dashboard/components/DashboardLatestAvailsCard.js
@@ -7,36 +7,76 @@ import {advancedHistorySearchHelper} from '../../avail-ingest-history/AdvancedHi
 
 import './DashboardLatestAvailsCard.scss';
 
+import LoadingElipsis from '../../../img/ajax-loader.gif';
+
 export default class DashboardLatestAvailsCard extends React.Component {
 
     table = null;
 
     constructor(props) {
         super(props);
+
+        this.getData = this.getData.bind(this);
+
         this.state = {
             pageSize: 6,
             cols:[{headerName: 'Date', field: 'received', valueFormatter: function(params) {
-                          if(params.data && params.data.received) return moment(params.data.received).format('L');
+                          if(params.data && params.data.received) return moment(params.data.received).format('L') + ' ' + moment(params.data.received).format('HH:mm');
                           else return '';
-                      }, width:150},
-                    {headerName: 'Provider', field: 'provider', width:120},
-                    {headerName: 'Status', field: 'state', width:70},
-                    {headerName: 'Ingest Method', field: 'ingestType', width:160},
-                    {headerName: 'Filename', valueFormatter: this.showFileNames, width:50}
+                      }, width:120},
+                    {headerName: 'Provider', field: 'provider', width:90},
+                    {headerName: 'Status', field: 'status', cellRendererFramework: this.statusIconRender, width:55},
+                    {headerName: 'Ingest Method', field: 'ingestType', width:105},
+                    {headerName: 'Filename', cellRenderer: this.showFileNames, width:180}
             ]
         };
+
         this.getData();
     }
 
+    statusIconRender(params){
+        console.log('statusIconRender', params)
+        let content = params.valueFormatted || params.value;
+        if (params.value !== undefined) {
+            if (content) {
+                switch (content) {
+                     case 'COMPLETED':
+                        return <span style={{ color: 'green'}}><i className="fas fa-check-circle"></i></span>;
+                     case 'FAILED':
+                        return <span title={params.data.errorDetails} style={{ color: 'red'}}><i className="fas fa-exclamation-circle"></i></span>;
+                     case 'PENDING':
+                        return <img src={LoadingElipsis}/>;
+                     default:
+                        return content;
+                 }
+            }
+            else return '';
+        } else {
+            return '';
+        }
+    }
+
     showFileNames(params){
-        console.log(params);
-        return "AAAAAAAAA";
+        console.log(params.data.attachments);
+        let toReturn='';
+        params.data.attachments.forEach( attachment => {
+            let filename = attachment.link.split(/(\\|\/)/g).pop();
+            switch (attachment.type) {
+                case 'Excel':
+                    toReturn += filename + ', ';
+                    break;
+            }
+        });
+        toReturn = toReturn.slice(0, -2);
+        return toReturn;
+        //return '<div title=' + toReturn + '>' + toReturn + '</div>';
     }
 
     getData() {
+        //setTimeout(this.getData, 5 * 1000);
         historyService.advancedSearch(advancedHistorySearchHelper.prepareAdvancedHistorySearchCall({}), 0, this.state.pageSize, [{id: 'received', desc:true}])
                 .then(response => {
-                    console.log(response);
+                    //console.log(response);
                     this.table.api.setRowData(response.data.data);
                     this.table.api.hideOverlay();
                 }).catch((error) => {
@@ -54,17 +94,30 @@ export default class DashboardLatestAvailsCard extends React.Component {
 
     render() {
         return (
-            <div className="dashboard-card-container no-padding" style={{width:"555px"}}>
+            <div className="dashboard-card-container no-padding" style={{width:'555px', height:'200px'}}>
+                <div className="dashboard-card-title">
+                    Most Recent Avails
+                    <span style={{float:'right', textDecoration: 'underline', paddingRight: '5px'}}>
+                        <a href='/avail-ingest-history'>View All</a>
+                    </span>
+                </div>
                 <div className="ag-theme-balham"
                     style={{
-                        height: '100%',
+                        height: 'calc(100% - 26px)',
                         width: '100%' }}
                     >
                     <AgGridReact
                         ref={this.setTable}
                         columnDefs= {this.state.cols}
                         headerHeight= '30'
+                        rowHeight= '23'
+                        suppressDragLeaveHidesColumns= {true}
+                        enableColResize= {false}
+                        enableSorting= {false}
                         suppressHorizontalScroll= {true}
+                        suppressMovableColumns = {true}
+                        suppressRowClickSelection = {true}
+                        suppressCellSelection = {true}
                     />
                 </div>
              </div>
