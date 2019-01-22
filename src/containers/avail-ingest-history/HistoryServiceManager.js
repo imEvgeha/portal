3b013ba858093897e8847424ddef19496fc07ba9
Adrian Buse <adrian.buse2@vubiquity.co.uk>
2019-01-22 13:02:36 +0100

    PORT201 - added some comments to describe functions in the new Service Managers

--- src/containers/avail-ingest-history/HistoryServiceManager.js
+++ src/containers/avail-ingest-history/HistoryServiceManager.js
@@ -3,15 +3,19 @@ import {resultHistoryPageLoading, resultPageHistoryUpdate, searchFormSetHistoryS
 import {historyService} from './HistoryService';
 
 export const historyServiceManager = {
+    //called by other systems, saves search criteria and updates data in redux which acts as a trigger for other elements
+    //for now just table listens for that
     search: (searchCriteria) => {
         store.dispatch(searchFormSetHistorySearchCriteria(searchCriteria));
         store.dispatch(resultHistoryPageLoading(true));
     },
 
+    //called by the table either as result of other systems triggering the table (page 0) or scrolling the table (page > 0)
     doSearch: (page, pageSize, sortedParams) => {
         return historyServiceManager.callService(historyService.advancedSearch, page, pageSize, sortedParams);
     },
 
+    //the actual call to the service (and further to server), generic interpretation of result and forward for detailed interpretation (by the table)
     callService: (searchFn, page, pageSize, sortedParams) => {
         return searchFn(store.getState().history.session.searchCriteria, page, pageSize, sortedParams)
             .then(response => {
