ed2bda96351205f68c584b2c1cc781e5afc7cae5
Adrian Buse <adrian.buse2@vubiquity.co.uk>
2019-03-05 11:23:49 +0100

    AVAILAPI-353 change how checboxes interact, added select all option

--- src/containers/avail/dashboard/SearchResultsTab.js
+++ src/containers/avail/dashboard/SearchResultsTab.js
@@ -50,40 +50,101 @@ class SearchResultsTab extends React.Component {
         };
         this.requestFile = this.requestFile.bind(this);
         this.toggleColumn = this.toggleColumn.bind(this);
+        this.toggleSelectAll = this.toggleSelectAll.bind(this);
         this.selectColumns = this.selectColumns.bind(this);
         this.saveColumns = this.saveColumns.bind(this);
         this.cancelColumns = this.cancelColumns.bind(this);
-    }
+        this.selectColumns = this.selectColumns.bind(this);
 
-    selectColumnsContentProvider() {
-        return this.props.availsMapping.mappings.map(column => {
-                if(column.javaVariableName=='title') return '';
-                let checked = this.props.columnsOrder.indexOf(column.javaVariableName) > -1 ? true : false;
-                return <div key={column.javaVariableName}><input type='checkbox' name={column.javaVariableName} style={{marginRight: '8px'}} onClick={this.toggleColumn} defaultChecked={checked} />{column.displayName}<br/></div>;
-            }
-        );
+
     }
 
     selectColumns() {
+        this.props.availsMapping.mappings.forEach(column => {
+            if (column.javaVariableName === 'title') return '';
+            let checked = this.props.columnsOrder.indexOf(column.javaVariableName) > -1;
+            const data = {
+                source: column,
+                hideShowColumns: this.hideShowColumns,
+                onChange: () => {
+                    this.toggleColumn(column.javaVariableName);
+                },
+                saveRefresh: (refresh) =>{
+                    this.hideShowColumns[column.javaVariableName].refresh = refresh;
+                }
+            };
+
+            this.hideShowColumns[column.javaVariableName] = {
+                data: data,
+                checked: () => checked,
+                refresh: () => {},
+                checkbox: <SpecialCheckbox key={column.javaVariableName} data={data}/>
+            };
+        });
+
+        const dataSelectAll={
+            source:{
+                javaVariableName: 'selectAll',
+                displayName:'Select All'
+            },
+            hideShowColumns: this.hideShowColumns,
+            onChange: (e) => {
+                this.toggleSelectAll(e);
+            },
+            saveRefresh: (refresh) =>{
+                this.hideShowColumns[dataSelectAll.source.javaVariableName].refresh = refresh;
+            }
+        };
+
+        this.hideShowColumns[dataSelectAll.source.javaVariableName] = {
+            data: dataSelectAll,
+            checked: () => {
+                let allSelected = true;
+                let hideShowColumns = this.hideShowColumns;
+                for (let key in hideShowColumns) {
+                    if (key === dataSelectAll.source.javaVariableName) continue;
+                    allSelected = allSelected && hideShowColumns[key].checked();
+                }
+                return allSelected;
+            },
+            refresh: () => {},
+            checkbox: <SpecialCheckbox key={dataSelectAll.source.javaVariableName} data={dataSelectAll}/>
+        };
+
+        const options = [this.hideShowColumns['selectAll'].checkbox];
+        for (let key in this.hideShowColumns) {
+            if(key === 'selectAll') continue;
+            options.push(this.hideShowColumns[key].checkbox);
+        }
+
         confirmModal.open('Select Visible Columns',
             this.saveColumns,
             this.cancelColumns,
-            {confirmLabel:'OK',  description: this.selectColumnsContentProvider()});
+            {confirmLabel: 'OK', description: options}
+        );
     }
 
-    toggleColumn(e){
-        if(this.hideShowColumns.hasOwnProperty(e.target.name) && this.hideShowColumns[e.target.name] != e.target.checked){
-            delete this.hideShowColumns[e.target.name];
-        }else{
-            this.hideShowColumns[e.target.name]=e.target.checked;
-        }
+    toggleSelectAll(e){
+        let currentValue = e.target.checked;
+        this.props.availsMapping.mappings.forEach(column => {
+            if(column.javaVariableName === 'title') return '';
+            this.hideShowColumns[column.javaVariableName].checked = () => currentValue;
+            this.hideShowColumns[column.javaVariableName].refresh();
+        });
+    }
+
+    toggleColumn(id){
+        const checkRec = this.hideShowColumns[id];
+        const currentValue = checkRec.checked();
+        checkRec.checked = () => !currentValue;
+        this.hideShowColumns['selectAll'].refresh();
     }
 
     saveColumns() {
         let cols = this.props.columnsOrder.slice();
         //remove all hidden columns
         Object.keys(this.hideShowColumns).map(key => {
-            if(this.hideShowColumns[key]===false){
+            if(this.hideShowColumns[key].checked() === false){
                 let position = cols.indexOf(key);
                 if(position>-1){
                     cols.splice(position, 1);
@@ -92,7 +153,7 @@ class SearchResultsTab extends React.Component {
         });
         //add new visible columns
         Object.keys(this.hideShowColumns).map(key => {
-            if(this.hideShowColumns[key]===true){
+            if(this.hideShowColumns[key].checked() === true){
                 let position = cols.indexOf(key);
                 if(position===-1){
                     cols.push(key);
@@ -182,3 +243,41 @@ class SearchResultsTab extends React.Component {
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(SearchResultsTab);
+
+import {Component} from 'react';
+
+class SpecialCheckbox extends Component {
+
+    static propTypes = {
+        data: t.object
+    };
+
+    constructor(props) {
+        super(props);
+        this.onChange = this.onChange.bind(this);
+        this.refresh = this.refresh.bind(this);
+        this.props.data.saveRefresh(this.refresh);
+    }
+
+    refresh(){
+        this.setState({});
+    }
+
+    onChange(e){
+        if(this.props.data.onChange){
+            this.props.data.onChange(e);
+        }
+        this.refresh();
+    }
+
+    render(){
+        const def = this.props.data.source;
+        const id= def.javaVariableName;
+        const dataRec = this.props.data.hideShowColumns[id];
+        return(
+            <div>
+                <input type='checkbox' name={id} style={{marginRight: '8px'}} onChange={this.onChange} checked={dataRec.checked()} />{def.displayName}<br/>
+            </div>
+        );
+    }
+}
