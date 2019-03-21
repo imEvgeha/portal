924088ca780908d768f29a49a15a5223e744f614
Adrian Buse <adrian.buse2@vubiquity.co.uk>
2019-03-21 13:19:11 +0100

    PORT-179 convert avail to server format

--- src/containers/avail/service/AvailService.js
+++ src/containers/avail/service/AvailService.js
@@ -1,19 +1,49 @@
 import Http from '../../../util/Http';
 import config from 'react-global-configuration';
-import moment from 'moment';
-import {momentToISO, prepareSortMatrixParam} from '../../../util/Common';
+import {prepareSortMatrixParam, safeTrim} from '../../../util/Common';
 
 const http = Http.create();
 
-const prepareAvail = function (avail) {
-
-    let availCopy = Object.assign({}, avail);
-    for(let key in availCopy) {
-        if(availCopy.hasOwnProperty(key) && availCopy[key] instanceof moment) {
-            availCopy[key] = momentToISO(availCopy[key]);
-        }
+const isNotEmpty = function(obj){
+    if(Array.isArray(obj)){
+        return obj.length > 0;
     }
+    return obj && safeTrim(obj);
+}
 
+const parse = function(value){
+    if(typeof  value === 'string')
+        return safeTrim(value);
+
+    if(Array.isArray(value))
+        return value.map(val => parse(val)).join(',');
+
+    if ('value' in value)
+        return parse(value.value);
+
+    return null;
+}
+
+const populate = function(key, value, location){
+    const dotPos = key.indexOf('.');
+    if(dotPos > 0) {
+        const firstKey = key.split('.')[0]
+        const restKey = key.substring(dotPos+1);
+        if(!location[firstKey])
+            location[firstKey]={};
+        populate(restKey, value, location[firstKey]);
+    }else{
+        location[key] = parse(value);
+    }
+}
+
+const prepareAvail = function (avail) {
+    let availCopy = {};
+    Object.keys(avail).forEach(key => {
+        if(isNotEmpty(avail[key])){
+            populate(key, avail[key], availCopy);
+        }
+    });
     return availCopy;
 };
 
