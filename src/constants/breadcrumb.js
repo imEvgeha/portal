d099ed5bce0e3019cb7382bd25778e0ea7304589
Adrian Buse <adrian.buse2@vubiquity.co.uk>
2019-03-21 13:42:38 +0100

    PORT-179 update layout - 1 column

--- src/constants/breadcrumb.js
+++ src/constants/breadcrumb.js
@@ -3,4 +3,4 @@ export const AVAILS_DASHBOARD = {name: 'Avails Dashboard', path: '/avails', onCl
 export const AVAILS_SEARCH_RESULTS = {name: 'Avails > Search Results', path: '/avails'};
 export const AVAILS_HISTORY = {name: 'Avails Ingest History', path: '/avails/history'};
 export const AVAILS_HISTORY_SEARCH_RESULTS = {name: 'Avails Ingest History > Search Results', path: '/avails/history'};
-export const AVAILS_CREATE = {name: 'Create Avail', path: '/avails/create'};
+export const AVAILS_CREATE = {name: 'Create Right', path: '/avails/create'};
