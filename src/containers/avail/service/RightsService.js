02e0de71d83e62d2548217c5f6e5926f5cb8f0f5
Adrian Buse <adrian.buse2@vubiquity.co.uk>
2019-04-02 13:30:40 +0200

    PORT-332 added create languages hack

--- src/containers/avail/service/RightsService.js
+++ src/containers/avail/service/RightsService.js
@@ -35,9 +35,26 @@ const populate = function(key, value, location){
     if(dotPos > 0) {
         const firstKey = key.split('.')[0];
         const restKey = key.substring(dotPos+1);
-        if(!location[firstKey])
-            location[firstKey]={};
-        populate(restKey, value, location[firstKey]);
+        if(firstKey === 'languages') {
+            if (!location[firstKey])
+                location[firstKey] = [];
+            const container = location[firstKey];
+            value=parse(value);
+            if(typeof  value === 'string') {
+                value = parse(value.split(','));
+            }
+            for(let i = 0; i < value.length; i++){
+                if(container.length <= i){
+                    container.push({[restKey]: value[i]});
+                }else{
+                    container[i][restKey] = value[i];
+                }
+            }
+        }else {
+            if (!location[firstKey])
+                location[firstKey] = {};
+            populate(restKey, value, location[firstKey]);
+        }
     }else{
         location[key] = parse(value);
     }
