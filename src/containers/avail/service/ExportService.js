8a69985c226ba8c94e9090313fad4b10eeb787e7
Adrian Buse <adrian.buse2@vubiquity.co.uk>
2019-05-09 12:56:43 +0200

    AVAILAPI-830 ask for languages as a single object when export

--- src/containers/avail/service/ExportService.js
+++ src/containers/avail/service/ExportService.js
@@ -3,10 +3,34 @@ import config from 'react-global-configuration';
 
 const http = Http.create();
 
+const languagehack  = (cols) => {
+    const columns = [...cols];
+
+    const index1 = columns.indexOf('languages.language');
+    const index2 = columns.indexOf('languages.audioType');
+
+    const indexMin = Math.min(index1, index2);
+    const indexMax = Math.max(index1, index2);
+
+    if(indexMax > -1){
+        //if at least one found
+        if(indexMin > -1){
+            //if both found
+            columns.splice(indexMin, 1, 'languages'); //replace first
+            columns.splice(indexMax, 1); //and delete second
+        }else{
+            //if only one found
+            columns.splice(indexMax, 1, 'languages'); //replace it
+        }
+    }
+
+    return columns;
+}
+
 export const exportService = {
     exportAvails: (rightsIDs, columns) => {
         http.defaults.timeout = config.get('avails.export.http.timeout');
-        return http.post(config.get('gateway.url') + config.get('gateway.service.avails') +'/avails/export', {columnNames: columns, rightIds: rightsIDs}, {responseType: 'arraybuffer'});
+        return http.post(config.get('gateway.url') + config.get('gateway.service.avails') +'/avails/export', {columnNames: languagehack(columns), rightIds: rightsIDs}, {responseType: 'arraybuffer'});
     },
 
     bulkExportAvails: (searchCriteria, columns) => {
@@ -17,6 +41,6 @@ export const exportService = {
             }
         }
         http.defaults.timeout = config.get('avails.export.http.timeout');
-        return http.post(config.get('gateway.url') + config.get('gateway.service.avails') +'/avails/export/bulk', {columnNames: columns}, {responseType: 'arraybuffer', params: {...params, page: 0, size: 1}});
+        return http.post(config.get('gateway.url') + config.get('gateway.service.avails') +'/avails/export/bulk', {columnNames: languagehack(columns)}, {responseType: 'arraybuffer', params: {...params, page: 0, size: 1}});
     }
 };
