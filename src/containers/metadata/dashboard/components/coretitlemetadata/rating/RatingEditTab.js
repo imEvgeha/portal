c8accd49b555e105644e1c69dfa54023136f8829
oleht <oleh.tsyupa@gmail.com>
2019-05-17 15:39:38 +0200

    Add edit/read mode

---
+++ src/containers/metadata/dashboard/components/coretitlemetadata/rating/RatingEditTab.js
@@ -0,0 +1,112 @@
+import React, { Component, Fragment } from 'react';
+import { Row, Col } from 'reactstrap';
+import { AvField } from 'availity-reactstrap-validation';
+import Select from 'react-select';
+import PropTypes from 'prop-types';
+import {configFields} from '../../../../service/ConfigService';
+import {connect} from 'react-redux';
+
+const mapStateToProps = state => {
+    return {
+        configRatingSystem: state.titleReducer.configData.find(e => e.key === configFields.RATING_SYSTEM),
+        configRatings: state.titleReducer.configData.find(e => e.key === configFields.RATINGS),
+        configAdvisoryCode: state.titleReducer.configData.find(e => e.key === configFields.ADVISORY_CODE),
+    };
+};
+
+class RatingEditTab extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    handleAdvisoryCodeChange = () => {};
+
+    render() {
+        const {
+            ratingSystem,
+            rating,
+            // advisoriesFreeText,
+            // advisoriesCode
+        } = this.props.data;
+
+        return (
+            <div id="ratingCreate">
+                <Fragment>
+                    <Row style={{ padding: '15px' }}>
+                        <Col md={2}>
+                            <b>Rating System<span style={{ color: 'red' }}>*</span></b>
+                        </Col>
+                        <Col md={2}>
+                            <AvField type="select"
+                                     name="ratingSystem"
+                                     id="titleRatingSystem"
+                                     onChange={(e) => this.props.handleChange(e, this.props.data)}
+                                     value={ratingSystem}
+                                     errorMessage="Field cannot be empty!">
+                                <option value={''}>Select Rating System</option>
+                                {
+                                    this.props.configRatingSystem && this.props.configRatingSystem.value.map((item, i) => {
+                                        return <option key={i} value={item.value}>{item.value}</option>;
+                                    })
+                                }
+                            </AvField>
+                        </Col>
+                        <Col md={2}>
+                            <b>Ratings</b>
+                        </Col>
+                        <Col md={2}>
+                            <AvField type="select"
+                                     name="ratings"
+                                     id="titleRatings"
+                                     onChange={(e) => this.props.handleChange(e, this.props.data)}
+                                     value={rating}
+                                     errorMessage="Field cannot be empty!">
+                                <option value={''}>Select Rating</option>
+                                {
+                                    this.props.configRatings && this.props.configRatings.value.map((item, i) => {
+                                        return <option key={i} value={item.value}>{item.value}</option>;
+                                    })
+                                }
+                            </AvField>
+                        </Col>
+                        <Col md={2}>
+                            <b>Advisory Codes</b>
+                        </Col>
+                        <Col md={2}>
+                            <Select
+                                onChange={this.handleAdvisoryCodeChange}
+                                options={this.props.configAdvisoryCode}
+                                isMulti
+                                placeholder='Select Advisory Code'
+                            />
+                        </Col>
+                    </Row>
+
+                    <Row style={{ padding: '15px' }}>
+                        <Col md={2}>
+                            <b>Advisories</b>
+                        </Col>
+                        <Col md={2}>
+                            <AvField type="text"
+                                     id="tittleAdvisories"
+                                     name="advisories"
+                                     onChange={(e) => this.props.handleChange(e, this.props.data)}
+                                     value={rating}
+                                     errorMessage="Please enter a valid advisories!" />
+                        </Col>
+                    </Row>
+                </Fragment>
+            </div>
+        );
+    }
+}
+
+RatingEditTab.propTypes = {
+    data: PropTypes.object,
+    handleChange: PropTypes.func.isRequired,
+    configRatingSystem: PropTypes.array,
+    configRatings: PropTypes.array,
+    configAdvisoryCode: PropTypes.array
+};
+
+export default connect(mapStateToProps)(RatingEditTab);
