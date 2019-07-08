31a8b77e030eaf3ac4df868eb5e1885b77bbe1b3
Hakan Genc <gnchakan1@gmail.com>
2019-07-08 11:23:29 +0200

    PORT-513

--- src/containers/metadata/dashboard/components/editorialmetadata/EditorialMetadataCreateTab.js
+++ src/containers/metadata/dashboard/components/editorialmetadata/EditorialMetadataCreateTab.js
@@ -300,7 +300,7 @@ class EditorialMetadataCreateTab extends Component {
                                 cols={20} rows={5}
                                 style={{ resize: 'none' }}
                                 validate={{
-                                    maxLength: { value: 1000, errorMessage: 'Too long Long Synopsis. Max 500 symbols.' }
+                                    maxLength: { value: 1000, errorMessage: 'Too long Long Synopsis. Max 1000 symbols.' }
                                 }} />                                
                             <span style={{float:'right', color: synopsis ? this.handleFieldLength(synopsis.longDescription) === 1000 ? 'red' : '#111' : '#111', fontSize: '13px'}}>{synopsis ? this.handleFieldLength(synopsis.longDescription)  : 0}/1000 char</span>
                         </Col>
