c9747f975328d7eed619d39ab654987e4c3d003d
oleht <oleh.tsyupa@gmail.com>
2019-07-26 16:39:28 +0200

    PORT-593

--- src/containers/config-api/EndpointContainer.js
+++ src/containers/config-api/EndpointContainer.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Pagination from '@atlaskit/pagination';
+import Spinner from '@atlaskit/spinner';
 import {deleteConfigItemById, getConfigApiValues, searchItem} from '../metadata/service/ConfigService';
 import PropTypes from 'prop-types';
 import {TextHeader} from '../../components/navigation/CustomNavigationElements';
@@ -33,7 +34,8 @@ export class EndpointContainer extends Component {
             data: [],
             total: 0,
             lastPage: 0,
-            lastSearchInput: ''
+            lastSearchInput: '',
+            isLoading: false
         };
     }
 
@@ -56,31 +58,31 @@ export class EndpointContainer extends Component {
         }
     }
 
-    loadEndpointData = (page) => {
+    loadEndpointData = (page, searchInput) => {
         let requestFunction;
-        if (this.state.lastSearchInput) {
-            requestFunction = searchItem(this.props.urlBase, this.props.selectedApi.url, 'id', this.state.lastSearchInput, page - 1, pageSize);
+        if (searchInput) {
+            this.setState({
+                lastSearchInput: searchInput
+            });
+            requestFunction = searchItem(this.props.urlBase, this.props.selectedApi.url, 'id', searchInput, page - 1, pageSize);
         } else {
             requestFunction = getConfigApiValues(this.props.urlBase, this.props.selectedApi.url, page - 1, pageSize);
         }
+        this.setState({isLoading: true});
         requestFunction.then((res) => {
             this.setState({
                 pages: Array.from({length: (res.data.total / pageSize < 1 ? 1 : res.data.total / pageSize)}, (v, k) => k + 1),
                 data: res.data.data,
                 total: res.data.total,
-                lastPage: page
+                lastPage: page,
+                isLoading: false
             });
         });
     };
 
-
     handleTitleFreeTextSearch = (searchCriteria) => {
-        this.setState({
-            lastSearchInput: searchCriteria.title
-        });
-        this.loadEndpointData(1);
+        this.loadEndpointData(1, searchCriteria.title);
     };
-
 
     onRemoveItem = (item) => {
         deleteConfigItemById(this.props.urlBase, this.props.selectedApi.url, item.id);
@@ -92,36 +94,39 @@ export class EndpointContainer extends Component {
             <DataContainer>
                 <TextHeader>{this.props.selectedApi.layout['display-name'] + ' {' + this.state.total + '} '}</TextHeader>
                 <DataBody>
-                    <FreeTextSearch ontainerId={'dashboard-title'}
+                    <FreeTextSearch containerId={'setting-api'}
                                     onSearch={this.handleTitleFreeTextSearch}/>
-                    <ListGroup
-                        style={{
-                            overflowY: 'hidden',
-                            overFlowX: 'hidden',
-                            // maxHeight: '60vh'
-                            margin: '10px'
-                        }}
-                        id='listContainer'
-                    >
-                        {this.state.data.map((item, i) => {
-                            return (
-                                <React.Fragment key={i}>
-                                    <ListGroupItem key={i}>
-                                        {item.id}
-                                        <FontAwesome
-                                            className='float-right'
-                                            name='times-circle'
-                                            style={{marginTop: '5px', cursor: 'pointer'}}
-                                            color='#111'
-                                            size='lg'
-                                            onClick={() => this.onRemoveItem(item)}
-                                        />
-                                    </ListGroupItem>
-                                </React.Fragment>
-                            );
-                        })}
-                    </ListGroup>
-                    <Pagination pages={this.state.pages} onChange={(event, newPage) => this.loadEndpointData(newPage)}/>
+                    { this.state.isLoading && <div style={{width: '400px', margin: 'auto'}}><Spinner size="xlarge" /></div> }
+                    { !this.state.isLoading &&
+                        <ListGroup
+                            style={{
+                                overflowY: 'hidden',
+                                overFlowX: 'hidden',
+                                margin: '10px'
+                            }}
+                            id='listContainer'
+                        >
+                            {this.state.data.map((item, i) => {
+                                return (
+                                    <React.Fragment key={i}>
+                                        <ListGroupItem key={i}>
+                                            {item.id}
+                                            <FontAwesome
+                                                className='float-right'
+                                                name='times-circle'
+                                                style={{marginTop: '5px', cursor: 'pointer'}}
+                                                color='#111'
+                                                size='lg'
+                                                onClick={() => this.onRemoveItem(item)}
+                                            />
+                                        </ListGroupItem>
+                                    </React.Fragment>
+                                );
+                            })}
+                        </ListGroup>
+                    }
+                    <div style={{width: '400px', margin: 'auto'}}><Pagination pages={this.state.pages}
+                                     onChange={(event, newPage) => this.loadEndpointData(newPage)}/></div>
                 </DataBody>
             </DataContainer>
         );
