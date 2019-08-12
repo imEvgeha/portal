53f430adfd3e06ac26fecaf1417aac26bc30c124
Adrian Buse <adrian.buse2@vubiquity.co.uk>
2019-08-12 08:02:33 +0200

    PORT-589 load mock data

--- src/containers/config/service/ConfigService.js
+++ src/containers/config/service/ConfigService.js
@@ -9,14 +9,16 @@ export const loadConfigAPIEndPoints = () => {
 
 export const configService = {
     create: (endpoint, rec) => {
-        return http.post(config.get('gateway.configuration') + config.get('gateway.service.configuration') + '/' + endpoint, rec);
+        console.log('configService', rec);
+        //return http.post(config.get('gateway.configuration') + config.get('gateway.service.configuration') + '/' + endpoint, rec);
     },
 
     update: (endpoint, id, rec) => {
-        return http.put(config.get('gateway.configuration') + config.get('gateway.service.configuration') + `/${endpoint}/${id}`, rec);
+        console.log('configService', rec);
+        //return http.put(config.get('gateway.configuration') + config.get('gateway.service.configuration') + `/${endpoint}/${id}`, rec);
     },
 
     delete: (endpoint, id) => {
-        return http.delete(config.get('gateway.configuration') + config.get('gateway.service.configuration') + `/${endpoint}/${id}`);
+        //return http.delete(config.get('gateway.configuration') + config.get('gateway.service.configuration') + `/${endpoint}/${id}`);
     }
 };
