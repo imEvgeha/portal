a5c6588ed60067a67ada030929096e4b28bed3c2
MilosAdzic <milos.adzic@computerrock.com>
2019-08-22 15:18:22 +0200

    PORT-620: temporally switch off logger middleware

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import {createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware, {END} from 'redux-saga';
-import {createLogger} from 'redux-logger';
+// import {createLogger} from 'redux-logger';
 import {composeWithDevTools} from 'redux-devtools-extension';
 import {routerMiddleware} from 'connected-react-router';
 import createRootReducer from './reducer';
@@ -12,11 +12,12 @@ const configureStore = (initialState = {}, history) => {
         routerMiddleware(history),
         sagaMiddleware,
     ];
-    const loggerMiddleware = createLogger({
-        // set webpack config so we can access to environment variables via process.env
-        // predicate: () => process.env.NODE_ENV === 'development',
-    });
-    middleware = [...middleware, loggerMiddleware];
+    // TODO - activate logger middleware when webpack config is set 
+    // so we can access to environment variables via process.env
+    // const loggerMiddleware = createLogger({
+    //     predicate: () => process.env.NODE_ENV === 'development',
+    // });
+    // middleware = [...middleware, loggerMiddleware];
 
     const store = createStore(
         createRootReducer(history),
