ad9797db6d787fc19c7bebfab27d0f7c91025c05
Milos Adzic <milos.adzic.ma@gmail.com>
2019-11-04 09:55:01 +0100

    PORT-1007: add temporal solution to enable drop down on comnined right table

--- src/avails/right-matching/match-rights/MatchRightsView.js
+++ src/avails/right-matching/match-rights/MatchRightsView.js
@@ -132,7 +132,6 @@ function MatchRightView({
                         columnDefs={columnDefs}
                         rowData={[combinedRight]}
                         onGridEvent={handleGridEvent}
-                        domLayout="autoHeight"
                     />
                 )}
             </div>
