ec0f43c7ed31b7f141e15962f6fd55c6c63325dc
Hakan Genc <gnchakan1@gmail.com>
2019-11-06 13:32:49 +0100

    PORT-528 small css changes

--- src/ui-elements/nexus-title/NexusTitle.js
+++ src/ui-elements/nexus-title/NexusTitle.js
@@ -5,7 +5,7 @@ import './NexusTitle.scss';
 const NexusTitle = ({title, isSubTitle, isInline, className, children}) => { // eslint-disable-line
     return (
         <div className={`nexus-c-title ${className}`}>
-            <p className={`nexus-c-title__${isSubTitle ? 'sub-label' : 'label'} nexus-c-title__${isInline ? 'inline-label' : ''}`}>{children}</p>
+            <p className={`nexus-c-title${isSubTitle ? '__sub-label' : '__label'} nexus-c-title${isInline ? '__inline-label' : ''}`}>{children}</p>
             {title && (
                 <div className="nexus-c-title__name">{title}</div> 
             )}
