e5a34dcd64a45c433936f3fbb428fcf1bb18b45c
oleht <oleh.tsyupa@gmail.com>
2019-11-22 13:34:18 +0100

    PORT-1101 temporary remove some of tabs

--- src/containers/avail/create/ManualRightsEntry/components/ManualRightsEntryTableTabs.js
+++ src/containers/avail/create/ManualRightsEntry/components/ManualRightsEntryTableTabs.js
@@ -4,12 +4,8 @@ import connect from 'react-redux/es/connect/connect';
 import {ManualRightEntryTab, TabContainer} from '../../../../../ui-elements/nexus-table-tab/TableTab';
 
 import {
-    CREATED,
-    ERRORS,
     FATAL,
-    PENDING,
-    TOTAL_RIGHTS,
-    UPDATED
+    TOTAL_RIGHTS
 } from '../../../../../constants/avails/manualRightsEntryTabs';
 import {updateManualRightEntrySelectedTab} from '../../../../../stores/actions/avail/manualRightEntry';
 import {rightsService} from '../../../service/RightsService';
@@ -17,22 +13,22 @@ import {rightsService} from '../../../service/RightsService';
 function ManualRightEntryTableTabs({selectedTab, updateManualRightEntrySelectedTab, getCustomSearchCriteria, fatalCount}) {
 
     const [totalRightsCount, setTotalRightsCount] = useState();
-    const [createdCount, setCreatedCount] = useState();
-    const [updatedCount, setUpdatedCount] = useState();
-    const [pendingCount, setPendingCount] = useState();
-    const [errorsCount, setErrorsCount] = useState();
+    // const [createdCount, setCreatedCount] = useState();
+    // const [updatedCount, setUpdatedCount] = useState();
+    // const [pendingCount, setPendingCount] = useState();
+    // const [errorsCount, setErrorsCount] = useState();
 
     useEffect(() => {
         rightsService.advancedSearch(getCustomSearchCriteria(TOTAL_RIGHTS), 0, 1)
             .then(response => setTotalRightsCount(response.data.total));
-        rightsService.advancedSearch(getCustomSearchCriteria(CREATED), 0, 1)
-            .then(response => setCreatedCount(response.data.total));
-        rightsService.advancedSearch(getCustomSearchCriteria(UPDATED), 0, 1)
-            .then(response => setUpdatedCount(response.data.total));
-        rightsService.advancedSearch(getCustomSearchCriteria(PENDING), 0, 1)
-            .then(response => setPendingCount(response.data.total));
-        rightsService.advancedSearch(getCustomSearchCriteria(ERRORS), 0, 1)
-            .then(response => setErrorsCount(response.data.total));
+        // rightsService.advancedSearch(getCustomSearchCriteria(CREATED), 0, 1)
+        //     .then(response => setCreatedCount(response.data.total));
+        // rightsService.advancedSearch(getCustomSearchCriteria(UPDATED), 0, 1)
+        //     .then(response => setUpdatedCount(response.data.total));
+        // rightsService.advancedSearch(getCustomSearchCriteria(PENDING), 0, 1)
+        //     .then(response => setPendingCount(response.data.total));
+        // rightsService.advancedSearch(getCustomSearchCriteria(ERRORS), 0, 1)
+        //     .then(response => setErrorsCount(response.data.total));
     }, []);
 
     return (
@@ -40,18 +36,18 @@ function ManualRightEntryTableTabs({selectedTab, updateManualRightEntrySelectedT
             <ManualRightEntryTab isActive={selectedTab === TOTAL_RIGHTS}
                                  onClick={() => updateManualRightEntrySelectedTab(TOTAL_RIGHTS)}>Total Rights
                 ({totalRightsCount})</ManualRightEntryTab>
-            <ManualRightEntryTab isActive={selectedTab === CREATED}
-                                 onClick={() => updateManualRightEntrySelectedTab(CREATED)}>Created
-                ({createdCount})</ManualRightEntryTab>
-            <ManualRightEntryTab isActive={selectedTab === UPDATED}
-                                 onClick={() => updateManualRightEntrySelectedTab(UPDATED)}>Updated
-                ({updatedCount})</ManualRightEntryTab>
-            <ManualRightEntryTab isActive={selectedTab === PENDING}
-                                 onClick={() => updateManualRightEntrySelectedTab(PENDING)}>Pending
-                ({pendingCount})</ManualRightEntryTab>
-            <ManualRightEntryTab isActive={selectedTab === ERRORS}
-                                 onClick={() => updateManualRightEntrySelectedTab(ERRORS)}>Errors
-                ({errorsCount})</ManualRightEntryTab>
+            {/*<ManualRightEntryTab isActive={selectedTab === CREATED}*/}
+            {/*                     onClick={() => updateManualRightEntrySelectedTab(CREATED)}>Created*/}
+            {/*    ({createdCount})</ManualRightEntryTab>*/}
+            {/*<ManualRightEntryTab isActive={selectedTab === UPDATED}*/}
+            {/*                     onClick={() => updateManualRightEntrySelectedTab(UPDATED)}>Updated*/}
+            {/*    ({updatedCount})</ManualRightEntryTab>*/}
+            {/*<ManualRightEntryTab isActive={selectedTab === PENDING}*/}
+            {/*                     onClick={() => updateManualRightEntrySelectedTab(PENDING)}>Pending*/}
+            {/*    ({pendingCount})</ManualRightEntryTab>*/}
+            {/*<ManualRightEntryTab isActive={selectedTab === ERRORS}*/}
+            {/*                     onClick={() => updateManualRightEntrySelectedTab(ERRORS)}>Errors*/}
+            {/*    ({errorsCount})</ManualRightEntryTab>*/}
             <ManualRightEntryTab isActive={selectedTab === FATAL}
                                  onClick={() => updateManualRightEntrySelectedTab(FATAL)}>Fatal
                 ({fatalCount})</ManualRightEntryTab>
