26d73ed1bb8bc262bf01cfb4a9ba367d032bf579
dradojkovic <djordje.radojkovic@computerrock.com>
2019-11-26 10:00:38 +0100

    PORT-1073: Prettify the component and date display; Format date based on locale

--- src/containers/metadata/dashboard/components/territorymetadata/TerritoryMetadataTab.js
+++ src/containers/metadata/dashboard/components/territorymetadata/TerritoryMetadataTab.js
@@ -1,56 +1,109 @@
-import React, { Component } from 'react';
-import { Row, Col, Container } from 'reactstrap';
+import React from 'react';
 import PropTypes from 'prop-types';
+import {Row, Col, Container} from 'reactstrap';
 import moment from 'moment';
-import {DATE_FORMAT, COUNTRY} from '../../../../../constants/metadata/constant-variables';
+import {useIntl} from 'react-intl';
+import {getDateFormatBasedOnLocale} from '../../../../../util/Common';
+import {COUNTRY} from '../../../../../constants/metadata/constant-variables';
 
-class TerritoryMetadataTab extends Component {
-    render() {
-        return (
-            <div id="territoryMetadataTabs">
-                <Container>
-                    <Row style={{ padding: '15px' }}>
-                        <Col>
-                            <b>Locale: </b> {this.props.data.locale ? this.props.getLanguageByCode(this.props.data.locale, COUNTRY) : <span style={{ color: '#999' }}>Empty</span>}
-                        </Col>
-                        <Col>
-                            <b>Box Office: </b> {this.props.data.boxOffice ? this.props.data.boxOffice : <span style={{ color: '#999' }}>Empty</span>}
-                        </Col>
-                    </Row>
-                    <Row style={{ padding: '15px' }}>
-                        <Col>
-                            <b>Release Year: </b> {this.props.data.releaseYear ? this.props.data.releaseYear : <span style={{ color: '#999' }}>Empty</span>}
-                        </Col>
-                        <Col>
-                            <b>Original Air Date: </b> {this.props.data.originalAirDate ? moment(this.props.data.originalAirDate).format(DATE_FORMAT) : <span style={{ color: '#999' }}>Empty</span>}
-                        </Col>
-                    </Row>
-                    <Row style={{ padding: '15px' }}>
-                        <Col>
-                            <b>Home Video Release Date: </b> {this.props.data.homeVideoReleaseDate ? moment(this.props.data.homeVideoReleaseDate).format(DATE_FORMAT) : <span style={{ color: '#999' }}>Empty</span>}
-                        </Col>
-                        <Col>
-                            <b>Avail Announce Date: </b> {this.props.data.availAnnounceDate ? moment(this.props.data.availAnnounceDate).format(DATE_FORMAT) : <span style={{ color: '#999' }}>Empty</span>}
-                        </Col>
-                    </Row>
-                    <Row style={{ padding: '15px' }}>
-                        <Col>
-                            <b>Theatrical Release Date: </b> {this.props.data.theatricalReleaseDate ? moment(this.props.data.theatricalReleaseDate).format(DATE_FORMAT) : <span style={{ color: '#999' }}>Empty</span>}
-                        </Col>
-                        <Col>
-                            <b>EST Release Date: </b> {this.props.data.estReleaseDate ? moment(this.props.data.estReleaseDate).format(DATE_FORMAT) : <span style={{ color: '#999' }}>Empty</span>}
-                        </Col>
-                    </Row>
-                </Container>
-            </div>
-        );
-    }
-}
+const TerritoryMetadataTab = ({data, getLanguageByCode}) => {
+    const {
+        locale,
+        boxOffice,
+        releaseYear,
+        originalAirDate,
+        homeVideoReleaseDate,
+        availAnnounceDate,
+        theatricalReleaseDate,
+        estReleaseDate
+    } = data || {};
+
+    // Get locale provided by intl
+    const intl = useIntl();
+    const {globalLocale = 'en-US'} = intl || {};
+
+    const dateFormat = getDateFormatBasedOnLocale(globalLocale);
+
+    return (
+        <div id="territoryMetadataTabs">
+            <Container>
+                <Row style={{padding: '15px'}}>
+                    <Col>
+                        <b>Locale: </b>
+                        {locale
+                            ? getLanguageByCode(locale, COUNTRY)
+                            : <span style={{color: '#999'}}>Empty</span>
+                        }
+                    </Col>
+                    <Col>
+                        <b>Box Office: </b>
+                        {boxOffice
+                            ? boxOffice
+                            : <span style={{color: '#999'}}>Empty</span>
+                        }
+                    </Col>
+                </Row>
+                <Row style={{padding: '15px'}}>
+                    <Col>
+                        <b>Release Year: </b>
+                        {releaseYear
+                            ? releaseYear
+                            : <span style={{color: '#999'}}>Empty</span>
+                        }
+                    </Col>
+                    <Col>
+                        <b>Original Air Date: </b>
+                        {originalAirDate
+                            ? moment(originalAirDate).format(dateFormat)
+                            : <span style={{color: '#999'}}>Empty</span>
+                        }
+                    </Col>
+                </Row>
+                <Row style={{padding: '15px'}}>
+                    <Col>
+                        <b>Home Video Release Date: </b>
+                        {homeVideoReleaseDate
+                            ? moment(homeVideoReleaseDate).format(dateFormat)
+                            : <span style={{color: '#999'}}>Empty</span>
+                        }
+                    </Col>
+                    <Col>
+                        <b>Avail Announce Date: </b>
+                        {availAnnounceDate
+                            ? moment(availAnnounceDate).format(dateFormat)
+                            : <span style={{color: '#999'}}>Empty</span>
+                        }
+                    </Col>
+                </Row>
+                <Row style={{padding: '15px'}}>
+                    <Col>
+                        <b>Theatrical Release Date: </b>
+                        {theatricalReleaseDate
+                            ? moment(theatricalReleaseDate).format(dateFormat)
+                            : <span style={{color: '#999'}}>Empty</span>
+                        }
+                    </Col>
+                    <Col>
+                        <b>EST Release Date: </b>
+                        {estReleaseDate
+                            ? moment(estReleaseDate).format(dateFormat)
+                            : <span style={{color: '#999'}}>Empty</span>
+                        }
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
+};
 
 TerritoryMetadataTab.propTypes = {
     data: PropTypes.object,
     getLanguageByCode: PropTypes.func
 };
 
+TerritoryMetadataTab.defaultProps = {
+    data: {},
+    getLanguageByCode: () => null,
+};
 
 export default TerritoryMetadataTab;
