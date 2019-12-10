2044af0412b779dcda41f709b29d5ffe4479065b
Adrian Buse <adrian.buse2@vubiquity.co.uk>
2019-12-10 13:45:57 +0100

    PORT-1080 adressing comments

--- src/containers/config/custom-types/DelayedOptions.js
+++ src/containers/config/custom-types/DelayedOptions.js
@@ -1,10 +1,12 @@
 import {renderer as akRenderer} from 'react-forms-processor-atlaskit';
 import PropTypes from 'prop-types';
 import React from 'react';
+import get from 'lodash.get';
 
 const DelayedOptions = ({field, onChange, onFieldFocus, onFieldBlur}) => {
-    if(field.options && Array.isArray(field.options) && field.options.length && field.options[0] && field.options[0].items && !field.options[0].items.includes(field.value)){
-        const val = field.options[0].items.find(option => JSON.stringify(option.value) === JSON.stringify(field.value));
+    const items = get(field, 'options[0].items', []);
+    if(!items.includes(field.value)){
+        const val = items.find(option => JSON.stringify(option.value) === JSON.stringify(field.value));
         //find object with same values inside as field.value, replace reference in value with new one from options.
         //this is necessary because select uses '===' on.value of each options to determine the current one. works ok with primitives (because it checks values)
         //doesn't work with nonprimitives where operator '===' checks references, thats why we need this trick.
