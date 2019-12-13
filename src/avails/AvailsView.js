6166c76dc8396dc3e99bfb9c150a485ebc6b9b58
Madhurima Kandul <madhkand@amdocs.com>
2019-12-13 14:14:40 +0200

    PORT-1150: Add Avails Ingest component and filter section

--- src/avails/AvailsView.js
+++ src/avails/AvailsView.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import PageHeader from '@atlaskit/page-header';
-import AvailsHistory from './history/AvailsHistory';
+import Ingest from './Ingest/Ingest';
 import './AvailsView.scss';
 
-
-//NOTE: This is just a skeleton component, feel free to edit as you wish. None of this has to stay
 const AvailsView = () => (
     <div className="nexus-c-avails-view">
-        <AvailsHistory/>
+        <Ingest/>
         <PageHeader>
             AVAILS
         </PageHeader>
