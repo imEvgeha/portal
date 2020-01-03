9f2ec515ff251874696162fc6aed45db0a952c1a
Madhurima Kandul <madhkand@amdocs.com>
2020-01-03 11:49:43 +0200

    PORT-1157: Add sleecteor for getting ingestion

--- src/avails/availsSelectors.js
+++ src/avails/availsSelectors.js
@@ -11,3 +11,16 @@ export const getTotalAvails = createSelector(
     getAvailsReducer,
     availsReducer => availsReducer.total,
 );
+
+const getHistory = createSelector(
+    getAvails,
+    (_, historyId) => historyId,
+    (avails, historyId) => avails.find(avail => avail.id === historyId) || {}
+);
+
+const getAttachmentId = (state, historyId, attachmentId) => attachmentId;
+
+export const getIngest = createSelector(
+    [getHistory, getAttachmentId],
+    (history, attachmentId) => history.attachments && history.attachments.find(a => a.id === attachmentId) || {}
+);
