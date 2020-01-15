89765ea7a594692aa127c6fde763056995c35c05
dradojkovic <djordje.radojkovic@computerrock.com>
2020-01-15 12:42:28 +0100

    PORT-1393: Add default for date, since moment(undefined).isValid() returns true for some odd reason

--- src/util/Common.js
+++ src/util/Common.js
@@ -225,7 +225,7 @@ const minTwoDigits = n => `${n < 10 ? '0' : ''}${n}`;
 const getDateFormatBasedOnLocale = (locale) => (moment().locale(locale).localeData().longDateFormat('L'));
 
 // Attach (UTC) to date, if it is simulcast
-const parseSimulcast = (date, dateFormat) => {
+const parseSimulcast = (date = null, dateFormat) => {
     const isUTC = date.endsWith('Z');
     return moment(date).isValid()
         ? `${moment(date).utc(isUTC).format(dateFormat)}${isUTC ? ' (UTC)' : ''}`
