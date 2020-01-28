7894681f6cfb649763d6d1d5940d5650ea90a573
Milos Adzic <milos.adzic.ma@gmail.com>
2020-01-28 19:17:56 +0100

    PORT-1517: fix relative paths

--- src/metadata/title-merging/TitleMergingView.js
+++ src/metadata/title-merging/TitleMergingView.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
+import {isEqual} from 'lodash.isequal';
 import SectionMessage from '@atlaskit/section-message';
 import './TitleMergingView.scss';
 import {NexusTitle, NexusGrid} from '../../ui-elements/';
 import {TITLE, SECTION_MESSAGE, FOCUSED_TITLE} from './constants';
-import {fetchTitle} from './metadataActions';
-import * as selectors from './metadataSelectors';
-import {getColumnDefs} from './titleMatchingSelectors';
+import {fetchTitle} from '../metadataActions';
+import * as selectors from '../metadataSelectors';
+import {getColumnDefs} from '../../avails/title-matching/titleMatchingSelectors';
+import {createColumnDefs} from '../../avails/title-matching/titleMatchingActions';
+import usePrevious from '../../util/hooks/usePrevious';
 
-const TitleMergingView = ({focusedTitle}) => {
-    const columnDefs = [];
+const TitleMergingView = ({focusedTitle, getColumnDefs, match}) => {
+    const previousColumnDefs = usePrevious(columnDefs);
+    const {params = {}} = match;
+
+    // TODO: this should be generate on initial app load
+    useEffect(() => {
+        if (!isEqual(previousColumnDefs, columnDefs)) {
+            createColumnDefs();
+        }
+    }, [columnDefs]);
+
+    useEffect(() => {
+        const {id} = params || {};
+        fetchTitle({id});
+    }, []);
 
     return (
         <div className="nexus-c-title-merging-view">
@@ -32,6 +48,8 @@ const TitleMergingView = ({focusedTitle}) => {
 
 TitleMergingView.propsTypes = {
     focusedTitle: PropTypes.object,
+    createColumnDefs: PropTypes.func.isRequired,
+    columnDefs: PropTypes.array,
 };
 
 TitleMergingView.defaultProps = {
@@ -42,11 +60,13 @@ const createMapStateToProps = () => {
     const titleSelector = selectors.createTitleSelector();
     return (state, props) => ({
         focusedTitle: titleSelector(state, props),
+        columnDefs: getColumnDefs(state),
     });
 };
 
 const mapDispatchToProps = dispatch => ({
     fetchTitle: payload => dispatch(fetchTitle(payload)),
+    createColumnDefs: () => dispatch(createColumnDefs()),
 });
 
 export default connect(createMapStateToProps, mapDispatchToProps)(TitleMergingView); // eslint-disable-line
