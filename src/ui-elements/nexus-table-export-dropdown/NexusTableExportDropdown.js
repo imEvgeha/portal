f6e3fceb256cf88121f765ad6e4f6d04bea9498c
Milos Adzic <milos.adzic.ma@gmail.com>
2020-02-28 13:11:39 +0100

    PORT-1756: update style

--- src/ui-elements/nexus-table-export-dropdown/NexusTableExportDropdown.js
+++ src/ui-elements/nexus-table-export-dropdown/NexusTableExportDropdown.js
@@ -80,8 +80,9 @@ function NexusTableExportDropdown({isSelectedOptionActive, selectedRows, totalRo
         return columns.map(({colDef: {field} = {}}) => field).filter(col => mappingColumnNames.includes(col));
     };
 
-    const renderDropdown = () => {
-        return <DropdownMenu
+    const renderDropdown = () => (
+        <DropdownMenu
+            className="nexus-c-button"
             trigger="Export"
             triggerType="button"
             triggerButtonProps={{isDisabled: isDisabled}}
@@ -90,18 +91,17 @@ function NexusTableExportDropdown({isSelectedOptionActive, selectedRows, totalRo
                 <DropdownItem onClick={onAllColumnsExportClick}>All Columns</DropdownItem>
                 <DropdownItem onClick={onVisibleColumnsExportClick}>Visible Columns</DropdownItem>
             </DropdownItemGroup>
-        </DropdownMenu>;
-    };
+        </DropdownMenu>
+    );
 
     return (
         <div className='nexus-c-right-repository-export'>
-            {isDisabled &&
+            {isDisabled ? (
                 <NexusTooltip
                     content={tooltipContent}
                     children={renderDropdown()}
                 />
-            }
-            {!isDisabled && renderDropdown()}
+            ) : renderDropdown()}
         </div>
     );
 }
