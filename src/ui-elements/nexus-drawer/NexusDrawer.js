5640f1e0791527ebb7f32e1c17b9eef8ebc6b8ad
dradojkovic <djordje.radojkovic@computerrock.com>
2020-03-09 17:00:31 +0100

    PORT-1893: Extend drawer with width and onClose props. Add Close button

--- src/ui-elements/nexus-drawer/NexusDrawer.js
+++ src/ui-elements/nexus-drawer/NexusDrawer.js
@@ -1,33 +1,60 @@
-import React, {Fragment, useContext, useEffect} from 'react';
+import React, {Fragment, useContext, useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
+import ArrowLeftIcon from '@atlaskit/icon/glyph/arrow-left';
+import ArrowRightIcon from '@atlaskit/icon/glyph/arrow-right';
 import {NexusOverlayContext} from '../nexus-overlay/NexusOverlay';
+import Button from '@atlaskit/button';
 
 import './NexusDrawer.scss';
 
 const NexusDrawer = ({
+    children,
     position,
+    width,
     isOpen,
+    onClose,
 }) => {
     const {setIsOverlayActive} = useContext(NexusOverlayContext);
-    useEffect(() => setIsOverlayActive(isOpen), [isOpen]);
 
-    const openClass = isOpen ? 'nexus-c-drawer--is-open' : '';
+    const [isOpenClass, setIsOpenClass] = useState('');
+
+    useEffect(() => {
+        setIsOverlayActive(isOpen);
+        setIsOpenClass(isOpen ? 'nexus-c-drawer--is-open' : '');
+        }, [isOpen]
+    );
 
     return (
-        <div className={`nexus-c-drawer nexus-c-drawer--is-${position} ${openClass} nexus-c-drawer--is-medium-width`}>
-            brtthaoeu
-            <button onClick={() => setIsOverlayActive(true)}> brttt </button>
+        <div
+            className={`
+                nexus-c-drawer 
+                nexus-c-drawer--is-${position} 
+                nexus-c-drawer--is-${width}-width
+                ${isOpenClass} 
+            `}
+        >
+            <Button
+                appearance="subtle"
+                onClick={onClose}
+                className={`nexus-c-drawer__close-btn nexus-c-drawer__close-btn--is-${position}`}
+            >
+                {position === 'right' ? <ArrowRightIcon /> : <ArrowLeftIcon />}
+            </Button>
+            {children}
         </div>
     );
 };
 
 NexusDrawer.propTypes = {
     position: PropTypes.string,
+    width: PropTypes.string,
     isOpen: PropTypes.bool.isRequired,
+    onClose: PropTypes.func.isRequired,
 };
 
 NexusDrawer.defaultProps = {
     position: 'right',
+    width: 'medium',
 };
 
 export default NexusDrawer;
