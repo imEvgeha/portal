bd14206c422a27099ff803c647504eca14d42805
Antonia Nikolaou <antonini@amdocs.com>
2020-03-26 09:30:36 +0200

    PORT-1704: Add UT

--- src/ui/elements/nexus-grid/hoc/withColumnsResizing.js
+++ src/ui/elements/nexus-grid/hoc/withColumnsResizing.js
@@ -1,42 +1,38 @@
 import React, {useState} from 'react';
-import {store} from '../../../../index';
 import {setGridColumnsSize} from '../../../../stores/actions';
 import {createColumnsSizeSelector} from '../../../../stores/selectors/columnsSize/columnsSizelSelectors';
 import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
 
 const AG_GRID_DEF_COL_DEF = {
     resizable: true
 };
 
-const withColumnsResizing = (colDef= AG_GRID_DEF_COL_DEF) => WrappedComponent =>{
-    const ComposedComponent = (props) => {
-        const {columnDefs, id, existingColumnsSize} = props;
-
+const withColumnsResizing = ({colDef= AG_GRID_DEF_COL_DEF} = {}) => WrappedComponent =>{
+     const ComposedComponent = (props) => {
+        const {columnDefs, id, existingColumnsSize, updateGridColumnsSize} = props;
         const existingTableColumnsSize = existingColumnsSize && id && existingColumnsSize[id] ? existingColumnsSize[id] : {};
         let columnDefsWithSizes = columnDefs;
         if(existingTableColumnsSize) {
             columnDefsWithSizes = columnDefs.map(c => c.colId && existingTableColumnsSize[c.colId] ? {...c, width: existingTableColumnsSize[c.colId]} : c);
         }
-
-
         const [columnsSize, setColumnsSize] = useState(existingTableColumnsSize);
 
         const handleColumnResized  = e => {
             if(e.finished){
                 const updatedColumnsSize = {...columnsSize, [e.column.colDef.colId] : e.column.actualWidth};
                 setColumnsSize(updatedColumnsSize);
-                id && store.dispatch(setGridColumnsSize(id, updatedColumnsSize));
+                id && updateGridColumnsSize(id, updatedColumnsSize);
             }
         };
-
-        return (
-            <WrappedComponent
-                {...props}
-                defaultColDef={colDef}
-                onColumnResized={handleColumnResized}
-                columnDefs={columnDefsWithSizes}
-            />
-        );
+         return (
+             <WrappedComponent
+                 {...props}
+                 defaultColDef={colDef}
+                 onColumnResized={handleColumnResized}
+                 columnDefs={columnDefsWithSizes}
+             />
+         );
     };
 
     const createMapStateToProps = () => {
@@ -46,7 +42,23 @@ const withColumnsResizing = (colDef= AG_GRID_DEF_COL_DEF) => WrappedComponent =>
         });
     };
 
-    return connect(createMapStateToProps)(ComposedComponent); // eslint-disable-line
+    const mapDispatchToProps = dispatch => ({
+        updateGridColumnsSize: (id, payload) => dispatch(setGridColumnsSize(id, payload)),
+    });
+
+    ComposedComponent.propTypes = {
+        ...WrappedComponent.propTypes,
+        updateGridColumnsSize: PropTypes.func,
+        existingColumnsSize: PropTypes.object,
+    };
+
+    ComposedComponent.defaultProps = {
+        ...WrappedComponent.defaultProps,
+        updateGridColumnsSize: null,
+        existingColumnsSize: {},
+    };
+
+    return connect(createMapStateToProps, mapDispatchToProps)(ComposedComponent); // eslint-disable-line
 };
 
 export default withColumnsResizing;
