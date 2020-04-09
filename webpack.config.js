89211c0a359b47cc37b8fa9e09508e4f26977f7d
Milos Adzic <milos.adzic.ma@gmail.com>
2020-04-09 18:01:14 +0200

    PORT-734: add webpack analyze and visualizer

--- webpack.config.js
+++
@@ -1,7 +0,0 @@
-const webpackMerge = require('webpack-merge');
-const commonConfig = require('./webpack/webpack.common.js');
-
-module.exports = ({env}) => {
-    const envConfig = require(`./webpack/webpack.${env}.js`);
-    return webpackMerge(commonConfig, envConfig);
-};
