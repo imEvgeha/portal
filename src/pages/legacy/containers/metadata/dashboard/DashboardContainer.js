e6d3b659e0cc6c7f1ae0b80a0283c4609ba884bd
Kuldeep Bora <KuldeepS.Bora@amdocs.com>
2020-04-30 19:36:57 +0300

    Removing Advance search functionality
    removing:
    1.  button
    2. <AdvanceSearch> component
    3.  Functions related to above

--- src/pages/legacy/containers/metadata/dashboard/DashboardContainer.js
+++ src/pages/legacy/containers/metadata/dashboard/DashboardContainer.js
@@ -3,16 +3,15 @@ import './DashboardContainer.scss';
 import React from 'react';
 import {connect} from 'react-redux';
 import FreeTextSearch from './components/FreeTextSearch';
-import AdvancedSearchPanel from './components/AdvancedSearchPanel';
 import {
-    searchFormUseAdvancedSearch,
+    //searchFormUseAdvancedSearch,
     resultPageLoading,
     resultPageSort,
     resultPageUpdate,
     resultPageSelect,
     searchFormShowSearchResults,
-    searchFormShowAdvancedSearch,
-    searchFormSetAdvancedSearchCriteria
+    //searchFormShowAdvancedSearch,
+    //searchFormSetAdvancedSearchCriteria
 } from '../../../stores/actions/metadata/index';
 import DashboardTab from './DashboardTab';
 import PropTypes from 'prop-types';
@@ -25,22 +24,22 @@ const mapStateToProps = state => {
     return {
         profileInfo: state.profileInfo,
         selected: state.titleReducer.session.titleTabPageSelection.selected,
-        showAdvancedSearch: state.titleReducer.session.showAdvancedSearch,
+        //showAdvancedSearch: state.titleReducer.session.showAdvancedSearch,
         showSearchResults: state.titleReducer.session.showSearchResults,
         searchCriteria: state.titleReducer.session.advancedSearchCriteria,
-        useAdvancedSearch: state.titleReducer.session.useAdvancedSearch,
+        //useAdvancedSearch: state.titleReducer.session.useAdvancedSearch,
     };
 };
 
 const mapDispatchToProps = {
-    searchFormUseAdvancedSearch,
+    //searchFormUseAdvancedSearch,
     resultPageLoading,
     resultPageSort,
     resultPageUpdate,
     resultPageSelect,
-    searchFormShowAdvancedSearch,
+    //searchFormShowAdvancedSearch,
     searchFormShowSearchResults,
-    searchFormSetAdvancedSearchCriteria
+    //searchFormSetAdvancedSearchCriteria
 };
 
 class DashboardContainer extends React.Component {
@@ -49,9 +48,9 @@ class DashboardContainer extends React.Component {
     constructor(props) {
         super(props);
         this.state = {};
-        this.toggleAdvancedSearch = this.toggleAdvancedSearch.bind(this);
+        //this.toggleAdvancedSearch = this.toggleAdvancedSearch.bind(this);
         this.handleTitleFreeTextSearch = this.handleTitleFreeTextSearch.bind(this);
-        this.handleTitleAdvancedSearch = this.handleTitleAdvancedSearch.bind(this);
+        //this.handleTitleAdvancedSearch = this.handleTitleAdvancedSearch.bind(this);
         this.handleBackToDashboard = this.handleBackToDashboard.bind(this);
         this.cleanSelection = this.cleanSelection.bind(this);
     }
@@ -67,9 +66,9 @@ class DashboardContainer extends React.Component {
                 if (state.rowInvalid !== undefined) {
                     criteria.rowInvalid = {value: state.rowInvalid};
                 }
-                this.props.searchFormShowAdvancedSearch(true);
-                this.props.searchFormSetAdvancedSearchCriteria(criteria);
-                this.handleTitleAdvancedSearch(criteria);
+               // this.props.searchFormShowAdvancedSearch(true);
+                //this.props.searchFormSetAdvancedSearchCriteria(criteria);
+                //this.handleTitleAdvancedSearch(criteria);
                 this.fromHistory = true;
             } else if (state.back) {
                 this.handleBackToDashboard();
@@ -84,13 +83,15 @@ class DashboardContainer extends React.Component {
     }
 
     handleBackToDashboard() {
-        this.props.searchFormShowAdvancedSearch(false);
+        //this.props.searchFormShowAdvancedSearch(false);
         this.props.searchFormShowSearchResults(false);
     }
 
+    /*
     toggleAdvancedSearch() {
         this.props.searchFormShowAdvancedSearch(!this.props.showAdvancedSearch);
     }
+    */
 
     handleTitleFreeTextSearch(searchCriteria) {
         // this.props.searchFormUseAdvancedSearch(false);
@@ -99,12 +100,14 @@ class DashboardContainer extends React.Component {
         this.cleanSelection();
     }
 
+    /*
     handleTitleAdvancedSearch(searchCriteria) {
         this.props.searchFormUseAdvancedSearch(true);
         this.props.searchFormShowSearchResults(true);
         titleSearchHelper.advancedSearch(searchCriteria);
         this.cleanSelection();
     }
+    */
 
     cleanSelection() {
         const titleTabPageSelection = {
@@ -117,35 +120,24 @@ class DashboardContainer extends React.Component {
     render() {
         return (
             <div>
-                <div className={'container-fluid vu-free-text-search ' + (this.props.showAdvancedSearch ? 'hide': '')}>
+                <div className={'container-fluid vu-free-text-search'}>
                     <div>
                         <table style={{width: '100%'}}>
                             <tbody>
                                 <tr>
                                     <td>
                                         <FreeTextSearch
-                                            disabled={this.props.showAdvancedSearch}
+                                            disabled={false}
                                             containerId="dashboard-title"
                                             onSearch={this.handleTitleFreeTextSearch}
                                         />
-                                    </td>
-                                    <td style={{width: '20px', height: '30px', paddingLeft: '8px'}}>
-                                        <button
-                                            className="btn btn-outline-secondary advanced-search-btn"
-                                            style={{height: '40px'}}
-                                            title="Advanced search"
-                                            id="dashboard-title-advanced-search-btn"
-                                            onClick={this.toggleAdvancedSearch}
-                                        >
-                                            <i className="fas fa-filter table-top-icon" style={{fontSize: '1.25em', marginLeft: '-3px', marginTop: '6px', padding: '0px'}}> </i>
-                                        </button>
-                                    </td>
+                                    </td>  
                                 </tr>
                             </tbody>
                         </table>
                     </div>
                 </div>
-                <AdvancedSearchPanel hide={!this.props.showAdvancedSearch} onSearch={this.handleTitleAdvancedSearch} onToggleAdvancedSearch={this.toggleAdvancedSearch} />
+            
                 <DashboardTab />
                 <div id="dashboard-result-table">
                     <div className="container-fluid">
