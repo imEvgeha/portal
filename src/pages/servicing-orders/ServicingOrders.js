8d6877e681a425a81fe9ea1e5367f0e624324d5f
Adrian Buse <adrian.buse2@vubiquity.co.uk>
2020-05-07 16:44:02 +0200

    PORT-2225 initial implementation of UI in page, minimum functionality

--- src/pages/servicing-orders/ServicingOrders.js
+++ src/pages/servicing-orders/ServicingOrders.js
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Select from '@atlaskit/select';
+import Button from '@atlaskit/button';
 import './servicingOrders.scss';
 
 const ServicingOrders = () => {
+    const [hideReady, setHideReady] = useState(false);
+    const [hideCompleted, setHideCompleted] = useState(false);
+    const NO_CUSTOMER_FILTER = { label: 'Select...', value: '' };
+    const [customerFilter, setCustomerFilter] = useState(NO_CUSTOMER_FILTER);
     return (
         <div className='servicing-orders'>
             <div className='servicing-orders--title'>
                 Servicing Orders
+            </div>
+            <div className='servicing-orders--external-filters'>
+                <div className='nexus-c--customer-filter'>
+                    Customer
+                    <Select
+                        options={[
+                            NO_CUSTOMER_FILTER,
+                            { label: 'MGM', value: 'mgm' },
+                            { label: 'MGM2', value: 'mgm2' },
+                        ]}
+                        placeholder={NO_CUSTOMER_FILTER.label}
+                        value={customerFilter}
+                        onChange={(val) => {
+                            setCustomerFilter(val);
+                        }}
+                    />
+                </div>
+                <Button
+                    isSelected={hideReady}
+                    onClick={() => setHideReady(!hideReady)}
+                >
+                    Hide Ready
+                </Button>
+                <Button
+                    isSelected={hideCompleted}
+                    onClick={() => setHideCompleted(!hideCompleted)}
+                >
+                    Hide Completed
+                </Button>
             </div>
         </div>
     );
