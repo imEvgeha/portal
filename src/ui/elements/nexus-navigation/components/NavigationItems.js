490cf76ca5b9ffeb55c8ffe52fcea69da400f4e2
Madhurima Kandul <madhkand@amdocs.com>
2020-05-08 12:12:52 +0300

    PORT-2255: Add dev or qa check for events page

--- src/ui/elements/nexus-navigation/components/NavigationItems.js
+++ src/ui/elements/nexus-navigation/components/NavigationItems.js
@@ -5,6 +5,7 @@ import TrayIcon from '@atlaskit/icon/glyph/tray';
 import DetailViewIcon from '@atlaskit/icon/glyph/detail-view';
 import RecentIcon from '@atlaskit/icon/glyph/recent';
 import NexusNavIcon from '../../../../assets/nexus-nav-icon.svg';
+import {URL} from '../../../../util/Common';
 import {AVAILS, METADATA, MEDIA, SERVICING_ORDERS, EVENT_MANAGEMENT} from '../constants';
 
 export const navigationPrimaryItems = (selectedItem, handleClick) => [
@@ -49,12 +50,14 @@ export const navigationPrimaryItems = (selectedItem, handleClick) => [
         isSelected: (selectedItem === SERVICING_ORDERS),
         onClick: () => handleClick(SERVICING_ORDERS),
     },
-    {
-        icon: RecentIcon,
-        id: EVENT_MANAGEMENT,
-        tooltip: 'Event Management',
-        isSelected: (selectedItem === EVENT_MANAGEMENT),
-        onClick: () => handleClick(EVENT_MANAGEMENT),
-    }
+    URL.isLocalOrDevOrQA() && (
+        {
+            icon: RecentIcon,
+            id: EVENT_MANAGEMENT,
+            tooltip: 'Event Management',
+            isSelected: (selectedItem === EVENT_MANAGEMENT),
+            onClick: () => handleClick(EVENT_MANAGEMENT),
+        }
+    )
 ];
 
