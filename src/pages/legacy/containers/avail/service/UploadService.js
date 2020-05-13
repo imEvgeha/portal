e79a30c98a92e2500a5798dc91c0cdfdc4b0125d
Milos Adzic <milos.adzic.ma@gmail.com>
2020-05-13 12:47:41 +0200

    PORT-746: remove dead code

--- src/pages/legacy/containers/avail/service/UploadService.js
+++ src/pages/legacy/containers/avail/service/UploadService.js
@@ -2,8 +2,12 @@ import config from 'react-global-configuration';
 import {nexusFetch} from '../../../../../util/http-client/index';
 import {keycloak} from '../../../../../auth/keycloak';
 
+// currently FETCH API doesn't support upload progress calculation
+// for upload progress we should switch upload to XHR (XMLHttpRequest) or
+// some of the extrenal package e.g. https://www.npmjs.com/package/fetch-progress
+
 export const uploadService = {
-    uploadAvail: ({file, externalId, setUploadedPercentage, params = {}}) => {
+    uploadAvail: ({file, externalId, params = {}, ...rest}) => {
         const {token} = keycloak || {};
         const formData = new FormData();
         formData.append('avail', file);
@@ -12,19 +16,11 @@ export const uploadService = {
               ...(token ? {'Authorization': `Bearer ${token}`} : {}),
             }
         };
-        // currently FETCH API doesn't support upload progress calculation
-        // for upload progress we should switch upload to XHR (XMLHttpRequest) or
-        // some of the extrenal package e.g. https://www.npmjs.com/package/fetch-progress
-        if (setUploadedPercentage) {
-            // options.onUploadProgress = (progressEvent) => {
-            //     const { loaded, total } = progressEvent;
-            //     const percentCompleted = Math.round((loaded * 100) / total);
-            //     setUploadedPercentage(percentCompleted);
-            // };
-        }
+
         if (externalId){
             params.externalId = externalId;
         }
+
         const queryParams = new URLSearchParams({...params}).toString();
         const url = config.get('gateway.url') + config.get('gateway.service.avails') + '/avails/upload' +
             (queryParams && `?${queryParams}`);
