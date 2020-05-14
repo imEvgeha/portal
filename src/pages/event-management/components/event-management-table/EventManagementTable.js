f41dbc8de4995d971cb5013c89c006972958782c
dradojkovic <djordje.radojkovic@computerrock.com>
2020-05-14 13:00:03 +0200

    PORT-2257: Add filters and sorting to EventManagementTable

--- src/pages/event-management/components/event-management-table/EventManagementTable.js
+++ src/pages/event-management/components/event-management-table/EventManagementTable.js
@@ -4,15 +4,35 @@ import NexusGrid from '../../../../ui/elements/nexus-grid/NexusGrid';
 import withInfiniteScrolling from '../../../../ui/elements/nexus-grid/hoc/withInfiniteScrolling';
 import withSideBar from '../../../../ui/elements/nexus-grid/hoc/withSideBar';
 import {getEventSearch} from '../../eventManagmentService'; 
+import withSorting from '../../../../ui/elements/nexus-grid/hoc/withSorting';
+import withFilterableColumns from '../../../../ui/elements/nexus-grid/hoc/withFilterableColumns';
+import createValueFormatter from '../../../../ui/elements/nexus-grid/elements/value-formatter/createValueFormatter';
 import columnDefs from '../../columnMappings.json';
 
 const EventManagementGrid = compose(
     withSideBar(),
+    withSorting(),
+    withFilterableColumns(),
     withInfiniteScrolling({fetchData: getEventSearch})
 )(NexusGrid);
 
-const EventManagementTable = () => (
-    <EventManagementGrid columnDefs={columnDefs} />
-);
+const EventManagementTable = () => {
+    const updateColumnDefs = (columnDefs) => {
+        return columnDefs.map(columnDef => (
+            {
+                ...columnDef,
+                valueFormatter: createValueFormatter(columnDef),
+                cellRenderer: 'loadingCellRenderer',
+            }
+        ));
+    };
+
+    return (
+        <EventManagementGrid
+            columnDefs={updateColumnDefs(columnDefs)}
+            mapping={columnDefs}
+        />
+    );
+};
 
 export default EventManagementTable;
--- src/pages/event-management/eventManagmentService.js
+++ src/pages/event-management/eventManagmentService.js
@@ -1,9 +1,67 @@
 import Http from '../../util/Http';
-import mockData from './eventManagementMockData.json';
+import {isEmpty, isObject} from 'lodash';
+const BASE_PARAM_NAME = 'event.messageHeader.';
 
 const http = Http.create();
 
-export const getEventSearch = () => {
+export const getEventSearch = (params, page = 0, pageSize = 100, sortedParams) => {
+    let paramString = '';
+
+    // Build sortParams string if sortparams are provided
+    if (!isEmpty(sortedParams)) {
+        paramString = sortedParams.reduce((sortedParams, {colId, sort}) => (
+            `${sortedParams}${colId}=${sort}`
+        ), ';');
+    }
+
+    // Spice it up with some page and pageSize stuff
+    paramString = `${paramString}?page=${page}&size=${pageSize}`;
+
+    // Build param string if params are provided
+    if (!isEmpty(params)) {
+        paramString = Object.keys(params).reduce((paramString, paramKey) => {
+
+            // Converts '-From' and '-To' suffixes to '-Start' and '-End' respectively
+            // and packs them into a param string
+            if (isObject(params[paramKey])) {
+                const {
+                    createdTimeStampFrom,
+                    createdTimeStampTo,
+                    postedTimeStampFrom,
+                    postedTimeStampTo
+                } = params[paramKey];
+
+                const createdTimeStampStart = createdTimeStampFrom
+                    ? `${BASE_PARAM_NAME}createdTimeStampStart=${createdTimeStampFrom}`
+                    : '';
+                const createdTimeStampEnd = createdTimeStampTo
+                    ? `${BASE_PARAM_NAME}createdTimeStampEnd=${createdTimeStampTo}`
+                    : '';
+
+                const postedTimeStampStart = postedTimeStampFrom
+                    ? `${BASE_PARAM_NAME}postedTimeStampStart=${postedTimeStampFrom}`
+                    : '';
+                const postedTimeStampEnd = postedTimeStampTo
+                    ? `${BASE_PARAM_NAME}postedTimeStampEnd=${postedTimeStampTo}`
+                    : '';
+
+                const createdTimeStampParam = (createdTimeStampStart && createdTimeStampEnd)
+                    ? `${createdTimeStampStart}&${createdTimeStampEnd}`
+                    : `${createdTimeStampStart || createdTimeStampEnd || ''}`;
+
+                const postedTimeStampParam = (postedTimeStampStart && postedTimeStampEnd)
+                    ? `${postedTimeStampStart}&${postedTimeStampEnd}`
+                    : `${postedTimeStampStart || postedTimeStampEnd || ''}`;
+
+                const paramValue = createdTimeStampParam || postedTimeStampParam;
+                return `${paramString}&${paramValue}`;
+            }
+
+            return `${paramString}&${BASE_PARAM_NAME}${paramKey}=${params[paramKey]}`;
+        }, paramString);
+    }
+    
     const url = './src/pages/event-management/eventManagementMockData.json';
-    return http.get(url);
+    
+    return http.get(`${url}${paramString}`);
 };
