b383e7f3536beb5d6ce22edbf92bf97f136b19d7
Adrian Buse <adrian.buse2@vubiquity.co.uk>
2020-05-15 11:01:51 +0200

    fix compilation errors after switch from axios to fetch

--- src/pages/event-management/eventManagmentService.js
+++
@@ -1,9 +0,0 @@
-import Http from '../../util/Http';
-import mockData from './eventManagementMockData.json';
-
-const http = Http.create();
-
-export const getEventSearch = () => {
-    const url = './src/pages/event-management/eventManagementMockData.json';
-    return http.get(url);
-};
