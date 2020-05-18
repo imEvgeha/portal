cf86191b8ef6b92538681433119220609994ea7d
Milos Adzic <milos.adzic.ma@gmail.com>
2020-05-18 17:55:02 +0200

    PORT-2267: cleanup code

--- src/pages/event-management/eventManagementConstants.js
+++ src/pages/event-management/eventManagementConstants.js
@@ -1,4 +1,4 @@
 export const TITLE = 'Event Management';
 export const DRAWER_TITLE = 'Event Details';
-export const EVENT_MESSAGE = 'Event Messge';
+export const EVENT_MESSAGE = 'Event Message';
 export const DOWNLOAD = 'Download';
