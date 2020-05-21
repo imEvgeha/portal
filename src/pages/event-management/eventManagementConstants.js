0b5ae246dcb5b9f37c854b2a7340f5af92507150
dradojkovic <djordje.radojkovic@computerrock.com>
2020-05-21 10:11:54 +0200

    PORT-2263: Add constants

--- src/pages/event-management/eventManagementConstants.js
+++ src/pages/event-management/eventManagementConstants.js
@@ -1,4 +1,20 @@
 export const TITLE = 'Event Management';
 export const DRAWER_TITLE = 'Event Details';
+
+export const EVENT_HEADER = 'Event Header';
+export const EVENT_HEADER_MAIN_FIELDS = [
+    'eventId',
+    'correlationId',
+    'eventType',
+    'eventSource',
+    'tenantId',
+    'objectId',
+    'createdTimeStamp',
+];
+export const EVENT_HEADER_SECONDARY_FIELDS = [
+    'postedTimeStamp',
+    'replyTo',
+];
+
 export const EVENT_MESSAGE = 'Event Message';
 export const DOWNLOAD = 'Download';
