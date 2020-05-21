484e33724ec39d60ab616c25bfe4669f05adfb4d
dradojkovic <djordje.radojkovic@computerrock.com>
2020-05-21 10:12:44 +0200

    PORT-2263: Add EventHeader component

---
+++ src/pages/event-management/components/event-header/EventHeader.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import EventHeader from './EventHeader';
+import {EVENT_HEADER_MAIN_FIELDS, EVENT_HEADER_SECONDARY_FIELDS} from '../../eventManagementConstants';
+
+describe('EventHeader', () => {
+    describe('HTML content', () => {
+        const wrapper = shallow(
+            <EventHeader
+                event={{
+                    eventId: 'string',
+                    correlationId: 'string',
+                    eventType: 'string',
+                    eventSource: 'string',
+                    tenantId: 'string',
+                    objectId: 'string',
+                    createdTimeStamp: 'string',
+                    postedTimeStamp: 'string',
+                    replyTo: 'string',
+                }}
+            />);
+        it('should match snapshot', () => {
+            expect(wrapper).toMatchSnapshot();
+        });
+        it('should render all main fields', () => {
+            expect(wrapper.find('.nexus-c-event-header__field').length).toEqual(EVENT_HEADER_MAIN_FIELDS.length);
+        });
+        it('should render all fields when secondary fields are revealed', () => {
+            const toggle = wrapper.find('.nexus-c-event-header__secondary-fields-toggle');
+            const fieldCount = EVENT_HEADER_MAIN_FIELDS.length + EVENT_HEADER_SECONDARY_FIELDS.length;
+            toggle.simulate('click');
+            expect(wrapper.find('.nexus-c-event-header__field').length).toEqual(fieldCount);
+        });
+    });
+});
