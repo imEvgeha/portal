063a5bba4304083813c8d36b6f316998ea1c5b41
Nikola Perisic <nikola.perisic@computerrock.com>
2020-05-28 17:09:10 +0200

    PORT-2236 minor component and tests fixes

--- src/pages/servicing-orders/servicing-order/components/services-table/ServicesTable.js
+++ src/pages/servicing-orders/servicing-order/components/services-table/ServicesTable.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import PropTypes from 'prop-types';
+import {isEmpty} from 'lodash';
 import EditorCloseIcon from '@atlaskit/icon/glyph/editor/close';
 import columnDefinitions from './columnDefinitions';
 import CustomActionsCellRenderer from '../../../../../ui/elements/nexus-grid/elements/cell-renderer/CustomActionsCellRenderer';
@@ -9,8 +10,7 @@ import constants from '../fulfillment-order/constants';
 import './ServicesTable.scss';
 
 const ServicesTable = ({data}) => {
-
-    const [services, setServices] = useState();
+    const [services, setServices] = useState({});
     const [providerServices, setProviderServices] = useState('');
 
     useEffect(() => {
@@ -51,11 +51,14 @@ const ServicesTable = ({data}) => {
         cellRendererParams: services && services[`${providerServices}`],
     });
 
-    return services ? (
+    const servicesCount = services[`${providerServices}`] ? services[`${providerServices}`].length : 0;
+    const amsAssetID = services.amsAssetId || null;
+
+    return (
         <div className="nexus-c-services-table">
             <div className="nexus-c-services-table__header">
-                <h5 className="nexus-c-services-table__title">{`${constants.SERVICES_TITLE} (${services[`${providerServices}`].length})`}</h5>
-                <div className="nexus-c-services-table__subtitle">{constants.SERVICES_BARCODE}: {services.amsAssetId}</div>
+                <h5 className="nexus-c-services-table__title">{`${constants.SERVICES_TITLE} (${servicesCount})`}</h5>
+                <div className="nexus-c-services-table__subtitle">{constants.SERVICES_BARCODE}: {amsAssetID}</div>
             </div>
             <NexusGrid
                 columnDefs={[
@@ -68,7 +71,7 @@ const ServicesTable = ({data}) => {
                 onGridReady={params => params.api.sizeColumnsToFit()}
             />
         </div>
-    ) : null;
+    );
 };
 
 ServicesTable.propTypes = {
