bef0acfda9feda8c5f3c0fd88fc2db6a231c7092
dradojkovic <djordje.radojkovic@computerrock.com>
2020-05-28 09:32:51 +0200

    PORT-2310: Update snapshots; Modify unit-tests to be more readable and stable

--- src/pages/event-management/EventManagement.test.js
+++ src/pages/event-management/EventManagement.test.js
@@ -4,36 +4,55 @@ import EventManagement from './EventManagement';
 import {GRID_EVENTS} from '../../ui/elements/nexus-grid/constants';
 
 describe('EventManagement', () => {
-    describe('HTML content', () => {
-        const wrapper = shallow(<EventManagement />);
-        it('should render header title', () => {
-            expect(wrapper.find('.nexus-c-event-management .nexus-c-event-management__title').text()).toEqual('Event Management');
-        });
-        it('should render EventDrawer', () => {
-            expect(wrapper.find('EventDrawer').length).toEqual(1);
-        });
+    let wrapper = null;
+    let eventManagementTableWrapper = null;
+
+    beforeEach(() => {
+        wrapper = shallow(<EventManagement />);
+        eventManagementTableWrapper = wrapper.find('EventManagementTable');
+    });
+
+    it('should match snapshot', () => {
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it('should render header title', () => {
+        expect(wrapper.find('.nexus-c-event-management .nexus-c-event-management__title').text()).toEqual('Event Management');
+    });
+
+    it('should render EventManagementTable', () => {
+        expect(eventManagementTableWrapper.length).toEqual(1);
+    });
+
+    describe('EventDrawer (gridApi)', () => {
+        const {READY, SELECTION_CHANGED} = GRID_EVENTS;
+        const event = {eventId:'123'};
 
         const deselectAllMock = jest.fn();
-        const event = {eventId:'123'};
         const gridApiMock = {
             deselectAll: deselectAllMock,
             getSelectedRows: () => ([event]),
         };
-        const eventManagementTableWrapper = wrapper.find('EventManagementTable');
-        const {READY, SELECTION_CHANGED} = GRID_EVENTS;
-        it('should render EventManagementTable', () => {
-            expect(eventManagementTableWrapper.length).toEqual(1);
-        });
-        it('should set api on grid event and passes correct prop to EventDrawer', () => {
+
+        beforeEach(() => {
             eventManagementTableWrapper.props().onGridEvent({type: READY, api: gridApiMock});
             eventManagementTableWrapper.props().onGridEvent({type: SELECTION_CHANGED, api: gridApiMock});
             wrapper.update();
+        });
+
+        afterEach(() => {
+            deselectAllMock.mockReset();
+        });
+
+        it('should render EventDrawer', () => {
+            expect(wrapper.find('EventDrawer').length).toEqual(1);
+        });
+
+        it('should set api on grid event and passes correct prop to EventDrawer', () => {
             expect(wrapper.find('EventDrawer').props().event).toEqual(event);
         });
+
         it('should correct prop to close drawer in EventDrawer', () => {
-            eventManagementTableWrapper.props().onGridEvent({type: READY, api: gridApiMock});
-            eventManagementTableWrapper.props().onGridEvent({type: SELECTION_CHANGED, api: gridApiMock});
-            wrapper.update();
             wrapper.find('EventDrawer').props().onDrawerClose();
             expect(deselectAllMock.mock.calls.length).toEqual(1);
         });
