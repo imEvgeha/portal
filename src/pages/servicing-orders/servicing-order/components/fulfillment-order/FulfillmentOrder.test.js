fe4527445360210d7d946ea2d9f5cb76325b55ea
Adrian Buse <adrian.buse2@vubiquity.co.uk>
2020-06-03 08:47:58 +0200

    PORT-2238 fix tests

--- src/pages/servicing-orders/servicing-order/components/fulfillment-order/FulfillmentOrder.test.js
+++ src/pages/servicing-orders/servicing-order/components/fulfillment-order/FulfillmentOrder.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import {shallow} from 'enzyme';
+import * as Redux from 'react-redux';
 import FulfillmentOrder from './FulfillmentOrder';
 
 describe('FulfillmentOrder', () => {
     describe('HTML content', () => {
-        
         const selectedFulfillmentOrder = {
             'fulfillmentOrderId': 'VU000134567-001',
             'dueDate': '10/05/2021',
@@ -16,7 +16,18 @@ describe('FulfillmentOrder', () => {
             'priority': '10',
             'startDate': '07/05/2021'
         };
+
+        let useSelectorSpy = jest.spyOn(Redux, 'useSelector');
+        let useDispatchSpy = jest.spyOn(Redux, 'useDispatch');
+
+        useSelectorSpy.mockReturnValue({});
+        useDispatchSpy.mockImplementation(() => (cb) => (cb));
+
         const wrapper = shallow(<FulfillmentOrder selectedFulfillmentOrder={selectedFulfillmentOrder}/>);
+        it('should be FulfillmentOrder', () => {
+            expect(wrapper.is(FulfillmentOrder));
+        });
+
         it('should render header title', () => {
             expect(wrapper.find('.fulfillment-order .fulfillment-order__title').text()).toEqual('Fulfillment Order');
         });
