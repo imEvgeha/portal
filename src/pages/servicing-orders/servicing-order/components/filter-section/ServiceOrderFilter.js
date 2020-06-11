35b74e7c2363b53b33ead77966191f3a8a3699de
Brandon Chung <brandon.k.chung@gmail.com>
2020-06-11 14:53:04 -0500

    add sort by due date for fulfillment orders

--- src/pages/servicing-orders/servicing-order/components/filter-section/ServiceOrderFilter.js
+++ src/pages/servicing-orders/servicing-order/components/filter-section/ServiceOrderFilter.js
@@ -1,22 +1,31 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
 import Button from '@atlaskit/button';
 import Select from '@atlaskit/select';
-import moment from 'moment';
-import './ServiceOrderFilter.scss';
-import NexusDrawer from '../../../../../ui/elements/nexus-drawer/NexusDrawer';
-import PartnerRequest from '../partner-request/PartnerRequest';
-import { FILTER_LIST } from './constants';
+import PropTypes from 'prop-types';
+import React, { useState } from 'react';
 import NexusDatePicker from '../../../../../ui/elements/nexus-date-and-time-elements/nexus-date-picker/NexusDatePicker';
-import {getValidDate} from '../../../../../util/utils';
+import NexusDrawer from '../../../../../ui/elements/nexus-drawer/NexusDrawer';
+import { getValidDate } from '../../../../../util/utils';
+import PartnerRequest from '../partner-request/PartnerRequest';
+import { FILTER_LIST, SORT_DIRECTION } from './constants';
+import './ServiceOrderFilter.scss';
 
-const ServiceOrderFilter = ({orderDetails, filter, setFilter}) => {
+const ServiceOrderFilter = ({
+    orderDetails,
+    filter,
+    setFilter,
+    dueDateSortDirection,
+    setDueDateSortDirection
+}) => {
     const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
-    return(
-        <div className='so-panel-filter-detail'>
-            <span className='so-panel-filter-detail__span nexus-c-table-toolbar__title--is-active'>Customer: {orderDetails.tenant}</span>
-            <p className='so-panel-filter-detail__p nexus-c-table-toolbar__title--is-active'>Order ID: {orderDetails.external_id}</p>
+    return (
+        <div className="so-panel-filter-detail">
+            <span className="so-panel-filter-detail__span nexus-c-table-toolbar__title--is-active">
+                Customer: {orderDetails.tenant}
+            </span>
+            <p className="so-panel-filter-detail__p nexus-c-table-toolbar__title--is-active">
+                Order ID: {orderDetails.external_id}
+            </p>
             <Button onClick={() => setIsDrawerOpen(true)}>Partner Request</Button>
             <NexusDrawer
                 isOpen={isDrawerOpen}
@@ -30,21 +39,40 @@ const ServiceOrderFilter = ({orderDetails, filter, setFilter}) => {
                 />
             </NexusDrawer>
             <NexusDatePicker
-                id='dueDate'
-                label='SO Due Date'
+                id="dueDate"
+                label="SO Due Date"
                 value={getValidDate(orderDetails.sr_due_date)}
                 isDisabled
                 isReturningTime={false}
                 onChange={() => {}}
             />
-            <p className='so-panel-filter-detail__p nexus-c-table-toolbar__title--is-active'>Filter</p>
-            <div className='so-panel-filter-detail__section'>
-                <Select
-                    options={FILTER_LIST}
-                    onChange={setFilter}
-                    value={filter}
-                    placeholder="Select Status"
-                />
+            <div className="so-panel-filter-detail__dropdowns">
+                <div className="so-panel-filter-detail__dropdown">
+                    <p className="so-panel-filter-detail__dropdown-title nexus-c-table-toolbar__title--is-active">
+                        Status Filter
+                    </p>
+                    <div className="so-panel-filter-detail__dropdown-element">
+                        <Select
+                            options={FILTER_LIST}
+                            onChange={setFilter}
+                            value={filter}
+                            placeholder="Select Status"
+                        />
+                    </div>
+                </div>
+                <div className="so-panel-filter-detail__dropdown">
+                    <p className="so-panel-filter-detail__dropdown-title nexus-c-table-toolbar__title--is-active">
+                        Sort by Due Date
+                    </p>
+                    <div className="so-panel-filter-detail__dropdown-element">
+                        <Select
+                            options={SORT_DIRECTION}
+                            onChange={setDueDateSortDirection}
+                            value={dueDateSortDirection}
+                            placeholder="Select Date"
+                        />
+                    </div>
+                </div>
             </div>
         </div>
     );
--- src/pages/servicing-orders/servicing-order/components/header-section/HeaderSection.js
+++ src/pages/servicing-orders/servicing-order/components/header-section/HeaderSection.js
@@ -1,54 +1,126 @@
-import React, {useState} from 'react';
-import {Link} from 'react-router-dom';
-import PropTypes from 'prop-types';
 import ArrowLeftIcon from '@atlaskit/icon/glyph/arrow-left';
-import {backArrowColor} from '../../../../legacy/constants/avails/constants';
-import {SERVICING_ORDERS} from '../../../../../ui/elements/nexus-navigation/constants';
-import './HeaderSection.scss';
-import FulfillmentOrderPanel from '../fulfillment-order-panel/FulfillmentOrderPanel';
+import { get } from 'lodash';
+import moment from 'moment';
+import PropTypes from 'prop-types';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import FilterSolidIcon from '../../../../../assets/filter-solid.svg';
 import FilterIcon from '../../../../../assets/filter.svg';
+import { SERVICING_ORDERS } from '../../../../../ui/elements/nexus-navigation/constants';
+import { getValidDate } from '../../../../../util/utils';
+import { backArrowColor } from '../../../../legacy/constants/avails/constants';
+import { FILTER_LIST, SORT_DIRECTION } from '../filter-section/constants';
 import ServiceOrderFilter from '../filter-section/ServiceOrderFilter';
-import {getValidDate} from '../../../../../util/utils';
+import FulfillmentOrderPanel from '../fulfillment-order-panel/FulfillmentOrderPanel';
+import './HeaderSection.scss';
 
-const HeaderSection = ({orderDetails, handleFulfillmentOrderChange, selectedFulfillmentOrder}) => {
+const HeaderSection = ({
+    orderDetails,
+    handleFulfillmentOrderChange,
+    selectedFulfillmentOrder
+}) => {
     const [showFilter, setShowFilter] = useState(true);
-    const [filter, setFilter] = useState('');
+    const [filter, setFilter] = useState(FILTER_LIST[0]);
+    const [dueDateSortDirection, setDueDateSortDirection] = useState(SORT_DIRECTION[0]);
+    orderDetails = {
+        soID: '12345',
+        customer: 'Paramount',
+        creationDate: '10/09/2020',
+        createdBy: 'John Wick',
+        fulfillmentOrders: [
+            {
+                fulfillmentOrderId: 'VU000134567-001',
+                external_id: 'VU000134567-001',
+                dueDate: '10/05/2021',
+                definition: {
+                    dueDate: '10/05/2021'
+                },
+                status: 'COMPLETED'
+            },
+            {
+                fulfillmentOrderId: 'VU000134597-002',
+                external_id: 'VU000134597-002',
+                dueDate: '09/05/2021',
+                definition: {
+                    dueDate: '09/05/2021'
+                },
+                status: 'PENDING'
+            },
+            {
+                fulfillmentOrderId: 'VU000134597-003',
+                external_id: 'VU000134597-003',
+                dueDate: '11/05/2021',
+                definition: {
+                    dueDate: '11/05/2021'
+                },
+                status: 'PENDING'
+            }
+        ]
+    };
     const toggleFilters = () => setShowFilter(!showFilter);
     const getFilteredList = () => {
         let filteredList = [];
-        if(orderDetails.fulfillmentOrders && Array.isArray(orderDetails.fulfillmentOrders)) {
-            if(!filter || filter.value === 'All')
-                filteredList =  orderDetails.fulfillmentOrders;
-            else
-                filteredList = orderDetails.fulfillmentOrders.filter(item=> item.status === filter.value);
+        if (orderDetails.fulfillmentOrders && Array.isArray(orderDetails.fulfillmentOrders)) {
+            if (!filter || filter.value === 'All') {
+                filteredList = orderDetails.fulfillmentOrders;
+            } else {
+                filteredList = orderDetails.fulfillmentOrders.filter(
+                    item => item.status === filter.value
+                );
+            }
         }
         return filteredList;
     };
 
+    const sortByDueDate = (prevFulfillmentOrder, currFulfillmentOrder) => {
+        const getMomentDueDate = fulfillmentOrder => {
+            return moment(get(fulfillmentOrder, 'definition.dueDate'));
+        };
+
+        const prevFulfillmentOrderDueDate = getMomentDueDate(prevFulfillmentOrder);
+        const currFulfillmentOrderDueDate = getMomentDueDate(currFulfillmentOrder);
+        const diff = prevFulfillmentOrderDueDate.diff(currFulfillmentOrderDueDate);
+
+        switch (dueDateSortDirection.value) {
+            case 'NONE':
+                return 0;
+
+            case 'ASCENDING':
+                return diff;
+
+            case 'DESCENDING':
+                return -diff;
+        }
+    };
+
     return (
-        <div className='panel-header'>
-            <div className='panel-header__title'>
-                <div className='panel-header__title--section'>
+        <div className="panel-header">
+            <div className="panel-header__title">
+                <div className="panel-header__title--section">
                     <Link to={`/${SERVICING_ORDERS.toLowerCase()}`}>
-                        <ArrowLeftIcon size='large' primaryColor={backArrowColor} />
+                        <ArrowLeftIcon size="large" primaryColor={backArrowColor} />
                     </Link>
-                    <span className='panel-header__title--text'>Servicing Order</span>
+                    <span className="panel-header__title--text">Servicing Order</span>
                 </div>
-                <div className='panel-header__filter'>
+                <div className="panel-header__filter">
                     <div onClick={toggleFilters}>
-                        {
-                            showFilter ? <FilterSolidIcon /> : <FilterIcon />
-                        }
+                        {showFilter ? <FilterSolidIcon /> : <FilterIcon />}
                     </div>
                 </div>
             </div>
-            {
-                showFilter && <ServiceOrderFilter orderDetails={orderDetails || {}} filter={filter} setFilter={setFilter} />
-            }
-            <div className='panel-header__list'>
-                {
-                    getFilteredList().map(({id, external_id, status, definition: {dueDate} = {}},index) => (
+            {showFilter && (
+                <ServiceOrderFilter
+                    orderDetails={orderDetails || {}}
+                    filter={filter}
+                    setFilter={setFilter}
+                    dueDateSortDirection={dueDateSortDirection}
+                    setDueDateSortDirection={setDueDateSortDirection}
+                />
+            )}
+            <div className="panel-header__list">
+                {getFilteredList()
+                    .sort(sortByDueDate)
+                    .map(({ id, external_id, status, definition: { dueDate } = {} }, index) => (
                         <FulfillmentOrderPanel
                             key={index}
                             id={id}
@@ -58,9 +130,7 @@ const HeaderSection = ({orderDetails, handleFulfillmentOrderChange, selectedFulf
                             selected={selectedFulfillmentOrder === id}
                             handleFulfillmentOrderChange={handleFulfillmentOrderChange}
                         />
-                        )
-                    )
-                }
+                    ))}
             </div>
         </div>
     );
@@ -69,11 +139,11 @@ const HeaderSection = ({orderDetails, handleFulfillmentOrderChange, selectedFulf
 HeaderSection.propTypes = {
     orderDetails: PropTypes.object.isRequired,
     handleFulfillmentOrderChange: PropTypes.func,
-    selectedFulfillmentOrder: PropTypes.string,
+    selectedFulfillmentOrder: PropTypes.string
 };
 
 HeaderSection.defaultProps = {
     handleFulfillmentOrderChange: () => null,
-    selectedFulfillmentOrder: '',
+    selectedFulfillmentOrder: ''
 };
 export default HeaderSection;
