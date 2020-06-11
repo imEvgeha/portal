11af20faa627edf6f9af1b26de5a582ae13015e2
Brandon Chung <brandon.k.chung@gmail.com>
2020-06-11 15:44:12 -0500

    add description field

--- src/pages/servicing-orders/servicing-order/components/filter-section/ServiceOrderFilter.js
+++ src/pages/servicing-orders/servicing-order/components/filter-section/ServiceOrderFilter.js
@@ -1,12 +1,12 @@
 import Button from '@atlaskit/button';
 import Select from '@atlaskit/select';
 import PropTypes from 'prop-types';
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import NexusDatePicker from '../../../../../ui/elements/nexus-date-and-time-elements/nexus-date-picker/NexusDatePicker';
 import NexusDrawer from '../../../../../ui/elements/nexus-drawer/NexusDrawer';
-import { getValidDate } from '../../../../../util/utils';
+import {getValidDate} from '../../../../../util/utils';
 import PartnerRequest from '../partner-request/PartnerRequest';
-import { FILTER_LIST, SORT_DIRECTION } from './constants';
+import {FILTER_LIST, SORT_DIRECTION} from './constants';
 import './ServiceOrderFilter.scss';
 
 const ServiceOrderFilter = ({
@@ -20,12 +20,22 @@ const ServiceOrderFilter = ({
 
     return (
         <div className="so-panel-filter-detail">
-            <span className="so-panel-filter-detail__span nexus-c-table-toolbar__title--is-active">
-                Customer: {orderDetails.tenant}
-            </span>
-            <p className="so-panel-filter-detail__p nexus-c-table-toolbar__title--is-active">
-                Order ID: {orderDetails.external_id}
-            </p>
+            <div className="so-panel-filter-detail__info-section">
+                <p className="so-panel-filter-detail__info nexus-c-table-toolbar__title--is-active">
+                    Customer: {orderDetails.tenant}
+                </p>
+
+                <p className="so-panel-filter-detail__info nexus-c-table-toolbar__title--is-active">
+                    Order ID: {orderDetails.external_id}
+                </p>
+            </div>
+
+            <div className="so-panel-filter-detail__info-section">
+                <p className="so-panel-filter-detail__info so-panel-filter-detail__info nexus-c-table-toolbar__title--is-active">
+                    Description: {orderDetails.description}
+                </p>
+            </div>
+
             <Button onClick={() => setIsDrawerOpen(true)}>Partner Request</Button>
             <NexusDrawer
                 isOpen={isDrawerOpen}
--- src/pages/servicing-orders/servicing-order/components/header-section/HeaderSection.js
+++ src/pages/servicing-orders/servicing-order/components/header-section/HeaderSection.js
@@ -1,32 +1,31 @@
 import ArrowLeftIcon from '@atlaskit/icon/glyph/arrow-left';
-import { get } from 'lodash';
+import {get} from 'lodash';
 import moment from 'moment';
 import PropTypes from 'prop-types';
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
 import FilterSolidIcon from '../../../../../assets/filter-solid.svg';
 import FilterIcon from '../../../../../assets/filter.svg';
-import { SERVICING_ORDERS } from '../../../../../ui/elements/nexus-navigation/constants';
-import { getValidDate } from '../../../../../util/utils';
-import { backArrowColor } from '../../../../legacy/constants/avails/constants';
-import { FILTER_LIST, SORT_DIRECTION } from '../filter-section/constants';
+import {SERVICING_ORDERS} from '../../../../../ui/elements/nexus-navigation/constants';
+import {getValidDate} from '../../../../../util/utils';
+import {backArrowColor} from '../../../../legacy/constants/avails/constants';
+import {FILTER_LIST, SORT_DIRECTION} from '../filter-section/constants';
 import ServiceOrderFilter from '../filter-section/ServiceOrderFilter';
 import FulfillmentOrderPanel from '../fulfillment-order-panel/FulfillmentOrderPanel';
 import './HeaderSection.scss';
 
-const HeaderSection = ({
-    orderDetails,
-    handleFulfillmentOrderChange,
-    selectedFulfillmentOrder
-}) => {
+const HeaderSection = ({orderDetails, handleFulfillmentOrderChange, selectedFulfillmentOrder}) => {
     const [showFilter, setShowFilter] = useState(true);
     const [filter, setFilter] = useState(FILTER_LIST[0]);
     const [dueDateSortDirection, setDueDateSortDirection] = useState(SORT_DIRECTION[0]);
     orderDetails = {
         soID: '12345',
         customer: 'Paramount',
+        description: 'ADDAMS FAMILY | Videoland / RTL NL | SLA 10',
         creationDate: '10/09/2020',
         createdBy: 'John Wick',
+        tenant: 'MGM',
+        external_id: 'VU00001363',
         fulfillmentOrders: [
             {
                 fulfillmentOrderId: 'VU000134567-001',
@@ -120,7 +119,7 @@ const HeaderSection = ({
             <div className="panel-header__list">
                 {getFilteredList()
                     .sort(sortByDueDate)
-                    .map(({ id, external_id, status, definition: { dueDate } = {} }, index) => (
+                    .map(({id, external_id, status, definition: {dueDate} = {}}, index) => (
                         <FulfillmentOrderPanel
                             key={index}
                             id={id}
