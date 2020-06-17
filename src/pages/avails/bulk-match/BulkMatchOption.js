46ce08295748666903fd205efb09d771ba52d66f
dradojkovic <djordje.radojkovic@computerrock.com>
2020-06-17 12:38:55 +0200

    PORT-2370: Add classnames package; Rename BulkMatchView -> BulkMatchOption; Refactor code style

--- src/pages/avails/bulk-match/BulkMatchOption.js
+++ src/pages/avails/bulk-match/BulkMatchOption.js
@@ -1,45 +1,46 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {uniq} from 'lodash';
-import './BulkMatchView.scss';
+import classNames from 'classnames';
+import './BulkMatchOption.scss';
 import {NexusTooltip} from '../../../ui/elements';
 import {WARNING_MSG, LABEL} from './bulkMatchConstants';
 
-const BulkMatchView = ({selectedRights}) => {
+const BulkMatchOption = ({selectedRights = []}) => {
 
     const checkSelectedRights = () => {
-        if(selectedRights.length <= 0){
+        if (selectedRights.length <= 0) {
             return false;
         }
+
         // all rights have coreTitleId empty and same contentType
         const emptyCoreTitle = selectedRights.every(r => r.coreTitleId === null && r.contentType === selectedRights[0].contentType);
-        if(!emptyCoreTitle){
+        if (!emptyCoreTitle) {
             return false;
         }
+
         // all rights have sourceRightId empty
         // OR all rights have sourceRightId NOT empty and all sourceRightIds are unique within selected rights
         const emptySourceRightId = selectedRights.every(r => r['sourceRightId'] === null);
         const notEmptySourceRightId = selectedRights.every(r => r['sourceRightId'] !== null);
         const sourceRightIds = selectedRights.map(r => r['sourceRightId']);
-        if(emptySourceRightId || (notEmptySourceRightId && uniq(sourceRightIds).length === selectedRights.length)){
-            return true;
-        }
-        return false;
+
+        return emptySourceRightId || (notEmptySourceRightId && uniq(sourceRightIds).length === selectedRights.length);
     };
 
     const addSpan = () => {
         return (
             <span
-                className={`
-                        nx-container-margin
-                        table-top-text
-                        nexus-c-bulk-match-view
-                        ${checkSelectedRights() ? 'active-link' :''}
-                    `}
+                className={classNames(
+                    'nx-container-margin',
+                    'table-top-text',
+                    'nexus-c-bulk-match-option',
+                    checkSelectedRights() && 'active-link'
+                )}
             >
                 {LABEL}
             </span>
-);
+        );
     };
 
     return (
@@ -49,9 +50,8 @@ const BulkMatchView = ({selectedRights}) => {
     );
 };
 
-BulkMatchView.propTypes = {
+BulkMatchOption.propTypes = {
     selectedRights: PropTypes.array.isRequired,
 };
 
-
-export default BulkMatchView;
+export default BulkMatchOption;
--- src/pages/avails/bulk-match/BulkMatchOption.test.js
+++ src/pages/avails/bulk-match/BulkMatchOption.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {shallow} from 'enzyme';
-import BulkMatchView from './BulkMatchView';
+import BulkMatchOption from './BulkMatchOption';
 import NexusTooltip from '../../../ui/elements/nexus-tooltip/NexusTooltip';
 import {WARNING_MSG} from './bulkMatchConstants';
 
@@ -22,7 +22,7 @@ describe('EventManagementTable', () => {
     ];
 
     beforeEach(() => {
-        wrapper = shallow(<BulkMatchView selectedRights={selectedRights} />);
+        wrapper = shallow(<BulkMatchOption selectedRights={selectedRights} />);
     });
 
     it('should match snapshot', () => {
@@ -33,40 +33,40 @@ describe('EventManagementTable', () => {
     it('should be active and not show warning message when coreTitleId and sourceRightId is null for all rights and contentType is the same for all rights', () => {
         selectedRights[0].sourceRightId = null;
         selectedRights[1].sourceRightId = null;
-        wrapper = shallow(<BulkMatchView selectedRights={selectedRights} />);
+        wrapper = shallow(<BulkMatchOption selectedRights={selectedRights} />);
         const tooltip = wrapper.find(NexusTooltip);
         expect(tooltip.props().content).toEqual('');
-        expect(wrapper.find('.nx-container-margin.table-top-text.nexus-c-bulk-match-view.active-link').length).toEqual(1);
+        expect(wrapper.find('.nx-container-margin.table-top-text.nexus-c-bulk-match-option.active-link').length).toEqual(1);
     });
 
     it('should be active and not show warning message when coreTitleId is null for all rights, contentType is the same for all rights and sourceRightId is unique for each right', () => {
         const tooltip = wrapper.find(NexusTooltip);
         expect(tooltip.props().content).toEqual('');
-        expect(wrapper.find('.nx-container-margin.table-top-text.nexus-c-bulk-match-view.active-link').length).toEqual(1);
+        expect(wrapper.find('.nx-container-margin.table-top-text.nexus-c-bulk-match-option.active-link').length).toEqual(1);
     });
 
     it('should be disabled and show warning message when sourceRightId is not unique for each selected right', () => {
         selectedRights[0].sourceRightId = 'rght_Ej7yr';
-        wrapper = shallow(<BulkMatchView selectedRights={selectedRights} />);
+        wrapper = shallow(<BulkMatchOption selectedRights={selectedRights} />);
         const tooltip = wrapper.find(NexusTooltip);
         expect(tooltip.props().content).toEqual(WARNING_MSG);
-        expect(wrapper.find('.nx-container-margin.table-top-text.nexus-c-bulk-match-view').length).toEqual(1);
+        expect(wrapper.find('.nx-container-margin.table-top-text.nexus-c-bulk-match-option').length).toEqual(1);
     });
 
     it('should be disabled and show warning message when contentType not the same for all selected right', () => {
         selectedRights[0].contentType = 'Episode';
-        wrapper = shallow(<BulkMatchView selectedRights={selectedRights} />);
+        wrapper = shallow(<BulkMatchOption selectedRights={selectedRights} />);
         const tooltip = wrapper.find(NexusTooltip);
         expect(tooltip.props().content).toEqual(WARNING_MSG);
-        expect(wrapper.find('.nx-container-margin.table-top-text.nexus-c-bulk-match-view').length).toEqual(1);
+        expect(wrapper.find('.nx-container-margin.table-top-text.nexus-c-bulk-match-option').length).toEqual(1);
     });
 
     it('should be disabled and show warning message when coreTitleId is not null for all selected right', () => {
         selectedRights[0].coreTitleId = 'Title1';
-        wrapper = shallow(<BulkMatchView selectedRights={selectedRights} />);
+        wrapper = shallow(<BulkMatchOption selectedRights={selectedRights} />);
         const tooltip = wrapper.find(NexusTooltip);
         expect(tooltip.props().content).toEqual(WARNING_MSG);
-        expect(wrapper.find('.nx-container-margin.table-top-text.nexus-c-bulk-match-view').length).toEqual(1);
+        expect(wrapper.find('.nx-container-margin.table-top-text.nexus-c-bulk-match-option').length).toEqual(1);
     });
 
 });
