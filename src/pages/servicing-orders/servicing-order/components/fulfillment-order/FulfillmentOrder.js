1b7d556b76ec6105eb9661934b5f8f870c5c0465
Brandon Chung <brandon.k.chung@gmail.com>
2020-06-17 14:50:05 -0500

    PORT-2240: minor fixes

--- src/pages/servicing-orders/servicing-order/components/fulfillment-order/FulfillmentOrder.js
+++ src/pages/servicing-orders/servicing-order/components/fulfillment-order/FulfillmentOrder.js
@@ -1,19 +1,20 @@
-import React, {useState, useEffect, useContext} from 'react';
-import {useSelector, useDispatch} from 'react-redux';
-import {get, set, isEqual, cloneDeep} from 'lodash';
-import Button, { ButtonGroup }from '@atlaskit/button';
-import './FulfillmentOrder.scss';
+import Button, {ButtonGroup} from '@atlaskit/button';
+import Page, {Grid, GridColumn} from '@atlaskit/page';
 import Select from '@atlaskit/select/dist/cjs/Select';
-import Page, { Grid, GridColumn } from '@atlaskit/page';
 import Textfield from '@atlaskit/textfield';
-import Constants from './constants';
+import {cloneDeep, get, isEqual, set} from 'lodash';
+import React, {useContext, useEffect, useState} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
 import NexusDatePicker from '../../../../../ui/elements/nexus-date-and-time-elements/nexus-date-picker/NexusDatePicker';
+import {NexusModalContext} from '../../../../../ui/elements/nexus-modal/NexusModal';
 import NexusTextArea from '../../../../../ui/elements/nexus-textarea/NexusTextArea';
 import {createLoadingSelector} from '../../../../../ui/loading/loadingSelectors';
 import {createSuccessMessageSelector} from '../../../../../ui/success/successSelector';
 import {getValidDate} from '../../../../../util/utils';
 import {saveFulfillmentOrder} from '../../servicingOrderActions';
-import {NexusModalContext} from '../../../../../ui/elements/nexus-modal/NexusModal';
+import {SAVE_FULFILLMENT_ORDER, SAVE_FULFILLMENT_ORDER_SUCCESS} from '../../servicingOrderActionTypes';
+import Constants from './constants';
+import './FulfillmentOrder.scss';
 
 export const FulfillmentOrder = ({selectedFulfillmentOrder = {}, fetchFulfillmentOrders, serviceOrder, children}) => {
     const {fieldKeys} = Constants;
@@ -29,7 +30,10 @@ export const FulfillmentOrder = ({selectedFulfillmentOrder = {}, fetchFulfillmen
 
     const ModalContent = (
         <>
-            <p>You are about to set a Fulfillment Order&apos;s readiness status to &quot;Ready&quot;. No further edits will be possible.</p>
+            <p>
+                You are about to set a Fulfillment Order&apos;s readiness status to &quot;Ready&quot;. No further edits
+                will be possible.
+            </p>
             <p>Do you wish to continue?</p>
         </>
     );
@@ -38,6 +42,9 @@ export const FulfillmentOrder = ({selectedFulfillmentOrder = {}, fetchFulfillmen
         width: 'small'
     };
 
+    // runs when a fulfillment order has been successfully edited and saved
+    // 1. re-fetches all the fulfillment orders
+    // 2. dispatches a success action with a value different than 'SUCCESS' so that this effect only runs once on a successful edit
     useEffect(
         () => {
             if (isSuccess && isSuccess !== 'ALREADY_SET') {
@@ -53,7 +60,6 @@ export const FulfillmentOrder = ({selectedFulfillmentOrder = {}, fetchFulfillmen
         [isSuccess]
     );
 
-
     useEffect(
         () => {
             setFulfillmentOrder(cloneDeep(savedFulfillmentOrder || selectedFulfillmentOrder));
@@ -61,38 +67,45 @@ export const FulfillmentOrder = ({selectedFulfillmentOrder = {}, fetchFulfillmen
         [selectedFulfillmentOrder, savedFulfillmentOrder]
     );
 
-    useEffect(() => {
-        setIsSaveDisabled(isEqual(fulfillmentOrder, savedFulfillmentOrder || selectedFulfillmentOrder));
-        get(fulfillmentOrder, fieldKeys.READINESS, '') === 'READY' ?  setIsFormDisabled(true) : setIsFormDisabled(false);
-    }, [fulfillmentOrder]);
+    useEffect(
+        () => {
+            setIsSaveDisabled(isEqual(fulfillmentOrder, savedFulfillmentOrder || selectedFulfillmentOrder));
+            get(fulfillmentOrder, fieldKeys.READINESS, '') === 'READY'
+                ? setIsFormDisabled(true)
+                : setIsFormDisabled(false);
+        },
+        [fulfillmentOrder]
+    );
 
     const onFieldChange = (path, value) => {
         const fo = cloneDeep(fulfillmentOrder);
         set(fo, path, value);
-        get(fo, fieldKeys.READINESS, '') === 'READY' ?
-            openWarningModal(fo) :
-            setFulfillmentOrder(fo);
+        get(fo, fieldKeys.READINESS, '') === 'READY' ? openWarningModal(fo) : setFulfillmentOrder(fo);
     };
 
-    const openWarningModal = (fo) => {
+    const openWarningModal = fo => {
         setModalContentAndTitle(ModalContent, modalHeading);
         setModalStyle(modalStyle);
-        setModalActions([{
-            text: 'Continue',
-            onClick: () => {
-                close();
-                setFulfillmentOrder(fo);
+        setModalActions([
+            {
+                text: 'Continue',
+                onClick: () => {
+                    close();
+                    setFulfillmentOrder(fo);
+                }
+            },
+            {
+                text: 'Cancel',
+                onClick: () => {
+                    close();
+                }
             }
-        },
-        {
-            text: 'Cancel',
-            onClick: () => {
-                close();
-            }
-        }]);
+        ]);
     };
 
-    const readinessOption = fulfillmentOrder ? Constants.READINESS_STATUS.find(l => l.value === fulfillmentOrder[fieldKeys.READINESS]) : {};
+    const readinessOption = fulfillmentOrder
+        ? Constants.READINESS_STATUS.find(l => l.value === fulfillmentOrder[fieldKeys.READINESS])
+        : {};
 
     const {setModalContentAndTitle, setModalActions, setModalStyle, close} = useContext(NexusModalContext);
 
@@ -109,9 +122,6 @@ export const FulfillmentOrder = ({selectedFulfillmentOrder = {}, fetchFulfillmen
         };
         const payload = {data: transformClientToServerFulfillmentOrder(fulfillmentOrder)};
         dispatch(saveFulfillmentOrder(payload));
-
-        //refetch to keep UI consistent with database
-        // fetchFulfillmentOrders(serviceOrder);
     };
 
     return (
@@ -148,8 +158,8 @@ export const FulfillmentOrder = ({selectedFulfillmentOrder = {}, fetchFulfillmen
                                 <label htmlFor="notes">Notes:</label>
                                 <NexusTextArea
                                     name="notes"
-                                    onTextChange={e => onFieldChange(fieldKeys.NOTE, e.target)}
-                                    notesValue={get(fulfillmentOrder, fieldKeys.NOTE, '')}
+                                    onTextChange={e => onFieldChange(fieldKeys.NOTES, e.target.value)}
+                                    notesValue={get(fulfillmentOrder, fieldKeys.NOTES, '')}
                                     isDisabled={isFormDisabled}
                                 />
                             </GridColumn>
@@ -191,7 +201,10 @@ export const FulfillmentOrder = ({selectedFulfillmentOrder = {}, fetchFulfillmen
                                         name="readiness-status"
                                         className="fulfillment-order__readiness-status"
                                         options={Constants.READINESS_STATUS}
-                                        value={{value: get(fulfillmentOrder, fieldKeys.READINESS, ''), label: readinessOption && readinessOption.label}}
+                                        value={{
+                                            value: get(fulfillmentOrder, fieldKeys.READINESS, ''),
+                                            label: readinessOption && readinessOption.label
+                                        }}
                                         onChange={val => onFieldChange(fieldKeys.READINESS, val.value)}
                                         isDisabled={isFormDisabled}
                                     />
