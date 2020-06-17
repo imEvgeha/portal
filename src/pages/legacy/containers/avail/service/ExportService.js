cc5f0bb9211e897470caa7ce5a4450b48e6e6b89
Aaron Rowe-Jardine <ajardine@juiceworldwide.com>
2020-06-17 10:52:12 -0400

    PORT-2366: Filter avails mapping before export

--- src/pages/legacy/containers/avail/service/ExportService.js
+++ src/pages/legacy/containers/avail/service/ExportService.js
@@ -3,35 +3,11 @@ import {nexusFetch} from '../../../../../util/http-client/index';
 import {parseAdvancedFilter} from './RightsService';
 import {prepareSortMatrixParam, encodedSerialize} from '../../../../../util/Common';
 
-const filterColumnNames = (cols) => {
-    const columns = [...cols];
-
-    const index1 = columns.indexOf('languageAudioTypes.language');
-    const index2 = columns.indexOf('languageAudioTypes.audioType');
-
-    const indexMin = Math.min(index1, index2);
-    const indexMax = Math.max(index1, index2);
-
-    if(indexMax > -1){
-        //if at least one found
-        if(indexMin > -1){
-            //if both found
-            columns.splice(indexMin, 1, 'languageAudioTypes'); //replace first
-            columns.splice(indexMax, 1); //and delete second
-        }else{
-            //if only one found
-            columns.splice(indexMax, 1, 'languageAudioTypes'); //replace it
-        }
-    }
-
-    return columns.filter((col) => { return !col.includes('territory.') });
-};
-
 export const exportService = {
     exportAvails: (rightsIDs, columns) => {
         const abortAfter = config.get('avails.export.http.timeout');
         const url = config.get('gateway.url') + config.get('gateway.service.avails') + '/avails/export';
-        const data = {columnNames: filterColumnNames(columns), rightIds: rightsIDs};
+        const data = {columnNames: columns, rightIds: rightsIDs};
 
         return nexusFetch(url, {
             method: 'post',
@@ -43,7 +19,7 @@ export const exportService = {
         const params = parseAdvancedFilter(searchCriteria);
         const url = config.get('gateway.url') + config.get('gateway.service.avails') + '/avails/export/bulk' + prepareSortMatrixParam(sortedParams);
         const abortAfter = config.get('avails.export.http.timeout');
-        const data = {columnNames: filterColumnNames(columns)};
+        const data = {columnNames: columns};
 
         return nexusFetch(url, {
             method: 'post',
