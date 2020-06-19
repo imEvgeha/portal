2cec9ff0148247a674d59dc9f7ffa2976df9f6cd
Demetris Savva <Demetris.Savva@amdocs.com>
2020-06-19 15:09:58 +0300

    Format boolean values in rights v2 table

--- src/ui/elements/nexus-grid/hoc/withFilterableColumns.js
+++ src/ui/elements/nexus-grid/hoc/withFilterableColumns.js
@@ -175,7 +175,7 @@ const withFilterableColumns = ({
                                 };
                                 break;
                             case BOOLEAN:
-                                columnDef.filter = TEXT;
+                                columnDef.filter = SET;
                                 columnDef.filterParams = {
                                     ...DEFAULT_FILTER_PARAMS,
                                     values: [false, true]
