2fddb8da2a63aefcac57374327bf5fc40eb01632
Ivan Stankovic <ivan.stankovic@webstorm.rs>
2020-06-22 17:41:47 +0200

    PORT-2200 add service & mock data; add tests

--- src/pages/sync-log/components/sync-log-table/SyncLogTable.js
+++ src/pages/sync-log/components/sync-log-table/SyncLogTable.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import {compose} from 'redux';
 import Button from '@atlaskit/button';
-// import {getSyncLog} from '../../syncLogService';
+import {getSyncLog} from '../../syncLogService';
 import NexusGrid from '../../../../ui/elements/nexus-grid/NexusGrid';
-// import withInfiniteScrolling from '../../../../ui/elements/nexus-grid/hoc/withInfiniteScrolling';
-import withSorting from '../../../../ui/elements/nexus-grid/hoc/withSorting';
+import withInfiniteScrolling from '../../../../ui/elements/nexus-grid/hoc/withInfiniteScrolling';
 import withColumnsResizing from '../../../../ui/elements/nexus-grid/hoc/withColumnsResizing';
 import createValueFormatter from '../../../../ui/elements/nexus-grid/elements/value-formatter/createValueFormatter';
-import columnDefs from '../../columnMappings.json';
-import {INITIAL_SORT, DOWNLOAD_BTN} from '../../syncLogConstants';
+import columnMappings from '../../columnMappings.json';
+import {DOWNLOAD_BTN} from '../../syncLogConstants';
 import './SyncLogTable.scss';
 
 const SyncLogGrid = compose(
     withColumnsResizing(),
-    withSorting(INITIAL_SORT),
-    // withInfiniteScrolling({fetchData: getSyncLog})
+    withInfiniteScrolling({fetchData: getSyncLog})
 )(NexusGrid);
 
 const SyncLogTable = () => {
@@ -38,9 +36,9 @@ const SyncLogTable = () => {
             </Button>
             <SyncLogGrid
                 className="nexus-c-sync-log-grid"
-                columnDefs={updateColumnDefs(columnDefs)}
+                columnDefs={updateColumnDefs(columnMappings)}
+                mapping={columnMappings}
                 rowSelection="single"
-                mapping={columnDefs}
             />
         </div>
     );
--- src/pages/sync-log/syncLogConstants.js
+++ src/pages/sync-log/syncLogConstants.js
@@ -1,4 +1,2 @@
 export const TITLE = 'Sync Log';
 export const DOWNLOAD_BTN = 'Download';
-
-export const INITIAL_SORT = {colId: 'updatedAt', sort: 'desc'};
