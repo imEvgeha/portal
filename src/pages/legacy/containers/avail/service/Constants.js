a4c4a813c745d5211a78e1168e0f0e5555094983
Adrian Buse <adrian.buse2@vubiquity.co.uk>
2020-06-23 09:52:42 +0200

    Revert "PORT-2032 activate the new type"
    This reverts commit 35b6277482beaf4a3e848ae1e651effd0f835f19.

--- src/pages/legacy/containers/avail/service/Constants.js
+++ src/pages/legacy/containers/avail/service/Constants.js
@@ -6,9 +6,9 @@ const STRING_TO_ARRAY_OF_STRINGS_HACKED_FIELDS = [
     'keywords'
 ];
 const MULTI_INSTANCE_OBJECTS_IN_ARRAY_HACKED_FIELDS = [];
-const ARRAY_OF_OBJECTS = ['pricing', 'territory', 'languageAudioTypes'];
-const NULL_TO_ARRAY = ['pricing', 'territory', 'castCrew', 'languageAudioTypes'];
-const NULL_TO_OBJECT = ['rating', 'episodic'];
+const ARRAY_OF_OBJECTS = ['territory', 'languageAudioTypes'];
+const NULL_TO_ARRAY = ['territory', 'castCrew', 'languageAudioTypes'];
+const NULL_TO_OBJECT = ['rating', 'episodic', 'pricing'];
 export {
     STRING_TO_ARRAY_OF_STRINGS_HACKED_FIELDS, ARRAY_OF_OBJECTS, MULTI_INSTANCE_OBJECTS_IN_ARRAY_HACKED_FIELDS, NULL_TO_ARRAY, NULL_TO_OBJECT
 };
