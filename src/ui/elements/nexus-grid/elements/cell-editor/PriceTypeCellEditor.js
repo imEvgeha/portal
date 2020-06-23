47529b7175daf1bbbb2d6ca9bac48b45f75d361f
Adrian Buse <adrian.buse2@vubiquity.co.uk>
2020-06-23 09:51:43 +0200

    PORT-2032 add Price Inline Edit

---
+++ src/ui/elements/nexus-grid/elements/cell-editor/PriceTypeCellEditor.js
@@ -0,0 +1,72 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {NexusModalContext} from '../../../nexus-modal/NexusModal';
+import PriceTypeFormSchema from '../../../../../pages/legacy/components/form/PriceTypeFormSchema';
+import NexusMultiInstanceField from '../../../nexus-multi-instance-field/NexusMultiInstanceField';
+import './MultiInstanceCellEditor.scss';
+
+class PriceTypeCellEditor extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: props.value,
+        };
+    }
+
+    isPopup = () => true;
+
+    getValue = () => this.state.value;
+
+    handleChange = (value) => {
+        const addedPriceTypes = value
+            .map((price) => price.pricing || price)
+            .filter(Boolean);
+        this.setState({value: addedPriceTypes});
+    };
+
+    getOptions = () => {
+        const {options = {}} = this.props;
+        const {priceTypes, currencies} = options || {};
+
+        return {priceTypes, currencies};
+    };
+
+    render() {
+        const {value} = this.state;
+
+        return (
+            <div className="nexus-c-multi-instance-cell-editor">
+                <NexusMultiInstanceField
+                    existingItems={value}
+                    onSubmit={this.handleChange}
+                    schema={PriceTypeFormSchema(this.getOptions())}
+                    keyForTagLabel="priceType"
+                    isUsingModal={false}
+                    specialCreate={true}
+                />
+            </div>
+        );
+    }
+}
+
+PriceTypeCellEditor.propTypes = {
+    options: PropTypes.shape({
+        priceTypes: PropTypes.array,
+        currencies: PropTypes.array,
+    }),
+    value: PropTypes.array,
+};
+
+PriceTypeCellEditor.defaultProps = {
+    options: {
+        priceTypes: [],
+        currencies: [],
+    },
+    value: null
+};
+
+PriceTypeCellEditor.contextType = NexusModalContext;
+
+export default PriceTypeCellEditor;
+
