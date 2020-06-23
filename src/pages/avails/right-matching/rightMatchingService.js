91e0ab430ae68caf1fda866c2d35502a544e108e
dradojkovic <djordje.radojkovic@computerrock.com>
2020-06-23 13:17:28 +0200

    PORT-2530: Remove extra whitespace

--- src/pages/avails/right-matching/rightMatchingService.js
+++ src/pages/avails/right-matching/rightMatchingService.js
@@ -134,8 +134,6 @@ export const getRightMatchingFieldSearchCriteria = (payload) => {
                 return query;
             }, {});
 
-
-
         return {
             fieldSearchCriteria: {
                 id,
