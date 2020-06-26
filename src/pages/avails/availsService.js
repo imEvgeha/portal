e046cfeaba025226588b914af589edb419507ba6
dradojkovic <djordje.radojkovic@computerrock.com>
2020-06-26 15:15:56 +0200

    PORT-2369: Modify availsService to include /rights/coreTitleId api

--- src/pages/avails/availsService.js
+++ src/pages/avails/availsService.js
@@ -1,7 +1,14 @@
 import config from 'react-global-configuration';
 import {nexusFetch} from '../../util/http-client';
 
+const baseUrl = `${config.get('gateway.url')}${config.get('gateway.service.avails')}`;
+
 export const getAffectedRights = params => {
-    const url = `${config.get('gateway.url')}${config.get('gateway.service.avails')}/rights/impacted?rightIds=${params}`;
+    const url = `${baseUrl}/rights/impacted?rightIds=${params}`;
     return nexusFetch(url);
+};
+
+export const setCoreTitleId = ({rightIds, coreTitleId}) => {
+    const url = `${baseUrl}/rights/coreTitleId?coreTitleId=${coreTitleId}&rightIds=${rightIds}`;
+    return nexusFetch(url, {method: 'PATCH'});
 };
