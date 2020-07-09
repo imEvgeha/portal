d65f7b1c6b9297c7871567953cd77f74b48cc54c
Brandon Chung <brandon.k.chung@gmail.com>
2020-07-09 18:46:12 -0500

    PORT-2672: add updates to partner request table

--- src/pages/servicing-orders/servicing-order/components/filter-section/ServiceOrderFilter.js
+++ src/pages/servicing-orders/servicing-order/components/filter-section/ServiceOrderFilter.js
@@ -1,13 +1,18 @@
 import Button from '@atlaskit/button';
+import styled from 'styled-components';
 import Select from '@atlaskit/select';
 import PropTypes from 'prop-types';
 import React, {useState} from 'react';
 import NexusDatePicker from '../../../../../ui/elements/nexus-date-and-time-elements/nexus-date-picker/NexusDatePicker';
 import NexusDrawer from '../../../../../ui/elements/nexus-drawer/NexusDrawer';
+import Heading from '../../../../../ui/atlaskit/heading/Heading';
+import {gridSize} from '@atlaskit/theme';
 import {getValidDate} from '../../../../../util/utils';
 import PartnerRequest from '../partner-request/PartnerRequest';
 import {SORT_DIRECTION} from './constants';
 import Constants from '../fulfillment-order/constants';
+import Drawer from '@atlaskit/drawer';
+import Page, {Grid, GridColumn} from '@atlaskit/page';
 import './ServiceOrderFilter.scss';
 
 const ServiceOrderFilter = ({
@@ -15,15 +20,17 @@ const ServiceOrderFilter = ({
     filter,
     setFilter,
     dueDateSortDirection,
-    setDueDateSortDirection
+    setDueDateSortDirection,
 }) => {
     const [isDrawerOpen, setIsDrawerOpen] = useState(false);
-    const { tenant, external_id, description, configured_pr_id, sr_due_date } = orderDetails || {};
-    let FilterList = [{ value: 'All', label: 'All' }];
-    FilterList = FilterList.concat(Object.keys(Constants.STATUS).map(key => ({
-        label: Constants.STATUS[key],
-        value: key
-    })));
+    const {tenant, external_id, description, configured_pr_id, sr_due_date} = orderDetails || {};
+    let FilterList = [{value: 'All', label: 'All'}];
+    FilterList = FilterList.concat(
+        Object.keys(Constants.STATUS).map(key => ({
+            label: Constants.STATUS[key],
+            value: key,
+        }))
+    );
     return (
         <div className="so-panel-filter-detail">
             <div className="so-panel-filter-detail__row">
@@ -45,17 +52,21 @@ const ServiceOrderFilter = ({
             )}
             <div className="so-panel-filter-detail__row">
                 <Button onClick={() => setIsDrawerOpen(true)}>Partner Request</Button>
-                <NexusDrawer
+                <Drawer
+                    isOpen={isDrawerOpen}
+                    onClose={() => setIsDrawerOpen(false)}
+                    width="extended"
+                >
+                    <PartnerRequest externalId={external_id} configuredPrId={configured_pr_id} />
+                </Drawer>
+                {/* <NexusDrawer
                     isOpen={isDrawerOpen}
                     onClose={() => setIsDrawerOpen(false)}
                     width="extended"
                     title="Partner Request"
                 >
-                    <PartnerRequest
-                        externalId={external_id}
-                        configuredPrId={configured_pr_id}
-                    />
-                </NexusDrawer>
+                    <PartnerRequest externalId={external_id} configuredPrId={configured_pr_id} />
+                </NexusDrawer> */}
             </div>
 
             <div className="so-panel-filter-detail__row">
@@ -77,7 +88,6 @@ const ServiceOrderFilter = ({
                         value={filter}
                         placeholder="Select Status"
                     />
-
                 </div>
                 <div className="so-panel-filter-detail__dropdown">
                     <label>Sort by Due Date</label>
@@ -87,7 +97,6 @@ const ServiceOrderFilter = ({
                         value={dueDateSortDirection}
                         placeholder="Select Date"
                     />
-
                 </div>
             </div>
         </div>
@@ -95,7 +104,7 @@ const ServiceOrderFilter = ({
 };
 
 ServiceOrderFilter.propTypes = {
-    orderDetails: PropTypes.object.isRequired
+    orderDetails: PropTypes.object.isRequired,
 };
 
 export default ServiceOrderFilter;
--- src/pages/servicing-orders/servicing-order/components/partner-request/PartnerRequest.js
+++ src/pages/servicing-orders/servicing-order/components/partner-request/PartnerRequest.js
@@ -1,67 +1,175 @@
-import React, {useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
+import React, {useEffect, useState} from 'react';
 import {uid} from 'react-uid';
-import {getServiceRequest} from '../../../servicingOrdersService';
+import styled from 'styled-components';
 import {parseSimulcast} from '../../../../../util/date-time/DateTimeUtils';
+import {getServiceRequest} from '../../../servicingOrdersService';
 import {
-    COLUMN_KEYS,
     COLUMNS,
+    COLUMN_KEYS,
     CREATED_BY,
     CREATED_DATE,
     DATE_FORMAT,
     MSS_ORDER_DETAILS,
-    STUDIO
+    STUDIO,
 } from '../filter-section/constants';
-import './PartnerRequest.scss';
+import Heading from '../../../../../ui/atlaskit/heading/Heading';
+import DynamicTable from '@atlaskit/dynamic-table';
+import {typography, gridSize} from '@atlaskit/theme';
+import Page, {Grid, GridColumn} from '@atlaskit/page';
 
 const PartnerRequest = ({externalId, configuredPrId}) => {
-    const [data, setData] = useState({list:[]});
-
+    const [data, setData] = useState({list: []});
+    const [loading, setLoading] = useState(true);
     useEffect(() => {
         getServiceRequest(externalId).then(res => {
             const partnerRequest = res.filter(req => req.id === configuredPrId)[0];
             const {tenant, createdBy, createdAt, definition} = partnerRequest;
             setData({
                 list: JSON.parse(definition).materials,
-                tenant, createdBy, createdAt
+                tenant,
+                createdBy,
+                createdAt,
             });
+            setLoading(false);
         });
     }, []);
 
+    const columnDefs = {
+        cells: COLUMNS.map((col, index) => {
+            const baseColumnDef = {
+                key: COLUMN_KEYS[index],
+                content: col,
+                isSortable: false,
+                width: 7,
+            };
+
+            if (col === 'Notes') {
+                return {...baseColumnDef, width: 30};
+            }
+
+            return baseColumnDef;
+        }),
+    };
+
+    const rows = [
+        ...data.list,
+        ...['', '', '', '', ''].map((val, index) => ({
+            amount: '',
+            artWork: '',
+            closedCaptions: null,
+            document: '',
+            embargoDate: null,
+            eopdueDate: '',
+            eopnotes: '',
+            eoppo: '',
+            eopresource: '',
+            eopstatus: 'Not Required',
+            id: uid(index) + index,
+            materialNotes: '',
+            metaData: '',
+            other: 'string',
+            ppsdueDate: '04/07/2021',
+            ppsnotes: '',
+            ppspo: '4865863',
+            ppsresource: 'DSOTO',
+            ppsstatus: 'PPS Assigned',
+            primaryVideo: 1 + index,
+            productDesc: '',
+            productID: '17' + index,
+            rebillType: '',
+            secondaryAudio: null,
+            srdueDate: `04/10/202${index}`,
+            subtitlesForced: null,
+            subtitlesFull: null,
+            trailer: '',
+            vendorAssetGroupID: 'DA16129',
+            vendorSpecID: 'E-ERIC-HD2',
+            version: 'TheatricalVersion',
+        })),
+    ].map(order => ({
+        // the key for each row
+        key: order.id,
+
+        // the cells in each row
+        cells: COLUMN_KEYS.map((key, index) => ({
+            key: uid(key, index),
+            content:
+                key === 'materialNotes' ? (
+                    <WrapperWithMaxHeight maxHeight="14">
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem
+                            sit provident doloremque dolores suscipit deleniti laudantium,
+                            repudiandae vel, blanditiis adipisci earum corrupti numquam, sint veniam
+                            incidunt delectus dolor. Quos, ullam? Lorem ipsum dolor sit amet
+                            consectetur adipisicing elit. Tempora adipisci, vero officia quaerat in
+                            at provident ratione tempore iste amet asperiores? Laborum pariatur
+                            optio necessitatibus inventore quae, ipsum a voluptate.
+                        </p>
+                    </WrapperWithMaxHeight>
+                ) : (
+                    order[key]
+                ),
+        })),
+    }));
+
+    const emptyTableText = () => (
+        <Heading size="h400" as="h3">
+            There are no partner requests.
+        </Heading>
+    );
+
     return (
-        <div className="nexus-c-partner-request">
-            <div className="nexus-c-partner-request__info">
-                <div className="nexus-c-partner-request__info-field">
-                    {STUDIO}: {data.tenant}
-                </div>
-                <div className="nexus-c-partner-request__info-field">
-                    {MSS_ORDER_DETAILS}: {externalId}
-                </div>
-                <div className="nexus-c-partner-request__info-field">
-                    {CREATED_DATE}: {data.createdAt ? parseSimulcast(data.createdAt, DATE_FORMAT): ''}
-                </div>
-                <div className="nexus-c-partner-request__info-field">
-                    {CREATED_BY}: {(data.createdBy || '')}
-                </div>
-            </div>
-            <div className="nexus-c-partner-request__table">
-                {COLUMNS.map((columnHeader, index) => (
-                    <div key={index} className="nexus-c-partner-request__table-column-header">
-                        {columnHeader.toUpperCase()}
-                    </div>
-                ))}
-                {data.list.map((order) => (
-                    COLUMN_KEYS.map((key, index) => (
-                        <div
-                            key={uid(key, index)}
-                            className="nexus-c-partner-request__table-cell"
-                        >
-                            {order[key]}
-                        </div>
-                    ))
-                ))}
-            </div>
-        </div>
+        <PartnerRequestWrapper>
+            <Page>
+                <Heading size="h900" as="h1" style={{marginBottom: `${gridSize() * 4}px`}}>
+                    Partner Request
+                </Heading>
+                <Grid layout="fluid" spacing="compact">
+                    <GridColumn medium={2}>
+                        <PartnerRequestInfoSection>
+                            <Heading size="h300" as="h2">
+                                {STUDIO}
+                            </Heading>
+                            <ParterRequestInfoField>{data.tenant}</ParterRequestInfoField>
+                        </PartnerRequestInfoSection>
+                        <PartnerRequestInfoSection>
+                            <Heading size="h300" as="h2">
+                                {MSS_ORDER_DETAILS}
+                            </Heading>
+                            <ParterRequestInfoField>{externalId}</ParterRequestInfoField>
+                        </PartnerRequestInfoSection>
+                    </GridColumn>
+                    <GridColumn>
+                        <PartnerRequestInfoSection>
+                            <Heading size="h300" as="h2">
+                                {CREATED_DATE}
+                            </Heading>
+                            <ParterRequestInfoField>
+                                {data.createdAt
+                                    ? parseSimulcast(data.createdAt, DATE_FORMAT)
+                                    : 'N/A'}
+                            </ParterRequestInfoField>
+                        </PartnerRequestInfoSection>
+                        <PartnerRequestInfoSection>
+                            <Heading size="h300" as="h2">
+                                {CREATED_BY}
+                            </Heading>
+                            <ParterRequestInfoField>
+                                {data.createdBy || 'N/A'}
+                            </ParterRequestInfoField>
+                        </PartnerRequestInfoSection>
+                    </GridColumn>
+                </Grid>
+                <DynamicTable
+                    isLoading={loading}
+                    head={columnDefs}
+                    rows={rows}
+                    isFixedWidth
+                    emptyView={emptyTableText()}
+                />
+            </Page>
+        </PartnerRequestWrapper>
     );
 };
 
@@ -72,7 +180,25 @@ PartnerRequest.propTypes = {
 
 PartnerRequest.defaultProps = {
     externalId: '',
-    configuredPrId: ''
+    configuredPrId: '',
 };
 
 export default PartnerRequest;
+
+const WrapperWithMaxHeight = styled.div`
+    max-height: ${props => props.maxHeight * gridSize()}px;
+    overflow-y: scroll;
+`;
+
+const PartnerRequestWrapper = styled.div`
+    padding-left: ${gridSize() * 2}px;
+    padding-right: ${gridSize() * 2}px;
+`;
+
+const PartnerRequestInfoSection = styled.div`
+    margin-bottom: ${gridSize() * 3}px;
+`;
+
+const ParterRequestInfoField = styled.p`
+    margin-top: ${gridSize() / 2}px;
+`;
