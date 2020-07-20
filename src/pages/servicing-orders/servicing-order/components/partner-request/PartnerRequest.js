700974f1848611297434acf442b8aa4758051efa
Brandon Chung <brandon.k.chung@gmail.com>
2020-07-20 14:25:25 -0500

    PORT-2672: remove styled-components from partner-request

--- src/pages/servicing-orders/servicing-order/components/partner-request/PartnerRequest.js
+++ src/pages/servicing-orders/servicing-order/components/partner-request/PartnerRequest.js
@@ -16,6 +16,7 @@ import {
     STUDIO,
 } from '../filter-section/constants';
 import {columnDefs, defaultColDef} from './PartnerRequestColumnDefinitions';
+import './PartnerRequest.scss';
 
 const PartnerRequest = ({externalId, configuredPrId}) => {
     const [data, setData] = useState({list: []});
@@ -67,48 +68,48 @@ const PartnerRequest = ({externalId, configuredPrId}) => {
     };
 
     return (
-        <PartnerRequestWrapper>
+        <div className="nexus-c-partner-request">
             <Page>
                 <Grid layout="fluid">
                     <GridColumn>
                         <Grid layout="fluid">
                             <GridColumn medium={2}>
-                                <InfoSection>
+                                <div className="nexus-c-partner-request__info-section">
                                     <h6>{STUDIO}</h6>
-                                    <InfoField>{data.tenant || 'N/A'}</InfoField>
-                                </InfoSection>
-                                <InfoSection>
+                                    <p className="nexus-c-partner-request__info-field">{data.tenant || 'N/A'}</p>
+                                </div>
+                                <div className="nexus-c-partner-request__info-section">
                                     <h6>{MSS_ORDER_DETAILS}</h6>
-                                    <InfoField>{externalId || 'N/A'}</InfoField>
-                                </InfoSection>
+                                    <p className="nexus-c-partner-request__info-field">{externalId || 'N/A'}</p>
+                                </div>
                             </GridColumn>
                             <GridColumn>
-                                <InfoSection>
+                                <div className="nexus-c-partner-request__info-section">
                                     <h6>{CREATED_DATE}</h6>
-                                    <InfoField>
+                                    <p className="nexus-c-partner-request__info-field">
                                         {data.createdAt
                                             ? parseSimulcast(data.createdAt, DATE_FORMAT)
                                             : 'N/A'}
-                                    </InfoField>
-                                </InfoSection>
-                                <InfoSection>
+                                    </p>
+                                </div>
+                                <div className="nexus-c-partner-request__info-section">
                                     <h6>{CREATED_BY}</h6>
-                                    <InfoField>{data.createdBy || 'N/A'}</InfoField>
-                                </InfoSection>
+                                    <p className="nexus-c-partner-request__info-field">{data.createdBy || 'N/A'}</p>
+                                </div>
                             </GridColumn>
                         </Grid>
                     </GridColumn>
                 </Grid>
-                <TableWrapper>
+                <div className="nexus-c-partner-request__table-wrapper">
                     <NexusGrid
                         defaultColDef={defaultColDef}
                         columnDefs={columnDefs}
                         rowData={rowData}
                         onFirstDataRendered={onFirstDataRendered}
                     />
-                </TableWrapper>
+                </div>
             </Page>
-        </PartnerRequestWrapper>
+        </div>
     );
 };
 
@@ -123,35 +124,3 @@ PartnerRequest.defaultProps = {
 };
 
 export default PartnerRequest;
-
-const PartnerRequestWrapper = styled.div`
-    padding-left: ${gridSize() * 2}px;
-    padding-right: ${gridSize() * 2}px;
-    height: 92%;
-`;
-
-const InfoSection = styled.div`
-    margin-top: ${gridSize() * 2}px;
-    margin-bottom: ${gridSize() * 2}px;
-`;
-
-const InfoField = styled.p`
-    margin-top: ${gridSize() / 2}px;
-`;
-
-const TableWrapper = styled.div`
-    margin-top: ${gridSize() * 2}px;
-    height: 80%;
-
-    /* the nexus grid container div */
-    & > div {
-        height: 100%;
-    }
-
-    .cell {
-        white-space: normal !important;
-        max-height: ${gridSize() * 20}px;
-        overflow-y: auto !important;
-        line-height: 1.618;
-    }
-`;
