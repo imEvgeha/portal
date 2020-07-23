4ecc21f05489f17908f395e01c10819947cd3b99
Madhurima Kandul <madhkand@amdocs.com>
2020-07-23 16:12:15 +0300

    Add base URL for set core title ID API

--- src/pages/avails/availsService.js
+++ src/pages/avails/availsService.js
@@ -19,6 +19,6 @@ export const getRestrictedTitles = params => {
 // Sets Core title ID of each listed right to a given coreTitleId (bulk match)
 // If coreTitleId is omitted, it will remove coreTitleId for each right (bulk unmatch)
 export const setCoreTitleId = ({rightIds, coreTitleId = ''}) => {
-    const url = `${baseUrl}/rights/coreTitleId?coreTitleId=${coreTitleId}&rightIds=${rightIds}`;
+    const url = `${config.get('gateway.url')}${config.get('gateway.service.avails')}/rights/coreTitleId?coreTitleId=${coreTitleId}&rightIds=${rightIds}`;
     return nexusFetch(url, {method: 'PATCH'});
 };
