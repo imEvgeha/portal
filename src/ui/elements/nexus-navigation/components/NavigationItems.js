5a11783165268f8acd358d2797fde3389ddcb39c
Brandon Chung <brandon.k.chung@gmail.com>
2020-07-28 13:53:04 -0500

    PORT-2878: add permissions to event management button to hide if there is no read permission

--- src/ui/elements/nexus-navigation/components/NavigationItems.js
+++ src/ui/elements/nexus-navigation/components/NavigationItems.js
@@ -4,55 +4,63 @@ import EditorMediaWrapLeftIcon from '@atlaskit/icon/glyph/editor/media-wrap-left
 import EditorSearchIcon from '@atlaskit/icon/glyph/editor/search';
 import RecentIcon from '@atlaskit/icon/glyph/recent';
 import TrayIcon from '@atlaskit/icon/glyph/tray';
+import {can} from '../../../../ability';
 import NexusNavIcon from '../../../../assets/nexus-nav-icon.svg';
 import {AVAILS, METADATA, MEDIA, SERVICING_ORDERS, EVENT_MANAGEMENT} from '../constants';
 
-export const navigationPrimaryItems = (selectedItem, handleClick) => [
-    {
-        component: () => <NexusNavIcon />,
-        id: 'logo',
-    },
-    {
-        icon: TrayIcon,
-        id: AVAILS,
-        tooltip: AVAILS,
-        isSelected: (selectedItem === AVAILS),
-        onClick: () => handleClick(AVAILS),
-    },
-    {
-        icon: EditorMediaWrapLeftIcon,
-        id: METADATA,
-        tooltip: METADATA,
-        isSelected: (selectedItem === METADATA),
-        onClick: () => handleClick(METADATA),
-    },
-    {
-        icon: EditorSearchIcon,
-        id: MEDIA,
-        tooltip: MEDIA,
-        isSelected: (selectedItem === MEDIA),
-        onClick: () => handleClick(MEDIA),
-    },
-    {
-        icon: TrayIcon,
-        id: `${AVAILS}/v2`,
-        tooltip: `${AVAILS}/v2`,
-        isSelected: (selectedItem === `${AVAILS}/v2`),
-        onClick: () => handleClick(`${AVAILS}/v2`),
-    },
-    {
-        icon: DetailViewIcon,
-        id: SERVICING_ORDERS,
-        tooltip: 'Servicing Orders',
-        isSelected: (selectedItem === SERVICING_ORDERS),
-        onClick: () => handleClick(SERVICING_ORDERS),
-    },
-    {
-        icon: RecentIcon,
-        id: EVENT_MANAGEMENT,
-        tooltip: 'Event Management',
-        isSelected: (selectedItem === EVENT_MANAGEMENT),
-        onClick: () => handleClick(EVENT_MANAGEMENT),
-    },
-];
+export const navigationPrimaryItems = (selectedItem, handleClick) => {
+    const canReadEventManager = can('read', 'EventManagement');
 
+    return [
+        {
+            component: () => <NexusNavIcon />,
+            id: 'logo',
+        },
+        {
+            icon: TrayIcon,
+            id: AVAILS,
+            tooltip: AVAILS,
+            isSelected: selectedItem === AVAILS,
+            onClick: () => handleClick(AVAILS),
+        },
+        {
+            icon: EditorMediaWrapLeftIcon,
+            id: METADATA,
+            tooltip: METADATA,
+            isSelected: selectedItem === METADATA,
+            onClick: () => handleClick(METADATA),
+        },
+        {
+            icon: EditorSearchIcon,
+            id: MEDIA,
+            tooltip: MEDIA,
+            isSelected: selectedItem === MEDIA,
+            onClick: () => handleClick(MEDIA),
+        },
+        {
+            icon: TrayIcon,
+            id: `${AVAILS}/v2`,
+            tooltip: `${AVAILS}/v2`,
+            isSelected: selectedItem === `${AVAILS}/v2`,
+            onClick: () => handleClick(`${AVAILS}/v2`),
+        },
+        {
+            icon: DetailViewIcon,
+            id: SERVICING_ORDERS,
+            tooltip: 'Servicing Orders',
+            isSelected: selectedItem === SERVICING_ORDERS,
+            onClick: () => handleClick(SERVICING_ORDERS),
+        },
+        ...(canReadEventManager
+            ? [
+                  {
+                      icon: RecentIcon,
+                      id: EVENT_MANAGEMENT,
+                      tooltip: 'Event Management',
+                      isSelected: selectedItem === EVENT_MANAGEMENT,
+                      onClick: () => handleClick(EVENT_MANAGEMENT),
+                  },
+              ]
+            : []),
+    ];
+};
