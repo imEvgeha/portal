c17ebf8d9416f282efe6a3c3a1ecb67993096324
Madhurima Kandul <madhkand@amdocs.com>
2020-07-29 17:08:25 +0300

    PORT-2521: remove unnecessary true value to boolean prop

--- src/pages/avails/selected-for-planning/components/SelectedForPlanning/SelectedForPlanning.js
+++ src/pages/avails/selected-for-planning/components/SelectedForPlanning/SelectedForPlanning.js
@@ -6,10 +6,7 @@ import withColumnsResizing from '../../../../../ui/elements/nexus-grid/hoc/withC
 import withSideBar from '../../../../../ui/elements/nexus-grid/hoc/withSideBar';
 import {COLUMN_MAPPINGS, SELECTED_FOR_PLANNING_TAB} from '../../constants';
 
-const SelectedForPlanningTable = compose(
-    withColumnsResizing(),
-    withSideBar(),
-)(NexusGrid);
+const SelectedForPlanningTable = compose(withColumnsResizing(), withSideBar())(NexusGrid);
 
 const SelectedForPlanning = ({activeTab}) => {
     return (
@@ -17,7 +14,7 @@ const SelectedForPlanning = ({activeTab}) => {
             id="selectedForPlanningRepo"
             columnDefs={COLUMN_MAPPINGS}
             rowSelection="multiple"
-            suppressRowClickSelection={true}
+            suppressRowClickSelection
             isGridHidden={activeTab !== SELECTED_FOR_PLANNING_TAB}
         />
     );
