093154d39b83700c2c06459a32244f993bc397c0
Antonia Nikolaou <antonini@amdocs.com>
2020-07-30 15:52:11 +0300

    PORT-2815: Add tooltip in Manual rights page

--- src/pages/avails/ingest-panel/components/ingest-report/IngestReport.js
+++ src/pages/avails/ingest-panel/components/ingest-report/IngestReport.js
@@ -4,12 +4,14 @@ import classnames from 'classnames';
 import RightsURL from '../../../../legacy/containers/avail/util/RightsURL';
 import Constants from '../../constants';
 import './IngestReport.scss';
+import NexusTooltip from "../../../../../ui/elements/nexus-tooltip/NexusTooltip";
 
 const IngestReport = ({
     report,
     isShowingError = true,
     filterClick,
     ingestId,
+    showTooltips = false
 }) => {
     const [activeFilter, setActiveFilter] = useState('total');
     const reportFields = Constants.REPORT;
@@ -26,28 +28,39 @@ const IngestReport = ({
     }, [report, onFilterClick]);
 
     const FILTERABLE_KEYS = ['total', 'pending', 'errors'];
+    const ORIGINAL_VALUES_KEYS = ['pending', 'errors'];
+
+    const createTag = key => (
+        <div className="ingest-report__field" key={key}>
+            <span className="ingest-report__field--label">{reportFields[key].label}</span>
+            <span
+                className={classnames(
+                    'ingest-report__field--value',
+                    (activeFilter === key) && 'filter-active'
+                )}
+                onClick={() => (
+                    key === 'fatal'
+                        ? window.open(RightsURL.getFatalsRightsSearchUrl(ingestId), '_blank')
+                        : FILTERABLE_KEYS.includes(key) && onFilterClick(key)
+                )}
+            >
+                {reportValues[key] || 0}
+            </span>
+        </div>
+    );
+
+    const createTooltipTag = key => (
+        <NexusTooltip content={reportValues[key].toString()}>
+            {createTag(key)}
+        </NexusTooltip>
+    );
 
     return (
         <div className="ingest-report">
             <div className="ingest-report__fields">
                 {
                     Object.keys(reportFields).map(key => (
-                        <div className="ingest-report__field" key={key}>
-                            <span className="ingest-report__field--label">{reportFields[key].label}</span>
-                            <span
-                                className={classnames(
-                                    'ingest-report__field--value',
-                                    (activeFilter === key) && 'filter-active'
-                                )}
-                                onClick={() => (
-                                    key === 'fatal'
-                                        ? window.open(RightsURL.getFatalsRightsSearchUrl(ingestId), '_blank')
-                                        : FILTERABLE_KEYS.includes(key) && onFilterClick(key)
-                                )}
-                            >
-                                {reportValues[key] || 0}
-                            </span>
-                        </div>
+                        showTooltips && ORIGINAL_VALUES_KEYS.includes(key) ? createTooltipTag(key) : createTag(key)
                     ))
                 }
             </div>
@@ -67,6 +80,7 @@ IngestReport.propTypes = {
     isShowingError: PropTypes.bool,
     filterClick: PropTypes.func,
     ingestId: PropTypes.string,
+    showTooltips: PropTypes.bool,
 };
 
 IngestReport.defaultProps = {
@@ -74,6 +88,7 @@ IngestReport.defaultProps = {
     isShowingError: true,
     filterClick: () => null,
     ingestId: '',
+    showTooltips: false,
 };
 
 export default IngestReport;
