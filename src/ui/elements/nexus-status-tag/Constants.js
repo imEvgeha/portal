ccc3c2feb73b21935694d0bbbfe701e8ffc14e30
Brandon Chung <brandon.k.chung@gmail.com>
2020-08-10 18:46:57 -0500

    PORT-2893: add FO statuses to StatusTag constants

--- src/ui/elements/nexus-status-tag/Constants.js
+++ src/ui/elements/nexus-status-tag/Constants.js
@@ -5,4 +5,11 @@ export default {
     MANUAL: 'Manual',
     IN_PROGRESS: 'In Progress',
     ON_HOLD: 'On Hold',
+
+    FO_NOT_STARTED: 'Not Started',
+    FO_IN_PROGRESS: 'In Progress',
+    FO_ON_HOLD: 'On Hold',
+    FO_COMPLETE: 'Complete',
+    FO_CANCELLED: 'Cancelled',
+    FO_FAILED: 'Failed',
 };
