9d5b9d06a33c44f39e6486e78b9b8afc7f208aaf
Antonia Nikolaou <antonini@amdocs.com>
2020-08-12 13:14:35 +0300

    PORT-2888: Add UT

--- src/pages/servicing-orders/components/servicing-orders-table-status-tooltip/ServicingOrdersTableStatusTooltip.test.js
+++ src/pages/servicing-orders/components/servicing-orders-table-status-tooltip/ServicingOrdersTableStatusTooltip.test.js
@@ -3,34 +3,57 @@ import {shallow} from 'enzyme';
 import ServicingOrdersTableStatusTooltip from './ServicingOrdersTableStatusTooltip';
 
 describe('ServicingOrdersTableStatusTooltip', () => {
+    let wrapper = null;
+    let props;
+
+    beforeEach(() => {
+        props = {
+            reportValues: {
+                notStarted: 20001,
+                inProgress: 1000,
+                onHold: 1001,
+                completed: 19999,
+                canceled: 3,
+                failed: 2,
+            },
+        };
+        wrapper = shallow(<ServicingOrdersTableStatusTooltip {...props} />);
+    });
+
     it('should match snapshot', () => {
-        const wrapper = shallow(<ServicingOrdersTableStatusTooltip statusBarInfo={{totalRows: 5, selectedRows: 3}} />);
         expect(wrapper).toMatchSnapshot();
     });
 
-    it('should show the selected row label if there are selected rows', () => {
-        const wrapper = shallow(<ServicingOrdersTableStatusTooltip statusBarInfo={{totalRows: 5, selectedRows: 3}} />);
-        const selectedRowsText = wrapper
-            .find('.nexus-c-servicing-orders-table-status-tooltip__description')
-            .at(1)
-            .text();
-
-        expect(selectedRowsText).toContain('Selected');
+    it('should include an nexus-c-servicing-orders-table-status-tooltip and nexus-c-servicing-orders-table-status-tooltip__fields  div', () => {
+        const ingestReport = wrapper.find('.nexus-c-servicing-orders-table-status-tooltip');
+        expect(ingestReport).toHaveLength(1);
+        const ingestReportFields = wrapper.find('.nexus-c-servicing-orders-table-status-tooltip__fields');
+        expect(ingestReportFields).toHaveLength(1);
     });
 
-    it('should not show the selected row label if there are no selected rows', () => {
-        const wrapper = shallow(<ServicingOrdersTableStatusTooltip statusBarInfo={{totalRows: 5, selectedRows: 0}} />);
-        const selectedRowsNode = wrapper.find('.nexus-c-servicing-orders-table-status-tooltip__description').at(1).length;
+    it('should include 6 tags and their values', () => {
+        const labels = wrapper.find('.nexus-c-servicing-orders-table-status-tooltip__field--label');
+        expect(labels).toHaveLength(6);
+        const values = wrapper.find('.nexus-c-servicing-orders-table-status-tooltip__field--value');
+        expect(values).toHaveLength(6);
 
-        expect(selectedRowsNode).toBeFalsy();
+        expect(labels.at(0).text()).toEqual('Not Started');
+        expect(values.at(0).text()).toEqual('20001');
+
+        expect(labels.at(1).text()).toEqual('In Progress');
+        expect(values.at(1).text()).toEqual('1000');
+
+        expect(labels.at(2).text()).toEqual('On Hold');
+        expect(values.at(2).text()).toEqual('1001');
+
+        expect(labels.at(3).text()).toEqual('Completed');
+        expect(values.at(3).text()).toEqual('19999');
+
+        expect(labels.at(4).text()).toEqual('Canceled');
+        expect(values.at(4).text()).toEqual('3');
+
+        expect(labels.at(5).text()).toEqual('Failed');
+        expect(values.at(5).text()).toEqual('2');
     });
 
-    it('should show the correct number of total rows', () => {
-        const wrapper = shallow(<ServicingOrdersTableStatusTooltip statusBarInfo={{totalRows: 55, selectedRows: 0}} />);
-        const totalRowsText = wrapper.find('.nexus-c-servicing-orders-table-status-tooltip__description')
-            .at(0)
-            .text();
-
-        expect(totalRowsText).toContain('55');
-    });
 });
--- src/ui/elements/nexus-grid/elements/cell-renderer/tooltip-cell-renderer/TooltipCellRenderer.test.js
+++ src/ui/elements/nexus-grid/elements/cell-renderer/tooltip-cell-renderer/TooltipCellRenderer.test.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 import TooltipCellRenderer from './TooltipCellRenderer';
+import NexusTooltip from "../../../../nexus-tooltip/NexusTooltip";
 
 describe('TooltipCellRenderer', () => {
     let wrapper = null;
 
     beforeEach(() => {
-        wrapper = shallow(<TooltipCellRenderer value="IN PROGRESS" />);
+        wrapper = shallow(<TooltipCellRenderer value="12" />);
     });
 
     it('should match snapshot', () => {
         expect(wrapper).toMatchSnapshot();
     });
 
-    it('should apply proper coloring for given value', () => {
-        const cellRenderer = wrapper.find('.nexus-c-emphasized-cell-renderer--is-teal');
-        expect(cellRenderer.exists()).toBeTruthy();
+    it('should render a NexusTooltip', () => {
+        const tooltip = wrapper.find(NexusTooltip);
+        expect(tooltip.exists()).toBeTruthy();
+        expect(tooltip.props().hasWhiteBackground).toEqual(true);
+
     });
 });
