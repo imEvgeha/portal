1b7ea8dd9b196d223410057547c6790f2fc707a3
dradojkovic <dradojkovic@computerrock.com>
2020-08-14 15:23:59 +0200

    PORT-2424: Update DOP-services to use real data instead of mocks

--- src/pages/avails/selected-for-planning/DOP-services.js
+++ src/pages/avails/selected-for-planning/DOP-services.js
@@ -3,49 +3,45 @@ import config from 'react-global-configuration';
 import {getUsername} from '../../../auth/authSelectors';
 import {store} from '../../../index';
 import {nexusFetch} from '../../../util/http-client/index';
-import {PAGE_SIZE, getSearchPayload, PROJECT_ATTRIBUTE_MOCK_RESPONSE, PROJECT_SEARCH_MOCK_RESPONSE, PROJECT_ID} from './constants';
+import {PAGE_SIZE, getSearchPayload, PROJECT_ID} from './constants';
 
+const DEFAULT_TIMEOUT = 60000;
 const username = getUsername(store.getState());
 
 const DOPService = {
     getUsersProjectsList: (offset = 1, limit = PAGE_SIZE) => {
-        const url = `${config.get('gateway.DOPUrl')}${config.get('gateway.service.DOPProjectManagement')}/search`;
+        const url = `${config.get('gateway.DOPUrl')}${config.get('gateway.service.DOPProjectManagementProject')}/search`;
         const body = getSearchPayload(username, offset, limit);
-        // return nexusFetch(url, {method: 'post', body: JSON.stringify(body)});
-        return new Promise((resolve, reject) => {
-            resolve(PROJECT_SEARCH_MOCK_RESPONSE);
-        });
+        return nexusFetch(url, {method: 'post', body: JSON.stringify(body)}, DEFAULT_TIMEOUT, true);
     },
     getProjectAttributes: (projectIds = []) => {
         const url = `${config.get('gateway.DOPUrl')}${config.get(
-            'gateway.service.DOPProjectManagement'
+            'gateway.service.DOPProjectManagementBase'
         )}/projectAttribute`;
         const body = {
             filterCriterion: [
                 {
                     fieldName: 'projectId',
                     operator: 'in',
-                    value: projectIds.join(','),
+                    // value: projectIds.join(','),
+                    value: 'Project8680,Project8679',
                     logicalAnd: true,
                 },
                 {
                     fieldName: 'code',
                     operator: 'in',
                     value:
-                        'PROJECT_NAME, format, licensee, licensor, platformCategory, releaseYear, rightID, title, transactionType',
+                        'PROJECT_NAME,format,licensee,licensor,platformCategory,releaseYear,rightID,title,transactionType',
                     logicalAnd: true,
                 },
             ],
         };
 
-        // return nexusFetch(url, {method: 'post', mode: 'no-cors', body: JSON.stringify(body)});
-        return new Promise((resolve, reject) => {
-            resolve(PROJECT_ATTRIBUTE_MOCK_RESPONSE);
-        });
+        return nexusFetch(url, {method: 'post', body: JSON.stringify(body)});
     },
     createProjectRequestData: (data = []) => {
         const selectedRightArray = !!data.length && data.map((right, index) => {
-            return { code: `selectedRightID[${index}]`, value: right.id}
+            return {code: `selectedRightID[${index}]`, value: right.id}
         });
         const selectedTerritoryArray = () => {
             const arr = [];
@@ -78,16 +74,16 @@ const DOPService = {
         return req;
     },
     createProject: (data) => {
-        const url = `${config.get('gateway.DOPUrl')}${config.get('gateway.service.DOPProjectManagement')}`;
+        const url = `${config.get('gateway.DOPUrl')}${config.get('gateway.service.DOPProjectManagementProject')}`;
         return nexusFetch(url, {
             method: 'POST',
             body: JSON.stringify(data),
         });
     },
-    startProject: (projectId) => {
-        const url = `${config.get('gateway.DOPUrl')}${config.get('gateway.service.DOPProjectManagement')}`;
+    startProject: ({projectId}) => {
+        const url = `${config.get('gateway.DOPUrl')}${config.get('gateway.service.DOPProjectManagementProject')}`;
         // TODO: Error handling if necessary
-        nexusFetch(`${url}/${projectId}/start`, {method: 'post'});
+        return nexusFetch(`${url}/${projectId}/start`, {method: 'post'});
     },
 };
 
