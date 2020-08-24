337c3fc8b2a57461728f5759e8199e6ccbe35f0e
Thomas Tupper <ttupper@uxdesignassociates.com>
2020-08-24 17:39:20 -0500

    PORT-3014: a few more tests

--- src/pages/servicing-orders/servicing-order/components/fulfillment-order-panel/FulfillmentOrderPanel.test.js
+++ src/pages/servicing-orders/servicing-order/components/fulfillment-order-panel/FulfillmentOrderPanel.test.js
@@ -35,8 +35,51 @@ describe('FulfillmentOrderPanel', () => {
 
     it('should have a servicer indicator', () => {
         const servicerElement = wrapper.find('.nexus-c-fulfillment-order-panel__servicer');
+        expect(servicerElement).toHaveLength(1);
+    });
+
+    it('should handle the DETE case', () => {
+        wrapper.setProps({
+            handleFulfillmentOrderChange,
+            id: 'VU000134567-001',
+            dueDate: '10/05/2021',
+            servicer: 'DETE',
+            externalId: 'COVFEFE',
+            status: 'COMPLETED',
+            selected: false,
+        });
+        wrapper.update();
         const servicerNodeText = wrapper.find('.nexus-c-fulfillment-order-panel__servicer-label').text();
         expect(servicerNodeText).toBe('DETE');
-        expect(servicerElement).toHaveLength(1);
+    });
+
+    it('should handle the CHOP case', () => {
+        wrapper.setProps({
+            handleFulfillmentOrderChange,
+            id: 'VU000134567-001',
+            dueDate: '10/05/2021',
+            servicer: 'CHOP',
+            externalId: 'COVFEFE',
+            status: 'COMPLETED',
+            selected: false,
+        });
+        wrapper.update();
+        const servicerNodeText = wrapper.find('.nexus-c-fulfillment-order-panel__servicer-label').text();
+        expect(servicerNodeText).toBe('CHOP');
+    });
+
+    it('should handle the JUICEBOX case', () => {
+        wrapper.setProps({
+            handleFulfillmentOrderChange,
+            id: 'VU000134567-001',
+            dueDate: '10/05/2021',
+            servicer: 'JUICEBOX',
+            externalId: 'COVFEFE',
+            status: 'COMPLETED',
+            selected: false,
+        });
+        wrapper.update();
+        const servicerNodeText = wrapper.find('.nexus-c-fulfillment-order-panel__servicer-label').text();
+        expect(servicerNodeText).toBe('JUICE BOX');
     });
 });
