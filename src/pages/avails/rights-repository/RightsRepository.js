746a9081a73e858df09ae7dc8eef70c1c40d74c0
dradojkovic <dradojkovic@computerrock.com>
2020-08-27 09:39:11 +0200

    PORT-3043: Modify RightsRepository to refresh Selected for Planning count when data is refreshed

--- src/pages/avails/rights-repository/RightsRepository.js
+++ src/pages/avails/rights-repository/RightsRepository.js
@@ -186,6 +186,7 @@ const RightsRepository = ({
         }
     }, [selectedRepoRights, selectedGridApi]);
 
+    // Fetch and set DOP projects count for current user
     useEffect(() => {
         DOPService.getUsersProjectsList(1, 1)
             .then(([response, headers]) => {
@@ -195,7 +196,7 @@ const RightsRepository = ({
             .catch(error => {
                 // error-handling here
             });
-    }, [activeTab, prePlanRights.length]);
+    }, [activeTab, prePlanRights.length, isPlanningTabRefreshed]);
 
     // Fetch only pre-plan rights from the current user
     useEffect(() => {
