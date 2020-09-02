41af48778ecc24f21faa723b520c2035a6b3970f
Kuldeep S Bora <KBORA@amdocs.com>
2020-09-02 16:16:27 +0300

    PORT-2650/2652: default roles

--- src/ability.js
+++ src/ability.js
@@ -67,12 +67,8 @@ const updateAbility = (roles = []) => {
     else if (roles.includes('metadata_admin')) {
         can(['create', 'read', 'update', 'delete'], 'Metadata');
     }
-
-    // } else if (roles.includes('metadata_user')) {
-    //     can(['create', 'read', 'update', 'delete'], 'Metadata');
-    // } else if (roles.includes('metadata_admin')) {
-    //can(['create', 'read', 'update', 'delete'], 'Metadata');
-    // }
+    //TODO: metadata_user to be added in PORT 2651
+    can(['create', 'read', 'update'], 'Metadata');
 
     // ******** Servicing Orders *************
     can(['create', 'read', 'update', 'delete'], 'ServicingOrders');
