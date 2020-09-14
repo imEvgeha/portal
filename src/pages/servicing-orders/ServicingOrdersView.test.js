dd5e5e59c5f1d53bab924931d5a316bed50f86fd
Demetris Savva <Demetris.Savva@amdocs.com>
2020-09-14 13:52:01 +0300

    Fix build

--- src/pages/servicing-orders/ServicingOrdersView.test.js
+++ src/pages/servicing-orders/ServicingOrdersView.test.js
@@ -6,7 +6,7 @@ describe('ServicingOrdersView', () => {
     describe('HTML content', () => {
         const wrapper = shallow(<ServicingOrdersView />);
         it('should render header title', () => {
-            expect(wrapper.find('.nexus-c-servicing-orders__title').text()).toEqual('Servicing Orders');
+            expect(wrapper.find('h1').text()).toEqual('Servicing Orders');
         });
         it('should match snapshot', () => {
             expect(wrapper).toMatchSnapshot();
