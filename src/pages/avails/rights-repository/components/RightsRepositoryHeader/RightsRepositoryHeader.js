c0ac5a631199780968bd43ceb7eae3ef491ced7c
Madhurima Kandul <madhkand@amdocs.com>
2020-09-17 13:50:59 +0300

    PORT-3086: Remove loading state for create new right button

--- src/pages/avails/rights-repository/components/RightsRepositoryHeader/RightsRepositoryHeader.js
+++ src/pages/avails/rights-repository/components/RightsRepositoryHeader/RightsRepositoryHeader.js
@@ -7,18 +7,14 @@ import {URL} from '../../../../../util/Common';
 import {CREATE_NEW_RIGHT} from '../../constants';
 import './RightsRepositoryHeader.scss';
 
-export const RightsRepositoryHeader = ({title, history, isTableDataLoading}) => {
+export const RightsRepositoryHeader = ({title, history}) => {
     return (
         <div className="nexus-c-rights-repository-header">
             <div className="nexus-c-rights-repository-header__title">
                 <RightsIcon fill="#42526E" />
                 <h1 className="nexus-c-rights-repository-header__title-text">{title}</h1>
             </div>
-            <Button
-                appearance="primary"
-                isLoading={isTableDataLoading}
-                onClick={() => history.push(URL.keepEmbedded('/avails/rights/create'))}
-            >
+            <Button appearance="primary" onClick={() => history.push(URL.keepEmbedded('/avails/rights/create'))}>
                 {CREATE_NEW_RIGHT}
             </Button>
         </div>
@@ -28,13 +24,11 @@ export const RightsRepositoryHeader = ({title, history, isTableDataLoading}) => 
 RightsRepositoryHeader.propTypes = {
     title: PropTypes.string,
     history: PropTypes.object,
-    isTableDataLoading: PropTypes.bool,
 };
 
 RightsRepositoryHeader.defaultProps = {
     title: 'Rights',
     history: {},
-    isTableDataLoading: false,
 };
 
 export default withRouter(RightsRepositoryHeader);
--- src/pages/avails/rights-repository/components/RightsRepositoryHeader/RightsRepositoryHeader.test.js
+++ src/pages/avails/rights-repository/components/RightsRepositoryHeader/RightsRepositoryHeader.test.js
@@ -12,7 +12,6 @@ describe('RightsRepositoryHeader', () => {
         history: {
             push: historyPushMock,
         },
-        isTableDataLoading: false,
     };
 
     beforeEach(() => {
