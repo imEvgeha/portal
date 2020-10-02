418a83f3d625939876a17cbf9ef2cc568a46a5e7
Nikola Perisic <nikola.perisic@computerrock.com>
2020-10-02 13:23:55 +0200

    PORT-3181 add refresh button

--- src/pages/dop-tasks/DopTasksView.js
+++ src/pages/dop-tasks/DopTasksView.js
@@ -1,4 +1,6 @@
 import React, {useState} from 'react';
+import RefreshIcon from '@atlaskit/icon/glyph/refresh';
+import IconButton from '../../ui/atlaskit/icon-button/IconButton';
 import DopTasksHeader from './components/dop-tasks-header/DopTasksHeader';
 import DopTasksTable from './components/dop-tasks-table/DopTasksTable';
 import QueuedTasks from './components/queued-tasks/QueuedTasks';
@@ -14,6 +16,7 @@ const DopTasksView = () => {
             <DopTasksHeader>
                 <QueuedTasks setUser={setUser} />
                 <SavedTableDropdown />
+                <IconButton icon={() => <RefreshIcon size="large" />} onClick={() => setUser(USER)} label="Refresh" />
             </DopTasksHeader>
             <DopTasksTable user={user} />
         </div>
