6ec6e2ef3f022276927259da58150cdee838871c
Nikola Perisic <nikola.perisic@computerrock.com>
2020-10-02 16:23:01 +0200

    PORT-3181 refresh table data, update tests

--- src/pages/dop-tasks/DopTasksView.js
+++ src/pages/dop-tasks/DopTasksView.js
@@ -1,6 +1,9 @@
 import React, {useState} from 'react';
+import PropTypes from 'prop-types';
 import RefreshIcon from '@atlaskit/icon/glyph/refresh';
+import {connect} from 'react-redux';
 import IconButton from '../../ui/atlaskit/icon-button/IconButton';
+import {toggleRefreshGridData} from '../../ui/grid/gridActions';
 import DopTasksHeader from './components/dop-tasks-header/DopTasksHeader';
 import DopTasksTable from './components/dop-tasks-table/DopTasksTable';
 import QueuedTasks from './components/queued-tasks/QueuedTasks';
@@ -8,7 +11,7 @@ import SavedTableDropdown from './components/saved-table-dropdown/SavedTableDrop
 import {USER} from './constants';
 import './DopTasksView.scss';
 
-const DopTasksView = () => {
+export const DopTasksView = ({toggleRefreshGridData}) => {
     const [user, setUser] = useState(USER);
 
     return (
@@ -16,11 +19,26 @@ const DopTasksView = () => {
             <DopTasksHeader>
                 <QueuedTasks setUser={setUser} />
                 <SavedTableDropdown />
-                <IconButton icon={() => <RefreshIcon size="large" />} onClick={() => setUser(USER)} label="Refresh" />
+                <IconButton
+                    icon={() => <RefreshIcon size="large" />}
+                    onClick={() => toggleRefreshGridData(true)}
+                    label="Refresh"
+                />
             </DopTasksHeader>
             <DopTasksTable user={user} />
         </div>
     );
 };
+const mapDispatchToProps = dispatch => ({
+    toggleRefreshGridData: payload => dispatch(toggleRefreshGridData(payload)),
+});
 
-export default DopTasksView;
+DopTasksView.propTypes = {
+    toggleRefreshGridData: PropTypes.func,
+};
+
+DopTasksView.defaultProps = {
+    toggleRefreshGridData: () => null,
+};
+
+export default connect(null, mapDispatchToProps)(DopTasksView);
