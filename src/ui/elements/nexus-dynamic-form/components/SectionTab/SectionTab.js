8aad0bb2d51d1464414e8b57531ad87a782cd660
Luka-Krsmanovic <luka.krsmanovic@computerrock.com>
2020-10-09 12:58:01 +0200

    PORT-3127 moving constants to existing file and adding constant for highlighting fields

--- src/ui/elements/nexus-dynamic-form/components/SectionTab/SectionTab.js
+++ src/ui/elements/nexus-dynamic-form/components/SectionTab/SectionTab.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
-import {TABS_MAPPINGS} from './constants';
+import {TABS_MAPPINGS} from '../../constants';
 import './SectionTab.scss';
 
 const SectionTab = ({section = '', isActive = false, onClick = null}) => {
--- src/ui/elements/nexus-dynamic-form/constants.js
+++ src/ui/elements/nexus-dynamic-form/constants.js
@@ -12,3 +12,14 @@ export const DATETIME_FIELDS = {
     REGIONAL_MIDNIGHT: 'regionalMidnight',
 };
 export const INCORRECT_VALUE = 'INCORRECT_VALUE';
+
+export const HIGHLIGHTED_FIELDS = ['originalRightIds', 'sourceRightId'];
+
+export const TABS_MAPPINGS = [
+    {tabName: 'Basic Info and Territory', id: 'Basic Info'},
+    {tabName: 'Title and Episodic', id: 'Title'},
+    {tabName: 'IDs', id: 'IDs'},
+    {tabName: 'Localization and captions', id: 'Localization'},
+    {tabName: 'Other', id: 'Other'},
+    {tabName: 'Music', id: 'Music'},
+];
