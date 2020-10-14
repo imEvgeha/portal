3280df2020a5c756052f0753e380ac90427e7563
Madhurima Kandul <madhkand@amdocs.com>
2020-10-14 17:47:39 +0300

    PORT-3393: Add bulk set option in pre-plan

--- src/ui/elements/nexus-textarea/NexusTextArea.js
+++ src/ui/elements/nexus-textarea/NexusTextArea.js
@@ -5,13 +5,12 @@ import TextArea from '@atlaskit/textarea';
 const NexusTextArea = ({onTextChange, notesValue, ...restProps}) => (
     <TextArea
         appearance="standard"
-        onChange={onTextChange}
+        onChange={e => onTextChange(e.target.value)}
         minimumRows={5}
         value={notesValue}
         {...restProps}
     />
 );
-
 
 NexusTextArea.propTypes = {
     onTextChange: PropTypes.func,
@@ -19,7 +18,7 @@ NexusTextArea.propTypes = {
 };
 
 NexusTextArea.defaultProps = {
-    onTextChange: null,
+    onTextChange: () => null,
     notesValue: '',
 };
 
