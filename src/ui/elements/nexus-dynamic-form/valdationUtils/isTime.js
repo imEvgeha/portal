fce0c444d774e29fa5b3c0d9354c1c43697a50da
Luka-Krsmanovic <luka.krsmanovic@computerrock.com>
2020-10-28 11:12:46 +0100

    PORT-3131 fixing error

--- src/ui/elements/nexus-dynamic-form/valdationUtils/isTime.js
+++ src/ui/elements/nexus-dynamic-form/valdationUtils/isTime.js
@@ -1,6 +1,6 @@
 import {INCORRECT_TIME} from '../constants';
 
-export function isYear(value) {
+export function isTime(value) {
     const timePattern = /^([01]\d|2[0-3]):[0-5]\d:[0-5]\d$/;
     return timePattern.test(value) ? undefined : INCORRECT_TIME;
 }
