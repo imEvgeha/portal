6c20adf191aa740a26ab352274b05c014cf45416
Luka-Krsmanovic <luka.krsmanovic@computerrock.com>
2020-10-28 11:00:32 +0100

    PORT-3131 adjusting isYear regex

--- src/ui/elements/nexus-dynamic-form/valdationUtils/isYear.js
+++ src/ui/elements/nexus-dynamic-form/valdationUtils/isYear.js
@@ -1,6 +1,6 @@
 import {INCORRECT_YEAR} from '../constants';
 
 export function isYear(value) {
-    const yearPattern = /^[1-9]\d\d\d/;
+    const yearPattern = /^[1-9]\d\d\d$/;
     return yearPattern.test(value) ? undefined : INCORRECT_YEAR;
 }
