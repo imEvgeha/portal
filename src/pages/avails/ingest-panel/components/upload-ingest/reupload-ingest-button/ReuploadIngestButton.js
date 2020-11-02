6485c66a651138285a7fbffc6cab380e6920f42a
Antonia Nikolaou <antonini@amdocs.com>
2020-11-02 11:04:27 +0200

    PORT-3290: Add Reupload file

---
+++ src/pages/avails/ingest-panel/components/upload-ingest/reupload-ingest-button/ReuploadIngestButton.js
@@ -0,0 +1,45 @@
+import React, {useCallback, useContext} from 'react';
+import PropTypes from 'prop-types';
+import UploadIcon from '../../../../../../assets/action-upload.svg';
+import {NexusTooltip} from '../../../../../../ui/elements';
+import {NexusModalContext} from '../../../../../../ui/elements/nexus-modal/NexusModal';
+import InputForm from '../InputForm/InputForm';
+
+const TITLE = 'Avail Ingest';
+
+const ReuploadIngestButton = ({attachment}) => {
+    const {openModal, closeModal} = useContext(NexusModalContext);
+    const openModalCallback = useCallback((node, params) => openModal(node, params), []);
+    const closeModalCallback = useCallback(() => closeModal(), []);
+
+    const closeUploadModal = useCallback(() => {
+        closeModalCallback();
+    }, []);
+
+    const buildForm = () => {
+        return (
+            <InputForm
+                closeModal={closeUploadModal}
+                attachment={attachment}
+                openModalCallback={openModalCallback}
+                closeModalCallback={closeModalCallback}
+            />
+        );
+    };
+
+    const reUploadIngestFile = () => {
+        openModal(buildForm(), {title: TITLE, width: 'small', shouldCloseOnOverlayClick: false});
+    };
+
+    return (
+        <NexusTooltip content="Upload Attachment">
+            <UploadIcon className="nexus-c-avails-ingest__download-icon" onClick={() => reUploadIngestFile()} />
+        </NexusTooltip>
+    );
+};
+
+ReuploadIngestButton.propTypes = {
+    attachment: PropTypes.object.isRequired,
+};
+
+export default ReuploadIngestButton;
