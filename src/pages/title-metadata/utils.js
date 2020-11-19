c1f34d986747a72bab343f9a6ca2b8289efdfe70
Nikola Perisic <nikola.perisic@computerrock.com>
2020-11-19 11:22:12 +0100

    PORT-3235 add title metadata sub fields

--- src/pages/title-metadata/utils.js
+++ src/pages/title-metadata/utils.js
@@ -4,9 +4,23 @@ import {NEXUS, VZ, MOVIDA} from './constants';
 export const fetchTitleMetadata = async (searchCriteria, offset, limit, sortedParams) => {
     try {
         const response = await titleService.advancedSearch(searchCriteria, offset, limit, sortedParams);
+        console.log(response);
         const {data = [], page, size, total} = response || {};
         const tableData = data.reduce((acc, obj) => {
-            const {id, title = '', contentType = '', releaseYear = ''} = obj || {};
+            const {
+                id,
+                title = '',
+                contentType = '',
+                releaseYear = '',
+                contentSubType = '',
+                duration = '',
+                countryOfOrigin = '',
+                animated = '',
+                eventType = '',
+                originalLanguage = '',
+                usBoxOffice = '',
+                category = '',
+            } = obj || {};
             const repository = id.includes('vztitl_') ? VZ : id.includes('movtitl_') ? MOVIDA : NEXUS;
             return [
                 ...acc,
@@ -15,6 +29,14 @@ export const fetchTitleMetadata = async (searchCriteria, offset, limit, sortedPa
                     repository,
                     contentType,
                     releaseYear,
+                    contentSubType,
+                    duration,
+                    countryOfOrigin,
+                    animated,
+                    eventType,
+                    originalLanguage,
+                    usBoxOffice,
+                    category,
                 },
             ];
         }, []);
