b07541d2f23309680edadbfb032220487b5c736c
Luka-Krsmanovic <luka.krsmanovic@computerrock.com>
2020-11-20 09:39:55 +0100

    PORT-3241 creating new modal component

--- src/ui/elements/nexus-persons-list/NexusPersonsList.js
+++ src/ui/elements/nexus-persons-list/NexusPersonsList.js
@@ -1,26 +1,27 @@
 /* eslint-disable */
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useContext} from 'react';
 import PropTypes from 'prop-types';
-import {cloneDeep} from 'lodash';
 import UserPicker from '@atlaskit/user-picker';
+import Button from '@atlaskit/button';
+import {Field, FormFooter} from '@atlaskit/form';
+import TextField from '@atlaskit/textfield';
+import {default as AKForm} from '@atlaskit/form/Form';
 import classnames from 'classnames';
 import {DragDropContext, Droppable} from 'react-beautiful-dnd';
 import {uid} from 'react-uid';
 import {getFilteredCastList, getFilteredCrewList} from '../../../pages/legacy/constants/metadata/configAPI';
 import {searchPerson} from '../../../pages/avails/right-details/rightDetailsServices';
-import NexusCharacterNameModal from './elements/NexusCharacterNameModal';
+import {NexusModalContext} from '../nexus-modal/NexusModal';
 import NexusPerson from '../nexus-person/NexusPerson';
 import NexusPersonRO from '../nexus-person-ro/NexusPersonRO';
 import {CAST, CAST_CONFIG, PERSONS_PER_REQUEST} from './constants';
 import './NexusPersonsList.scss';
 
 const NexusPersonsList = ({personsList, uiConfig, hasCharacter, isEdit, updateCastCrew}) => {
+    const {openModal, closeModal} = useContext(NexusModalContext);
+
     const [persons, setPersons] = useState(personsList || []);
     const [searchText, setSearchText] = useState('');
-    const [modalData, setModalData] = useState({});
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [selectedPersonIndex, setSelectedPersonIndex] = useState(null);
-    const [modalInputValue, setModalInputValue] = useState('');
 
     useEffect(() => {
         const updatedPersons = [...personsList];
@@ -96,38 +97,69 @@ const NexusPersonsList = ({personsList, uiConfig, hasCharacter, isEdit, updateCa
         updateCastCrew(updatedPersons, isCast);
     };
 
-    const onCharacterSubmit = () => {
-        const updatedPersons = cloneDeep(persons);
-        const isCast = uiConfig.type === CAST;
-        updatedPersons[selectedPersonIndex].characterName = modalInputValue;
-        setPersons(updatedPersons);
-        setSelectedPersonIndex(null);
-        setModalInputValue('');
-        updateCastCrew(updatedPersons, isCast);
+    const closeCharacterModal = () => {
         closeModal();
     };
 
-    const closeModal = () => {
-        setModalData({});
-        setIsModalOpen(false);
-        setSelectedPersonIndex(null);
-        setModalInputValue('');
-    };
-
-    const openModal = id => {
-        setSelectedPersonIndex(id);
+    const openCharacterModal = id => {
         const selectedPerson = persons && persons[id];
-        setModalInputValue(selectedPerson.characterName);
-        setModalData({
+        const data = {
+            personName: selectedPerson.displayName,
             characterName: selectedPerson.characterName,
-            displayName: selectedPerson.displayName,
+        };
+        const message = data.characterName ? 'Edit Character' : 'Add New Character';
+        openModal(modalContent(data, id), {
+            title: <div className="nexus-c-array__modal-title">{message}</div>,
+            width: 'medium',
         });
-        setIsModalOpen(true);
     };
 
-    const onModalInputChanged = event => {
-        const {value} = event.target;
-        setModalInputValue(value);
+    const onModalSubmit = (values, id) => {
+        const updatedPersons = [...persons];
+        const [person] = updatedPersons.filter(entry => {
+            return entry.id === id;
+        });
+        if (person) {
+            person.characterName = values.characterName;
+            const isCast = uiConfig.type === CAST;
+            setPersons(updatedPersons);
+            updateCastCrew(updatedPersons, isCast);
+        }
+        closeCharacterModal();
+    };
+
+    const modalContent = ({personName, characterName}, id) => {
+        return (
+            <div>
+                <AKForm onSubmit={values => onModalSubmit(values, id)}>
+                    {({formProps, dirty, submitting, reset, getValues, setFieldValue}) => (
+                        <form {...formProps}>
+                            <Field name="personName" defaultValue={personName} label="Person Name" isDisabled>
+                                {({fieldProps}) => <TextField {...fieldProps} />}
+                            </Field>
+                            <Field name="characterName" defaultValue={characterName} label="Character Name" isRequired>
+                                {({fieldProps}) => <TextField {...fieldProps} />}
+                            </Field>
+                            <FormFooter>
+                                <Button type="submit" appearance="primary">
+                                    Submit
+                                </Button>
+                                <Button
+                                    className="cancel-button"
+                                    appearance="danger"
+                                    onClick={() => {
+                                        reset();
+                                        closeModal();
+                                    }}
+                                >
+                                    Cancel
+                                </Button>
+                            </FormFooter>
+                        </form>
+                    )}
+                </AKForm>
+            </div>
+        );
     };
 
     const reorder = (list, startIndex, endIndex) => {
@@ -193,7 +225,7 @@ const NexusPersonsList = ({personsList, uiConfig, hasCharacter, isEdit, updateCa
                                       index={i}
                                       hasCharacter={hasCharacter}
                                       onRemove={() => removePerson(person)}
-                                      onEditCharacter={index => openModal(index)}
+                                      onEditCharacter={index => openCharacterModal(index)}
                                   />
                               );
                           })
@@ -202,15 +234,6 @@ const NexusPersonsList = ({personsList, uiConfig, hasCharacter, isEdit, updateCa
                   persons.map((person, i) => {
                       return <NexusPersonRO key={uid(person.id, i)} person={person} />;
                   })}
-            <NexusCharacterNameModal
-                onSubmit={onCharacterSubmit}
-                displayName={modalData.displayName}
-                characterName={modalData.characterName}
-                value={modalInputValue}
-                onChange={onModalInputChanged}
-                isModalOpen={isModalOpen}
-                closeModal={closeModal}
-            />
         </>
     );
 };
