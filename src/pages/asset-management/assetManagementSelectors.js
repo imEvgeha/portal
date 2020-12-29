480563e39b24f717ccc3609fd45794a7ffd8ebf0
Madhurima Kandul <madhkand@amdocs.com>
2020-12-29 15:47:27 +0200

    remove asset management from pages folder

--- src/pages/asset-management/assetManagementSelectors.js
+++
@@ -1,5 +0,0 @@
-import {createSelector} from 'reselect';
-
-const getAssetManagementReducer = state => state.assetManagement;
-
-export const getPosterList = createSelector(getAssetManagementReducer, assets => assets.posterList);
--- src/pages/asset-management/constants.js
+++
@@ -1,2 +0,0 @@
-export const CHOOSE_ARTWORK_PATH = '/media-asset/choose-artwork';
-export const POSTER_RESOLUTION = '1920x1080';
