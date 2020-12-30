26bb0be0ee61a9e4d0fa813617e1cf3cabf42d6f
Madhurima Kandul <madhkand@amdocs.com>
2020-12-30 16:12:41 +0200

    Add cors config to fetch MAM API

--- packages/portal-media-asset-management/src/assetManagementService.js
+++ packages/portal-media-asset-management/src/assetManagementService.js
@@ -1,12 +1,10 @@
 export const fetchPosters = url => {
     const headers = new Headers();
-    headers.append('Authorization', `Basic ${btoa('admin:admin')}`);
     headers.append('Accept', 'application/json');
-    headers.append('Access-Control-Allow-Origin', '*');
-    headers.append('Access-Control-Allow-Credentials', 'true');
+    headers.append('Authorization', `Basic ${btoa('admin:admin')}`);
     return fetch(url, {
         method: 'GET',
-        credentials: 'include',
         headers,
-    }).then(response => response.body);
+    })
+        .then(response => response.json());
 };
