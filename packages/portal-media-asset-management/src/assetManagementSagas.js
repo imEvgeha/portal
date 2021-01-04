a4cfc941a18ecf7244473ebeee298364ee1d8b0f
Madhurima Kandul <madhkand@amdocs.com>
2021-01-04 14:42:29 +0200

    MAM: Add thumbnail images to posters

--- packages/portal-media-asset-management/src/assetManagementSagas.js
+++ packages/portal-media-asset-management/src/assetManagementSagas.js
@@ -7,18 +7,8 @@ function* resourcePosters({payload}) {
     try {
         const url = `http://vidispine-stg.misc.odg.ondemand.co.uk/API/item/${payload}/posterresource`;
         const resource = yield call(fetchPosters, url);
-        /* const resource = {
-            "uri": [
-                "http://vidispine-stg.misc.odg.ondemand.co.uk/API/poster/VX-6/VX-8018;version=0"
-            ]
-        }; */
         const resourceURL = get(resource, 'uri[0]', '');
         const timeFrames = yield call(fetchPosters, resourceURL);
-        /* const timeFrames = {
-            "uri": [
-                "1500499@30000"
-            ]
-        }; */
         const posters = [];
         get(timeFrames, 'uri', []).map(frame => {
             posters.push(`${resourceURL}/${frame}`);
