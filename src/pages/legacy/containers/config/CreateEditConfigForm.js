64565907eda22cb322719867e319aaa532f3f2fd
Madhurima Kandul <madhkand@amdocs.com>
2021-02-11 15:32:30 +0200

    PORT-3914: use updated state value while saving form

--- src/pages/legacy/containers/config/CreateEditConfigForm.js
+++ src/pages/legacy/containers/config/CreateEditConfigForm.js
@@ -1,20 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
+import PropTypes from 'prop-types';
 import {get, cloneDeep} from 'lodash';
 import Button from '@atlaskit/button';
 import {Form} from 'react-forms-processor';
 import {validateAllFields} from 'react-forms-processor/dist/utilities/validation';
-import {renderer as akRenderer, FormButton} from 'react-forms-processor-atlaskit';
+import {renderer as akRenderer} from 'react-forms-processor-atlaskit';
 import {Modal, ModalBody, ModalFooter} from 'reactstrap';
 import {Dropdown, DropdownItem, DropdownMenu, DropdownToggle} from 'reactstrap';
-
 import RepeatingFormField from './custom-types/Repeats';
-
 import RepeatingField from './custom-types/RepeatsPrimitives';
 import NexusDateTimePicker from '@vubiquity-nexus/portal-ui/lib/elements/nexus-date-and-time-elements/nexus-date-time-picker/NexusDateTimePicker';
-import {isObject, isObjectEmpty} from '@vubiquity-nexus/portal-utils/lib/Common';
+import {isObject} from '@vubiquity-nexus/portal-utils/lib/Common';
 import {getConfigApiValues} from '../../common/CommonConfigService';
 import {cache} from './EndpointContainer';
-import PropTypes from 'prop-types';
 import DynamicObjectType from './custom-types/DynamicObjectType';
 import ObjectType from './custom-types/ObjectType';
 import ObjectKey from './custom-types/ObjectKey';
@@ -22,17 +20,11 @@ import DelayedOptions from './custom-types/DelayedOptions';
 import {Can} from '@vubiquity-nexus/portal-utils/lib/ability';
 import {Field as AkField} from '@atlaskit/form';
 
-export default class CreateEditConfigForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: this.props.value,
-            dropdownOpen: false,
-        };
-        this.optionsHandler = this.optionsHandler.bind(this);
-    }
+const CreateEditConfigForm = ({value, onRemoveItem, onCancel, schema, onSubmit, displayName, label}) => {
+    const [val, setVal] = useState(value);
+    const [dropdownOpen, setDropdownOpen] = useState(false);
 
-    renderer = (field, onChange, onFieldFocus, onFieldBlur) => {
+    const renderer = (field, onChange, onFieldFocus, onFieldBlur) => {
         const {defaultValue, id, label, type, value, dynamic = false, misc = {}} = field;
         if (field.hasOwnProperty('disable')) {
             field.disabled = field.disable;
@@ -47,7 +39,7 @@ export default class CreateEditConfigForm extends React.Component {
         const noItemsMessage = misc.noItemsMessage;
         const idAttribute = misc.idAttribute;
 
-        const currentValue = get(value, field.name, value) || get(this.state.value, field.id, defaultValue);
+        const currentValue = get(value, field.name, value) || get(val, field.id, defaultValue);
 
         if (currentValue) {
             field.defaultValue = currentValue;
@@ -135,7 +127,7 @@ export default class CreateEditConfigForm extends React.Component {
         }
     };
 
-    convertDataToOption = (dataSource, schema) => {
+    const convertDataToOption = (dataSource, schema) => {
         let label, value;
         const {displayValueDelimiter = ' / '} = schema;
         if (Array.isArray(schema.label) && schema.label.length > 1) {
@@ -154,12 +146,12 @@ export default class CreateEditConfigForm extends React.Component {
         return {label, value};
     };
 
-    processOptions = (rawOptions, field) => {
-        const items = rawOptions.map(rec => this.convertDataToOption(rec, field.source));
+    const processOptions = (rawOptions, field) => {
+        const items = rawOptions.map(rec => convertDataToOption(rec, field.source));
         return [{items}];
     };
 
-    getLicensees = (field, context) => {
+    const getLicensees = (field, context) => {
         //needs to be moved to API side to make this generic
         const servicingRegion = get(context, 'value.servicingRegionName', '');
 
@@ -171,7 +163,7 @@ export default class CreateEditConfigForm extends React.Component {
             if (cache[field.source.url][servicingRegion] instanceof Promise) {
                 return cache[field.source.url][servicingRegion].then(() => {});
             }
-            return this.processOptions(cache[field.source.url][servicingRegion], field);
+            return processOptions(cache[field.source.url][servicingRegion], field);
         }
         const promiseLicensees = getConfigApiValues(
             field.source.url,
@@ -185,7 +177,7 @@ export default class CreateEditConfigForm extends React.Component {
                 ...cache[field.source.url],
                 [servicingRegion]: response.data,
             };
-            return this.processOptions(response.data, field);
+            return processOptions(response.data, field);
         });
         cache[field.source.url] = {
             ...cache[field.source.url],
@@ -194,17 +186,17 @@ export default class CreateEditConfigForm extends React.Component {
         return promiseLicensees;
     };
 
-    optionsHandler(fieldId, fields, context) {
+    const optionsHandler = (fieldId, fields, context) => {
         const field = fields.find(({id}) => id === fieldId);
         if (field) {
             if ((field.type === 'select' || field.type === 'multiselect') && field.source) {
                 if (field.name === 'licensees') {
                     //licensees needs to be filtered by selected servicing region name
-                    return this.getLicensees(field, context);
+                    return getLicensees(field, context);
                 } else if (cache[field.source.url] === undefined) {
                     const promise = getConfigApiValues(field.source.url, 0, 1000).then(response => {
                         cache[field.source.url] = response.data;
-                        return this.processOptions(response.data, field);
+                        return processOptions(response.data, field);
                     });
                     cache[field.source.url] = promise;
                     return promise;
@@ -212,114 +204,114 @@ export default class CreateEditConfigForm extends React.Component {
                     if (cache[field.source.url] instanceof Promise) {
                         return cache[field.source.url].then(() => {});
                     }
-                    return this.processOptions(cache[field.source.url], field);
+                    return processOptions(cache[field.source.url], field);
                 } else {
                     console.error('Cannot load dropdown values from: ', field.source.url);
                 }
             } else {
                 if (field.type === 'array' || field.type === 'object') {
-                    field.misc.fields.forEach(subfield => this.optionsHandler(subfield.id, field.misc.fields, context));
+                    field.misc.fields.forEach(subfield => optionsHandler(subfield.id, field.misc.fields, context));
                 }
             }
         }
         return null;
-    }
-
-    handleDeleteItem = item => {
-        this.props.onRemoveItem(item);
-        this.props.onCancel();
     };
 
-    toggle = () => {
-        this.setState(previousState => ({
-            dropdownOpen: !previousState.dropdownOpen,
-        }));
+    const handleDeleteItem = item => {
+        onRemoveItem(item);
+        onCancel();
     };
 
-    render() {
-        return (
-            <Modal
-                isOpen={!!this.props.value}
-                toggle={this.props.onCancel}
-                style={{paddingLeft: '30px', maxWidth: '650px'}}
-            >
-                <ModalBody>
-                    <p>
-                        <b style={{color: '#999', fontSize: '11px', textTransform: 'uppercase'}}>
-                            {this.props.displayName}
-                        </b>
-                    </p>
-                    <p style={{marginTop: '-4px'}}>
-                        <b>
-                            {this.props.value && this.props.label ? (
-                                this.props.label
-                            ) : (
-                                <i style={{fontSize: '20px', color: '#666'}}>New {this.props.displayName}</i>
-                            )}
-                        </b>
-                    </p>
-                    <Can I="delete" a="ConfigUI">
-                        {Object.entries(this.props.value).length !== 0 && (
-                            <div style={{position: 'absolute', top: '20px', right: '20px', cursor: 'pointer'}}>
-                                <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
-                                    <DropdownToggle color="light">
-                                        <b>...</b>
-                                    </DropdownToggle>
-                                    <DropdownMenu>
-                                        <DropdownItem onClick={() => this.handleDeleteItem(this.props.value)}>
-                                            Delete
-                                        </DropdownItem>
-                                    </DropdownMenu>
-                                </Dropdown>
-                            </div>
-                        )}
-                    </Can>
-                    <Form
-                        renderer={(field, onChange, onFieldFocus, onFieldBlur) =>
-                            this.renderer(field, onChange, onFieldFocus, onFieldBlur)
-                        }
-                        defaultFields={this.props.schema}
-                        optionsHandler={this.optionsHandler}
-                        onChange={value => this.setState({value: value})}
-                        defaultValue={this.state.value}
-                        validationHandler={(field, fields, parentContext) => {
-                            //if regular array of objects
-                            if (field.type === 'array' && field.misc.fields.length > 1 && !field.dynamic) {
-                                //get the array
-                                const currentValue =
-                                    get(this.state.value, field.name, []) || get(this.state.value, field.id, []);
-                                let isValid = true;
-                                let index = 0;
-                                //for each element of the array
-                                while (isValid && index < currentValue.length) {
-                                    //create a virtual form (list of fields with schema props and values)
-                                    let toValidate = cloneDeep(field.misc);
-                                    toValidate.showValidationBeforeTouched = true;
-                                    //populate all fields with current values
-                                    for (let j = 0; j < toValidate.fields.length; j++) {
-                                        toValidate.fields[j].visible = true;
-                                        const fieldValue = currentValue[index][toValidate.fields[j].id];
-                                        if (fieldValue || fieldValue === 0 || fieldValue === false) {
-                                            toValidate.fields[j].value = fieldValue;
-                                        }
+    const toggle = () => {
+        setDropdownOpen(!dropdownOpen);
+    };
+
+    return (
+        <Modal isOpen={!!value} toggle={onCancel} style={{paddingLeft: '30px', maxWidth: '650px'}}>
+            <ModalBody>
+                <p>
+                    <b style={{color: '#999', fontSize: '11px', textTransform: 'uppercase'}}>{displayName}</b>
+                </p>
+                <p style={{marginTop: '-4px'}}>
+                    <b>{value && label ? label : <i style={{fontSize: '20px', color: '#666'}}>New {displayName}</i>}</b>
+                </p>
+                <Can I="delete" a="ConfigUI">
+                    {Object.entries(value).length !== 0 && (
+                        <div style={{position: 'absolute', top: '20px', right: '20px', cursor: 'pointer'}}>
+                            <Dropdown isOpen={dropdownOpen} toggle={toggle}>
+                                <DropdownToggle color="light">
+                                    <b>...</b>
+                                </DropdownToggle>
+                                <DropdownMenu>
+                                    <DropdownItem onClick={() => handleDeleteItem(value)}>Delete</DropdownItem>
+                                </DropdownMenu>
+                            </Dropdown>
+                        </div>
+                    )}
+                </Can>
+                <Form
+                    renderer={(field, onChange, onFieldFocus, onFieldBlur) =>
+                        renderer(field, onChange, onFieldFocus, onFieldBlur)
+                    }
+                    defaultFields={schema}
+                    optionsHandler={optionsHandler}
+                    onChange={setVal}
+                    defaultValue={val}
+                    validationHandler={field => {
+                        //if regular array of objects
+                        if (field.type === 'array' && field.misc.fields.length > 1 && !field.dynamic) {
+                            //get the array
+                            const currentValue = get(val, field.name, []) || get(val, field.id, []);
+                            let isValid = true;
+                            let index = 0;
+                            //for each element of the array
+                            while (isValid && index < currentValue.length) {
+                                //create a virtual form (list of fields with schema props and values)
+                                let toValidate = cloneDeep(field.misc);
+                                toValidate.showValidationBeforeTouched = true;
+                                //populate all fields with current values
+                                for (let j = 0; j < toValidate.fields.length; j++) {
+                                    toValidate.fields[j].visible = true;
+                                    const fieldValue = currentValue[index][toValidate.fields[j].id];
+                                    if (fieldValue || fieldValue === 0 || fieldValue === false) {
+                                        toValidate.fields[j].value = fieldValue;
                                     }
-                                    //validate the virtual form
-                                    //if valid check the next object in array
-                                    //until one invalid found or all checked
-                                    isValid = !validateAllFields(toValidate).find(({isValid}) => !isValid);
-                                    index++;
                                 }
-                                return isValid ? '' : 'Invalid';
+                                //validate the virtual form
+                                //if valid check the next object in array
+                                //until one invalid found or all checked
+                                isValid = !validateAllFields(toValidate).find(({isValid}) => !isValid);
+                                index++;
                             }
-                        }}
-                    >
-                        <ModalFooter>
-                            <Button onClick={this.props.onCancel}>Cancel</Button>
-                            <FormButton onClick={this.props.onSubmit} />
-                        </ModalFooter>
-                    </Form>
-                </ModalBody>
-            </Modal>
-        );
-    }
-}
+                            return isValid ? '' : 'Invalid';
+                        }
+                    }}
+                >
+                    <ModalFooter>
+                        <Button onClick={onCancel}>Cancel</Button>
+                        <Button appearance="primary" onClick={() => onSubmit(val)}>
+                            OK
+                        </Button>
+                    </ModalFooter>
+                </Form>
+            </ModalBody>
+        </Modal>
+    );
+};
+
+CreateEditConfigForm.propTypes = {
+    value: PropTypes.oneOfType([PropTypes.object, PropTypes.array, PropTypes.string]).isRequired,
+    onRemoveItem: PropTypes.func.isRequired,
+    onCancel: PropTypes.func.isRequired,
+    schema: PropTypes.oneOfType([PropTypes.object, PropTypes.array]).isRequired,
+    onSubmit: PropTypes.func.isRequired,
+    displayName: PropTypes.string,
+    label: PropTypes.string,
+};
+
+CreateEditConfigForm.defaultProps = {
+    displayName: '',
+    label: '',
+};
+
+export default CreateEditConfigForm;
