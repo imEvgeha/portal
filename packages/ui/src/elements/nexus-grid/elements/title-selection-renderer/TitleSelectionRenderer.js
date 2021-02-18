d2a1103e589ccca9f620dacd9bb4b6c0a7af2e18
Madhurima Kandul <madhkand@amdocs.com>
2021-02-18 12:57:38 +0200

    PORT-3931: add styling

--- packages/ui/src/elements/nexus-grid/elements/title-selection-renderer/TitleSelectionRenderer.js
+++ packages/ui/src/elements/nexus-grid/elements/title-selection-renderer/TitleSelectionRenderer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './TitleSelectionRenderer.scss';
 
 const TitleSelectionRenderer = ({value, column, node, isNexusDisabled, selectionType}) => {
     const checkedHandler = event => {
