59a931495daae302166d33dc2bcb9514e2bc93ec
Antonia Nikolaou <antonini@amdocs.com>
2021-02-23 14:59:06 +0200

    PORT-3920: Create dropdowns

--- src/pages/avails/avails-table-release-report/AvailsTableReleaseReport.js
+++ src/pages/avails/avails-table-release-report/AvailsTableReleaseReport.js
@@ -1,22 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import Button from '@atlaskit/button';
 import DropdownMenu from '@atlaskit/dropdown-menu';
+import Select from '@atlaskit/select';
+import moment from 'moment';
 import {connect} from 'react-redux';
-import {CREATE_REPORT} from './constants';
+import {CREATE_REPORT, MONTHS, SELECT_MONTH, SELECT_YEAR, YEARS} from './constants';
 import './AvailsTableReleaseReport.scss';
 
 const AvailsTableReleaseReport = ({}) => {
-    const onCreate = () => {};
+    const [selectedMonth, setSelectedMonth] = useState('');
+    const [selectedYear, setSelectedYear] = useState('');
+
+    const onCreateReport = () => {
+        const monthNumber = moment().month(selectedMonth).format('MM');
+        console.log(monthNumber);
+    };
 
     return (
         <div className="nexus-c-right-repository-release-report">
             <DropdownMenu className="nexus-c-button" trigger="New Release Report" triggerType="button">
                 <div className="nexus-c-right-repository-release-report-content">
+                    <Select
+                        className="nexus-c-right-repository-release-report__year-dropdown"
+                        placeholder={SELECT_YEAR}
+                        onChange={val => setSelectedYear(val.value)}
+                        options={YEARS.map(m => ({value: m, label: m}))}
+                    />
+                    <Select
+                        className="nexus-c-right-repository-release-report__month-dropdown"
+                        placeholder={SELECT_MONTH}
+                        onChange={val => setSelectedMonth(val.value)}
+                        options={MONTHS.map(m => ({value: m, label: m}))}
+                    />
                     <Button
                         className="nexus-c-right-repository-release-report-content__btn"
-                        onClick={onCreate}
+                        onClick={onCreateReport}
                         appearance="primary"
+                        shouldFitContainer
                     >
                         {CREATE_REPORT}
                     </Button>
--- src/pages/avails/avails-table-release-report/constants.js
+++ src/pages/avails/avails-table-release-report/constants.js
@@ -1,1 +1,18 @@
 export const CREATE_REPORT = 'Create Report';
+export const SELECT_YEAR = 'Select year';
+export const YEARS = [2019, 2020, 2021, 2022];
+export const SELECT_MONTH = 'Select month';
+export const MONTHS = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+];
