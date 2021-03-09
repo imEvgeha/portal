2839623d772522f4b69132813a40a9ee459cbe6e
Luka-Krsmanovic <luka.krsmanovic@computerrock.com>
2021-03-09 12:36:28 +0100

    PORT-3953 adjusting solution, removed promise.all

--- src/pages/legacy/containers/avail/dashboard/AuditHistory.js
+++ src/pages/legacy/containers/avail/dashboard/AuditHistory.js
@@ -1,40 +1,33 @@
 import React, {useState, useEffect} from 'react';
 import PropTypes from 'prop-types';
-import NexusSpinner from '@vubiquity-nexus/portal-ui/lib/elements/nexus-spinner/NexusSpinner';
 import {getRightsHistory} from '../../../../avails/availsService';
 import AuditHistoryTable from '../../../components/AuditHistoryTable/AuditHistoryTable';
 
 const AuditHistory = ({selectedRights}) => {
-    const [eventsHistory, setEventsHistory] = useState(null);
+    const [eventsHistory, setEventsHistory] = useState({});
     useEffect(() => {
-        //instead of promise.all loop through selectedRights
-        Promise.all(
-            selectedRights.map(right => {
-                return getRightsHistory(right.id);
-            })
-        ).then(responses => {
-            setEventsHistory(responses);
+        selectedRights.forEach(right => {
+            getRightsHistory(right.id).then(response => {
+                setEventsHistory(prevState => {
+                    const updatedEventsHistory = {...prevState};
+                    updatedEventsHistory[right.id] = response;
+                    return updatedEventsHistory;
+                });
+            });
         });
     }, [selectedRights]);
 
     const buildModalContent = () => {
-        if (eventsHistory) {
-            return (
-                <div>
-                    {selectedRights.map((right, index) => (
-                        <AuditHistoryTable key={right.id} focusedRight={right} data={eventsHistory[index]} />
-                    ))}
-                </div>
-            );
-        } else {
-            return selectedRights.map((right, index) => (
-                <div key={index}>
-                    <NexusSpinner />
-                </div>
-            ));
-        }
+        return (
+            <div>
+                {selectedRights.map(right => (
+                    <AuditHistoryTable key={right.id} focusedRight={right} data={eventsHistory[right.id]} />
+                ))}
+            </div>
+        );
     };
 
+    console.log(eventsHistory);
     return buildModalContent();
 };
 
