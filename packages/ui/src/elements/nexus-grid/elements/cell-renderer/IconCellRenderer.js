01aad2f747c8de4b2db97ed909babe4d5c64662f
Antonia Nikolaou <antonini@amdocs.com>
2021-04-13 16:15:59 +0300

    PORT-3819: Add iconcellrenderer

---
+++ packages/ui/src/elements/nexus-grid/elements/cell-renderer/IconCellRenderer.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import loadingGif from '@vubiquity-nexus/portal-assets/img/loading.gif';
+import NexusTooltip from '@vubiquity-nexus/portal-ui/lib/elements/nexus-tooltip/NexusTooltip';
+import {getDeepValue} from '@vubiquity-nexus/portal-utils/lib/Common';
+import {getIcon} from '../value-formatter/createValueFormatter';
+
+const IconCellRenderer = params => {
+    const {
+        data,
+        colDef,
+        colDef: {field},
+        valueToDisplay = null,
+        icon = '',
+        tooltip = '',
+    } = params;
+    if (!data && colDef !== 'actions') {
+        return <img src={loadingGif} alt="loadingSpinner" />;
+    }
+
+    const value = getDeepValue(data, field);
+
+    return (!valueToDisplay && value) || (valueToDisplay && value === valueToDisplay) ? (
+        <NexusTooltip content={tooltip}>
+            <span className="nexus-c-repository-icon">{getIcon(icon, false)}</span>
+        </NexusTooltip>
+    ) : (
+        ' '
+    );
+};
+export default IconCellRenderer;
