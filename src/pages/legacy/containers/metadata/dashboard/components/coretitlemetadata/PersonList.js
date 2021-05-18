6a7c347bc16f4bb09b970a8f4dcecb99ee6dded7
Madhurima Kandul <madhkand@amdocs.com>
2021-05-18 17:43:57 +0300

    PORT-4209: fix rearranging credits

--- src/pages/legacy/containers/metadata/dashboard/components/coretitlemetadata/PersonList.js
+++ src/pages/legacy/containers/metadata/dashboard/components/coretitlemetadata/PersonList.js
@@ -72,7 +72,7 @@ class PersonList extends React.Component {
         const result = Array.from(list);
         const [removed] = result.splice(startIndex, 1);
         result.splice(endIndex, 0, removed);
-
+        result.forEach((c, i) => (c.creditsOrder = i));
         return result;
     };
     onDragEnd = result => {
