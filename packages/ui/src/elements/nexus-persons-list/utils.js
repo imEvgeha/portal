84bac6c52c89bdaa90614e69e96779941be093dd
Antonia Nikolaou <antonini@amdocs.com>
2021-06-01 15:26:40 +0300

    PORT-4023: Add personType in api

--- packages/ui/src/elements/nexus-persons-list/utils.js
+++ packages/ui/src/elements/nexus-persons-list/utils.js
@@ -1,7 +1,7 @@
 import {getFilteredCastList, getFilteredCrewList} from '@vubiquity-nexus/portal-utils/lib/castCrewUtils';
 import {CAST, PERSONS_PER_REQUEST, MIN_CHARS_FOR_SEARCHING} from './constants';
 
-const handleResponse = resTable => {
+const handleResponse = (resTable, searchText) => {
     if (resTable && resTable.length > 0) {
         return resTable.map(e => {
             return {
@@ -14,7 +14,7 @@ const handleResponse = resTable => {
     }
     return {
         id: 'create',
-        name: 'Create New +',
+        name: `+ Create "${searchText}"`,
         original: 'create',
     };
 };
@@ -25,11 +25,11 @@ export const loadOptions = (uiConfig, searchText, searchPerson) => {
     if (type === CAST) {
         return searchPerson(searchText, PERSONS_PER_REQUEST, type, true).then(res => {
             const resTable = getFilteredCastList(res.data, true, true);
-            return handleResponse(resTable);
+            return handleResponse(resTable, searchText);
         });
     }
     return searchPerson(searchText, PERSONS_PER_REQUEST, type).then(res => {
         const resTable = getFilteredCrewList(res.data, true);
-        return handleResponse(resTable);
+        return handleResponse(resTable, searchText);
     });
 };
