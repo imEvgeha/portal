bc8b5b0af447e691ec5b98f49e46106c4a5e1af5
Antonia Nikolaou <antonini@amdocs.com>
2021-06-01 14:49:42 +0300

    PORT-4023: Add create new in crew

--- packages/ui/src/elements/nexus-persons-list/utils.js
+++ packages/ui/src/elements/nexus-persons-list/utils.js
@@ -1,37 +1,35 @@
 import {getFilteredCastList, getFilteredCrewList} from '@vubiquity-nexus/portal-utils/lib/castCrewUtils';
 import {CAST, PERSONS_PER_REQUEST, MIN_CHARS_FOR_SEARCHING} from './constants';
 
-export const loadOptions = (uiConfig, searchText, searchPerson) => {
-    const {type} = uiConfig;
-    if (searchText.length < MIN_CHARS_FOR_SEARCHING) return [];
-    if (type === CAST) {
-        return searchPerson(searchText, PERSONS_PER_REQUEST, type, true).then(res => {
-            const resTable = getFilteredCastList(res.data, true, true);
-            if (resTable && resTable.length > 0) {
-                return getFilteredCastList(res.data, true, true).map(e => {
-                    return {
-                        id: e.id,
-                        name: e.displayName,
-                        byline: e.personType.toString().toUpperCase(),
-                        original: JSON.stringify(e),
-                    };
-                });
-            }
-            return {
-                id: 'create',
-                name: 'Create New +',
-                original: 'create',
-            };
-        });
-    }
-    return searchPerson(searchText, PERSONS_PER_REQUEST, type).then(res =>
-        getFilteredCrewList(res.data, true).map(e => {
+const handleResponse = resTable => {
+    if (resTable && resTable.length > 0) {
+        return resTable.map(e => {
             return {
                 id: e.id,
                 name: e.displayName,
                 byline: e.personType.toString().toUpperCase(),
                 original: JSON.stringify(e),
             };
-        })
-    );
+        });
+    }
+    return {
+        id: 'create',
+        name: 'Create New +',
+        original: 'create',
+    };
+};
+
+export const loadOptions = (uiConfig, searchText, searchPerson) => {
+    const {type} = uiConfig;
+    if (searchText.length < MIN_CHARS_FOR_SEARCHING) return [];
+    if (type === CAST) {
+        return searchPerson(searchText, PERSONS_PER_REQUEST, type, true).then(res => {
+            const resTable = getFilteredCastList(res.data, true, true);
+            return handleResponse(resTable);
+        });
+    }
+    return searchPerson(searchText, PERSONS_PER_REQUEST, type).then(res => {
+        const resTable = getFilteredCrewList(res.data, true);
+        return handleResponse(resTable);
+    });
 };
