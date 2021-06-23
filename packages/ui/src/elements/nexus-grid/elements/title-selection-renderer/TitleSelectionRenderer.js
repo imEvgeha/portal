9c00f2efee2288e49186c331d2b581261bd0447f
Theo Macris <tmacris@vubiquity.net>
2021-06-23 23:06:27 +0300

    PORT-4578: Revert onChange to onClick

--- packages/ui/src/elements/nexus-grid/elements/title-selection-renderer/TitleSelectionRenderer.js
+++ packages/ui/src/elements/nexus-grid/elements/title-selection-renderer/TitleSelectionRenderer.js
@@ -14,7 +14,7 @@ const TitleSelectionRenderer = ({value, column, node, isNexusDisabled, selection
         <input
             type={selectionType}
             className={`title-selection-${selectionType}__${value ? 'selected' : 'unselected'}`}
-            onChange={checkedHandler}
+            onClick={checkedHandler}
             checked={value}
         />
     );
