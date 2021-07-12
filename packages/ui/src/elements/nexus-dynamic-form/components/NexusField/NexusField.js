f29a4cbe4205231c3f41afd0062ce2732bf8946d
Antonia Nikolaou <antonini@amdocs.com>
2021-07-12 10:36:38 +0300

    PORT-4445: getLabel for localizes

--- packages/ui/src/elements/nexus-dynamic-form/components/NexusField/NexusField.js
+++ packages/ui/src/elements/nexus-dynamic-form/components/NexusField/NexusField.js
@@ -84,6 +84,8 @@ const NexusField = ({
         maxLength,
     };
 
+    const emetLanguage = get(formData, 'editorial.language');
+
     const getIsReadOnly = () => {
         return (isReadOnlyInEdit && view === VIEWS.EDIT) || isReadOnly;
     };
@@ -194,35 +196,33 @@ const NexusField = ({
                 ) {
                     multiselectFieldProps.value = fieldProps.value.map(val => ({label: val, value: val}));
                 }
-                const emetLanguage = get(formData,'editorial.language');
-                if(showLocalized === true && emetLanguage !== 'en' && !selectLocalizedValues) {
-                    selectLocalizedValues = Object.assign({}, selectValues);
-                        const newValues = selectLocalizedValues[path].map(item => {
-                            const localLang = item.localizations.find(local => local.language === emetLanguage);
-                            const enName = item.name; // name field in genre object
-                            if(localLang) {
-                                item.displayName = `${localLang.language}(${enName})`
-                            }
-                            else {
-                                if(emetLanguage)
-                                    item.displayName = `(${enName})*`
-                                else
-                                    item.displayName = enName;
-                            }
-                            return item;
-                        });
-                        selectLocalizedValues[path] = newValues;
-                        // displayName is used in dropdown for display purpose only. to send to api, use "name"
-                        newOptionsConfig = { defaultLabelPath: "displayName", defaultValuePath: "name"}
 
+                if (showLocalized === true && emetLanguage !== 'en' && !selectLocalizedValues) {
+                    selectLocalizedValues = Object.assign({}, selectValues);
+                    const newValues = selectLocalizedValues[path].map(item => {
+                        const localLang = item.localizations.find(local => local.language === emetLanguage);
+                        const enName = item.name; // name field in genre object
+                        if (localLang) {
+                            item.displayName = `${localLang.language}(${enName})`;
+                        } else if (emetLanguage) item.displayName = `(${enName})*`;
+                            else item.displayName = enName;
+                        return item;
+                    });
+                    selectLocalizedValues[path] = newValues;
+                    // displayName is used in dropdown for display purpose only. to send to api, use "name"
+                    newOptionsConfig = {defaultLabelPath: 'displayName', defaultValuePath: 'name'};
                 }
 
                 return (
                     <NexusSelect
                         fieldProps={multiselectFieldProps}
                         type={type}
-                        optionsConfig={showLocalized === true && emetLanguage !== 'en' ? newOptionsConfig : optionsConfig}
-                        selectValues={showLocalized === true && emetLanguage !== 'en' ? selectLocalizedValues : selectValues}
+                        optionsConfig={
+                            showLocalized === true && emetLanguage !== 'en' ? newOptionsConfig : optionsConfig
+                        }
+                        selectValues={
+                            showLocalized === true && emetLanguage !== 'en' ? selectLocalizedValues : selectValues
+                        }
                         path={path}
                         isRequired={isRequired}
                         isMultiselect={true}
@@ -300,28 +300,43 @@ const NexusField = ({
     };
 
     const hasLocalizedValue = value => {
-        if(typeof value === 'string' && value.includes('(') && value.includes(')*'))
-            return false;
+        if (typeof value === 'string' && value.includes('(') && value.includes(')*')) return false;
         return true;
-    }
+    };
+
+    const getLabel = item => {
+        if (typeof item === 'object' && localizationConfig) {
+            return emetLanguage === 'en' ? item[localizationConfig.default] : item[localizationConfig.localized];
+        } 
+            return typeof item === 'object' ? item.label : item;
+        
+    };
 
     const getValue = fieldProps => {
         if (Array.isArray(fieldProps.value)) {
             if (fieldProps.value.length) {
-                const arrayValues = fieldProps?.value?.map(item => typeof item === 'object'? item.label : item);
-                if(showLocalized) {
-                    return <div>
-                        {arrayValues.map((item,index) =>{
-                            if(!hasLocalizedValue(item)) {
+                const arrayValues = fieldProps?.value?.map(item => getLabel(item));
+                if (showLocalized) {
+                    return (
+                        <div>
+                            {arrayValues.map((item, index) => {
+                                if (!hasLocalizedValue(item)) {
+                                    return (
+                                        <span key={index} title={LOCALIZED_VALUE_NOT_DEFINED} className="italic">
+                                            {item}
+                                            {index !== arrayValues.length - 1 && ', '}
+                                        </span>
+                                    );
+                                }
                                 return (
-                                <span key={index} title={LOCALIZED_VALUE_NOT_DEFINED} className="italic">
-                                    {item}{index !== arrayValues.length -1 && ", "}
-                                </span>
-                                )
-                            }
-                            return <span key={index}>{item}{index !== arrayValues.length -1 && ", "} </span>
-                        })}
-                    </div>
+                                    <span key={index}>
+                                        {item}
+                                        {index !== arrayValues.length - 1 && ', '}{' '}
+                                    </span>
+                                );
+                            })}
+                        </div>
+                    );
                 }
                 return fieldProps.value.map(x => x && getFieldValue(x)).join(', ');
             }
@@ -484,7 +499,7 @@ NexusField.propTypes = {
     isRequiredVZ: PropTypes.bool,
     oneIsRequiredVZ: PropTypes.bool,
     showLocalized: PropTypes.bool,
-    localizationConfig: PropTypes.object
+    localizationConfig: PropTypes.object,
 };
 
 NexusField.defaultProps = {
@@ -522,8 +537,7 @@ NexusField.defaultProps = {
     isRequiredVZ: false,
     oneIsRequiredVZ: false,
     showLocalized: false,
-    localizationConfig: {}
-
+    localizationConfig: {},
 };
 
 export default NexusField;
