75366bf7f1c1a79de3721afa9bbb2f742c040c2f
Evgeniy Lyashenko <elyashenko@vubiquity.net>
2021-10-07 17:49:44 +0300

    Bugfix/port 4986 (#2154)
    * PORT-5143: propagate dialogue changes
    
    * PORT-5073: fixed bug with rightId field (#2147)
    
    * PORT-5138: added info about auto-decorate length (#2148)
    
    * PORT-4986: Added the ability to open navbar links in new tabs
    
    * Code optimized

--- packages/ui/src/elements/nexus-navigation/components/NavigationItems.js
+++ packages/ui/src/elements/nexus-navigation/components/NavigationItems.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import DetailViewIcon from '@atlaskit/icon/glyph/detail-view';
 import EditorBulletListIcon from '@atlaskit/icon/glyph/editor/bullet-list';
 import EditorMediaWrapRightIcon from '@atlaskit/icon/glyph/editor/media-wrap-right';
@@ -7,9 +8,30 @@ import TrayIcon from '@atlaskit/icon/glyph/tray';
 import NexusNavIcon from '@vubiquity-nexus/portal-assets/nexus-nav-icon.svg';
 import {can} from '@vubiquity-nexus/portal-utils/lib/ability';
 import {AVAILS, SERVICING_ORDERS, EVENT_MANAGEMENT, DOP_TASKS, TITLE_METADATA} from '../constants';
+import './NavigationItems.scss';
+
+export const ComponentWrapper = ({link, handleClick, children}) => {
+    return (
+        <a
+            href={link}
+            onClick={e => {
+                handleClick(link);
+                e.preventDefault();
+            }}
+        >
+            <button className="navigation-button">{children}</button>
+        </a>
+    );
+};
 
 export const navigationPrimaryItems = (selectedItem, handleClick) => {
     const canReadEventManager = can('read', 'EventManagement');
+    const generateComponent = (link, Icon) => (
+        <ComponentWrapper handleClick={handleClick} link={link}>
+            <Icon />
+        </ComponentWrapper>
+    );
+    const DopIcon = () => <EditorBulletListIcon size="large" />;
 
     return [
         {
@@ -18,6 +40,7 @@ export const navigationPrimaryItems = (selectedItem, handleClick) => {
         },
         {
             icon: TrayIcon,
+            component: () => generateComponent(AVAILS, TrayIcon),
             id: AVAILS,
             tooltip: AVAILS,
             isSelected: selectedItem === AVAILS,
@@ -25,6 +48,7 @@ export const navigationPrimaryItems = (selectedItem, handleClick) => {
         },
         {
             icon: EditorMediaWrapRightIcon,
+            component: () => generateComponent('metadata', EditorMediaWrapRightIcon),
             id: TITLE_METADATA,
             tooltip: 'Title Metadata',
             isSelected: selectedItem === TITLE_METADATA,
@@ -32,6 +56,7 @@ export const navigationPrimaryItems = (selectedItem, handleClick) => {
         },
         {
             icon: () => <EditorBulletListIcon size="large" />,
+            component: () => generateComponent(DOP_TASKS, DopIcon),
             id: DOP_TASKS,
             tooltip: 'DOP Tasks',
             isSelected: selectedItem === DOP_TASKS,
@@ -39,6 +64,7 @@ export const navigationPrimaryItems = (selectedItem, handleClick) => {
         },
         {
             icon: DetailViewIcon,
+            component: () => generateComponent(SERVICING_ORDERS, DetailViewIcon),
             id: SERVICING_ORDERS,
             tooltip: 'Servicing Orders',
             isSelected: selectedItem === SERVICING_ORDERS,
@@ -48,6 +74,7 @@ export const navigationPrimaryItems = (selectedItem, handleClick) => {
             ? [
                   {
                       icon: RecentIcon,
+                      component: () => generateComponent(EVENT_MANAGEMENT, RecentIcon),
                       id: EVENT_MANAGEMENT,
                       tooltip: 'Event Management',
                       isSelected: selectedItem === EVENT_MANAGEMENT,
@@ -56,4 +83,14 @@ export const navigationPrimaryItems = (selectedItem, handleClick) => {
               ]
             : []),
     ];
+};
+
+ComponentWrapper.propTypes = {
+    link: PropTypes.string,
+    handleClick: PropTypes.func,
+};
+
+ComponentWrapper.defaultProps = {
+    link: '',
+    handleClick: () => null,
 };
