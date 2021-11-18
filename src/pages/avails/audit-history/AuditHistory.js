01813524ee4c70cd71cf3fde712889c26c489c2c
Antonia Nikolaou <antonini@amdocs.com>
2021-11-18 12:38:44 +0200

    PORT-4191: empty

--- src/pages/avails/audit-history/AuditHistory.js
+++ src/pages/avails/audit-history/AuditHistory.js
@@ -20,6 +20,7 @@ const AuditHistory = ({selectedRights}) => {
     const buildModalContent = () => {
         return (
             <div>
+                <div>ok</div>
                 {selectedRights.map(right => (
                     <AuditHistoryTable key={right.id} focusedRight={right} data={eventsHistory[right.id]} />
                 ))}
