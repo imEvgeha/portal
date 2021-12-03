ad8358bb6e28ff8c85b574156edd0d7a3a14ad23
elyashenko <elyashenko@vubiquity.net>
2021-12-03 13:49:48 +0200

    PORT-4181: Code optimized

--- packages/ui/src/elements/nexus-grid/elements/cell-editor/DropdownCellEditor.js
+++ packages/ui/src/elements/nexus-grid/elements/cell-editor/DropdownCellEditor.js
@@ -114,7 +114,6 @@ class DropdownCellEditor extends Component {
 
     render() {
         const {value} = this.state;
-        // const filteredValue = value.filter(item => item.isDisabled && item.withdrawn);
 
         return (
             <div className="nexus-c-dropdown-cell-editor">
