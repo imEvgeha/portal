7f548587103ff46880decdf8c9793dcdba84804d
pdemetriou <Vubiquity.765>
2021-12-13 15:25:39 +0200

    feature/PORT-5396: Fix issue with tests

--- src/setupTestEnv.js
+++ src/setupTestEnv.js
@@ -1,6 +1,8 @@
 import {configure} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import 'babel-polyfill';
+// import 'babel-polyfill';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
 
 configure({adapter: new Adapter()});
 
