5aef177ed2759808bdfb3c643bd34848de6a2eaa
elyashenko <elyashenko@vubiquity.net>
2021-12-20 13:16:36 +0200

    PORT-5415: Design optimized, new modal implemented for download metadata

--- src/pages/title-metadata/components/title-metadata-header/components/DownloadEmetModal/DownloadEmetModal.js
+++ src/pages/title-metadata/components/title-metadata-header/components/DownloadEmetModal/DownloadEmetModal.js
@@ -1,20 +1,13 @@
-import React, {useState} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import {downloadFile} from '@vubiquity-nexus/portal-utils/lib/Common';
-import {Button} from 'primereact/button';
 import {Dropdown} from 'primereact/dropdown';
 import {connect} from 'react-redux';
-import {exportService} from '../../../../../legacy/containers/avail/service/ExportService';
-import {createInitialValues} from '../utils';
 import {createLanguagesSelector, createCountrySelector} from './downloadEmetModalSelectors';
-import {downloadFormSubtitle, downloadFormFields, cancelButton, downloadButton} from '../constants';
+import {downloadFormSubtitle, downloadFormFields} from '../constants';
 import './DownloadEmetModal.scss';
 
-const DownloadEmetModal = ({closeModal, languages, locale, showSuccess}) => {
-    const initialValues = createInitialValues(downloadFormFields);
-    const [values, setValues] = useState(initialValues);
+const DownloadEmetModal = ({languages, locale, values, setValues}) => {
 
-    const isDisabled = values ? !Object.values(values).every(value => Boolean(value) === true) : true;
     const buildField = field => {
         const {name, placeholder} = field;
         const updatedPlaceholder = `Select ${placeholder}...`;
@@ -54,7 +47,7 @@ const DownloadEmetModal = ({closeModal, languages, locale, showSuccess}) => {
                         options={getOptions().sort(selectCompare)}
                         onChange={handleChange}
                         optionLabel="label"
-                        filterBy="label"
+                        filterBy="label,value"
                         placeholder={updatedPlaceholder}
                         filter={!isItStatus}
                     />
@@ -63,39 +56,12 @@ const DownloadEmetModal = ({closeModal, languages, locale, showSuccess}) => {
         );
     };
 
-    const handleDownload = () => {
-        exportService
-            .bulkExportMetadata(values)
-            .then(response => {
-                const buffer = new Uint8Array(response.value).buffer;
-                const buftype = 'application/vnd.ms-excel;charset=utf-8';
-                const blob = new Blob([buffer], {type: buftype});
-                showSuccess();
-                closeModal()
-                downloadFile(blob, 'Editorial_Metadata');
-            }).catch(err => err)
-    };
 
     return (
         <div className="nexus-c-download-emet-modal">
             <h5 className="nexus-c-download-emet-modal__subtitle">{downloadFormSubtitle}</h5>
             <div className="nexus-c-download-emet-modal__fields">
                 {downloadFormFields.map(field => buildField(field))}
-            </div>
-            <div className="nexus-c-download-emet-modal__buttons">
-                <Button
-                    onClick={() => {
-                        closeModal();
-                    }}
-                    className="p-button-outlined p-button-secondary nexus-c-cancel-button"
-                    label={cancelButton}
-                />
-                <Button
-                    label={downloadButton}
-                    onClick={handleDownload}
-                    className="p-button-outlined p-button-secondary nexus-c-download-emet-modal__button"
-                    disabled={isDisabled}
-                />
             </div>
         </div>
     );
@@ -111,10 +77,10 @@ const createMapStateToProps = () => {
 };
 
 DownloadEmetModal.propTypes = {
-    closeModal: PropTypes.func.isRequired,
     languages: PropTypes.array.isRequired,
     locale: PropTypes.array.isRequired,
-    showSuccess: PropTypes.func.isRequired,
+    values: PropTypes.array.isRequired,
+    setValues: PropTypes.func.isRequired,
 };
 
 export default connect(createMapStateToProps, null)(DownloadEmetModal);
