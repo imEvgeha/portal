72ab6a488533aad0ddd755db2810b1a9fecc96b7
dpjetlovic <dpjetlovic@vubiquity.net>
2021-12-23 13:33:20 +0100

    PORT-5337: Replaced clean object function

--- src/pages/title-metadata/utils.js
+++ src/pages/title-metadata/utils.js
@@ -400,15 +400,13 @@ const handleDirtyRatingsValues = values => {
     }
 };
 
-const cleanObject = object => {
-    Object.entries(object).forEach(([k, v]) => {
-        if (v && typeof v === 'object') cleanObject(v);
-        if ((v && typeof v === 'object' && !Object.keys(v).length) || v === null || v === undefined || v.length === 0) {
-            if (Array.isArray(object)) object.splice(k, 1);
-            else if (!(v instanceof Date)) delete object[k];
-        }
-    });
-    return object;
+// remove undefined and null values from object
+const cleanObject = obj => {
+    return JSON.parse(
+        JSON.stringify(obj, (key, value) => {
+            return value === null ? undefined : value;
+        })
+    );
 };
 
 const handleDirtyEMETValues = (initialValues, values) => {
