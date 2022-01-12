9c0d1264407a9f40e6c392c3de28a2868c18dc05
elyashenko <elyashenko@vubiquity.net>
2022-01-12 16:24:41 +0200

    PORT-5461: Toast and debouncing created for download file from upload log

--- packages/ui/src/elements/nexus-grid/elements/cell-renderer/IconCellRenderer.js
+++ packages/ui/src/elements/nexus-grid/elements/cell-renderer/IconCellRenderer.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import loadingGif from '@vubiquity-nexus/portal-assets/img/loading.gif';
 import NexusTooltip from '@vubiquity-nexus/portal-ui/lib/elements/nexus-tooltip/NexusTooltip';
 import {downloadFile, getDeepValue} from '@vubiquity-nexus/portal-utils/lib/Common';
+import { debounce } from 'lodash';
+import { Toast } from 'primereact/toast';
+import { store } from '../../../../../../../src';
 import { downloadUploadedEMETLog } from '../../../../../../../src/pages/title-metadata/service/UploadLogService';
+import { SUCCESS_ICON } from '../../../../../lib/elements/nexus-toast-notification/constants';
+import { addToast } from '../../../../../lib/toast/toastActions';
 import {getIcon} from '../value-formatter/createValueFormatter';
 import './IconCellRenderer.scss';
 
 const IconCellRenderer = params => {
+    const toast = useRef(null);
     const {
         data,
         colDef: {field},
@@ -21,19 +27,31 @@ const IconCellRenderer = params => {
     const value = getDeepValue(data, field);
     const idToFileDownloading = data.reportId;
 
-    const handleClick = () => {
-        if(idToFileDownloading) {
-            downloadUploadedEMETLog(idToFileDownloading)
-                .then(response => {
-                    const name = data.sourceFileName.includes('.xlsx') ? data.sourceFileName.replace('.xlsx', '') : data.sourceFileName;
-                    downloadFile(response, `${name}_Report_`, '.xlsx', false);
-                })
-                .catch(err => console.error(err));
-        }
+    const successToast = {
+        title: 'Success',
+        icon: SUCCESS_ICON,
+        isAutoDismiss: true,
+        description: 'File downloading started',
     };
+
+    const handleClick = useCallback(
+        debounce(() => {
+            if(idToFileDownloading) {
+                downloadUploadedEMETLog(idToFileDownloading)
+                    .then(response => {
+                        const name = data.sourceFileName.includes('.xlsx') ? data.sourceFileName.replace('.xlsx', '') : data.sourceFileName;
+                        downloadFile(response, `${name}_Report_`, '.xlsx', false);
+                        store.dispatch(addToast(successToast));
+                    }) 
+                    .catch(err => console.error(err));
+            }
+        }, 1000),
+        []
+    );
 
     return (!valueToDisplay && value) || (valueToDisplay && value === valueToDisplay) ? (
         <NexusTooltip content={tooltip}>
+            <Toast ref={toast} position="bottom-left" />
             <span 
                 className={`nexus-c-repository-icon ${params.position === 'center' ? 'nexus-c-repository-icon-position-center' : ''}`} 
                 onClick={handleClick}
