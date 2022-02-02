957c52f96c4ccd02a762cc2470d080829b4d95c0
elyashenko <elyashenko@vubiquity.net>
2022-02-02 15:58:02 +0200

    PORT-5077: New Import Assets api connected, old logic updated

--- src/pages/manual-tasks/asset-management/assetManagementReducer.js
+++ src/pages/manual-tasks/asset-management/assetManagementReducer.js
@@ -11,13 +11,20 @@ export const UPLOAD_ARTWORK_REQUEST = 'UPLOAD_ARTWORK_REQUEST';
 export const UPLOAD_ARTWORK_ERROR = 'UPLOAD_ARTWORK_ERROR';
 export const UPLOAD_ARTWORK_SUCCESS = 'UPLOAD_ARTWORK_SUCCESS';
 
+export const UPLOAD_MEDIA_INGEST = 'UPLOAD_MEDIA_INGEST';
+export const REMOVE_MEDIA_INGEST = 'REMOVE_MEDIA_INGEST';
+export const UPLOAD_MEDIA_INGEST_SUCCESS = 'UPLOAD_MEDIA_INGEST_SUCCESS';
+
 export const fetchPosters = createAction(FETCH_POSTERS);
 export const fetchAsset = createAction(FETCH_ASSET);
 export const uploadArtwork = createAction(UPLOAD_ARTWORK);
+export const uploadMediaIngest = createAction(UPLOAD_MEDIA_INGEST);
+export const removeMediaIngest = createAction(REMOVE_MEDIA_INGEST);
 
 const initialState = {
     posterList: [],
     details: undefined,
+    uploadedMediaIngests: [],
 };
 
 const assetManagementReducer = (state = initialState, action) => {
@@ -31,6 +38,16 @@ const assetManagementReducer = (state = initialState, action) => {
             return {
                 ...state,
                 details: action.payload,
+            };
+        case UPLOAD_MEDIA_INGEST_SUCCESS:
+            return {
+                ...state,
+                uploadedMediaIngests: [...state.uploadedMediaIngests, action.payload],
+            };
+        case REMOVE_MEDIA_INGEST:
+            return {
+                ...state,
+                uploadedMediaIngests: [],
             };
         default:
             return state;
