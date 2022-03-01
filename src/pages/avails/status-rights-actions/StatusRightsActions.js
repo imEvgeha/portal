9f36c612d6b08cc0e2b5c9192700f0f88d8537c1
dpjetlovic <dpjetlovic@vubiquity.net>
2022-03-01 12:07:11 +0100

    PORT-5425: Fixed Re-Sync button, small service refactor

--- src/pages/avails/status-rights-actions/StatusRightsActions.js
+++ src/pages/avails/status-rights-actions/StatusRightsActions.js
@@ -1,9 +1,10 @@
-import React, {useState, useRef} from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import PropTypes from 'prop-types';
 import NexusTooltip from '@vubiquity-nexus/portal-ui/lib/elements/nexus-tooltip/NexusTooltip';
 import {addToast} from '@vubiquity-nexus/portal-ui/lib/toast/NexusToastNotificationActions';
 import withToasts from '@vubiquity-nexus/portal-ui/lib/toast/hoc/withToasts';
 import classNames from 'classnames';
+import {isEmpty} from 'lodash';
 import {useDispatch} from 'react-redux';
 import {postReSync} from '../status-log-rights-table/StatusLogService';
 import {RE_SYNC, RE_SYNC_TOOLTIP} from './constants';
@@ -12,7 +13,13 @@ import './StatusRightsActions.scss';
 export const StatusRightsActions = ({statusLogResyncRights}) => {
     const node = useRef();
     const dispatch = useDispatch();
-    const [isReSyncActive, setIsReSyncActive] = useState(true);
+    const [isReSyncActive, setIsReSyncActive] = useState(false);
+
+    useEffect(() => {
+        statusLogResyncRights && !isEmpty(statusLogResyncRights.rights)
+            ? setIsReSyncActive(true)
+            : setIsReSyncActive(false);
+    }, [statusLogResyncRights]);
 
     const successToast = {
         summary: 'Success',
@@ -21,18 +28,19 @@ export const StatusRightsActions = ({statusLogResyncRights}) => {
     };
 
     const reSync = statusLogResyncRights => {
-        setIsReSyncActive(false);
-        postReSync(statusLogResyncRights)
-            .then(res => {
-                setIsReSyncActive(true);
-                dispatch(addToast(successToast));
-            })
-            .catch(setIsReSyncActive(true));
+        postReSync(statusLogResyncRights).then(res => {
+            dispatch(addToast(successToast));
+        });
     };
 
     return (
         <>
-            <div className="nexus-c-status-rights-actions d-flex align-items-center" ref={node}>
+            <div
+                className={`${
+                    !isReSyncActive ? 'nexus-c-status-rights-actions' : 'nexus-c-status-rights-actions--is-active'
+                } d-flex align-items-center`}
+                ref={node}
+            >
                 <div
                     className={classNames('nexus-c-status-rights-actions__menu-item', {
                         'nexus-c-status-rights-actions__menu-item--is-active': isReSyncActive,
