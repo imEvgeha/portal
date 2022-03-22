1a47c25bb65a65aee9434cd15d6f22fa21830837
elyashenko <elyashenko@vubiquity.net>
2022-03-22 10:38:51 +0200

    PORT-5456: Code Refactoring

--- src/pages/legacy/containers/avail/service/ProfileService.js
+++ src/pages/legacy/containers/avail/service/ProfileService.js
@@ -22,13 +22,15 @@ const getSelectValues = (field, alternateSelector, isInitAvailsMappingFlow = fal
         (isEmpty(availsStoredEndpoints) && isEmpty(store.getState().endpointConfigValues?.[key])) ||
         isInitAvailsMappingFlow
     ) {
-        return nexusFetch(url, {isWithErrorHandling: false}).then(response => {
+        return nexusFetch(url, {isWithErrorHandling: false})
+        .then(response => {
             if (isInitAvailsMappingFlow) {
                 store.dispatch(loadSelectLists(alternateSelector, response.data));
             } else {
                 store.dispatch(storeConfigValues({[key]: response.data}));
             }
-        }).catch(err => {
+        })
+        .catch(err => {
             store.dispatch(
                 addToast({
                     detail: `${err.type}, you failed to get the field ${key}.`,
