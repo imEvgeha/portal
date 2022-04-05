bbc18fe496a53a738b856c9d85060e7ef7bd37da
Miroslav.Sabev <miroslav.sabev@amdocs.com>
2022-04-05 12:15:00 +0300

    bugfix/PORT-5521: fixing typo in the comment

--- packages/ui/src/elements/nexus-grid/hoc/withInfiniteScrolling.js
+++ packages/ui/src/elements/nexus-grid/hoc/withInfiniteScrolling.js
@@ -55,7 +55,7 @@ const withInfiniteScrolling =
                 }
             }, [props.shouldGridRefresh, fetchData, gridApi]);
 
-            // Handle the Auto Refresh/Update of the Grid after Right has been ingested into the systrm
+            // Handle the Auto Refresh/Update of the Grid after Right has been ingested into the system
             useEffect(() => {
                 if (isMounted.current && fetchData && gridApi) {
                     updateData(fetchData, gridApi);
