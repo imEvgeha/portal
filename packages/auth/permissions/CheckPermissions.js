a568a2ed349c386b11e7a4ef4efed3272ff8e500
pdemetriou <Vubiquity.765>
2022-04-20 14:15:08 +0300

    feature/VBQT-1639: Roles implementation

--- packages/auth/permissions/CheckPermissions.js
+++ packages/auth/permissions/CheckPermissions.js
@@ -1,23 +1,19 @@
-import {cloneDeep} from 'lodash/lang';
-
 // eslint-disable-next-line no-underscore-dangle
-let _allPermissions = {};
+let _allRoles = [];
 
-const isAllowed = (allPermissions, permissions) => {
-    _allPermissions = cloneDeep(allPermissions);
-
-    return permissions
-        ? typeof permissions === 'string'
-            ? _allPermissions.includes(permissions)
-            : constructPermissions(permissions)
-        : true;
+export const setRoles = roles => {
+    _allRoles = [...roles];
 };
 
-const constructPermissions = permissions => {
-    let iterationPerm = permissions.operation === 'AND';
-    permissions.values.forEach(value => {
-        const tmpCondition = typeof value === 'string' ? _allPermissions.includes(value) : constructPermissions(value);
-        permissions.operation === 'AND' ? (iterationPerm &&= tmpCondition) : (iterationPerm ||= tmpCondition);
+const isAllowed = roles => {
+    return roles ? (typeof roles === 'string' ? _allRoles.includes(roles) : constructRoles(roles)) : true;
+};
+
+const constructRoles = roles => {
+    let iterationPerm = roles.operation === 'AND';
+    roles.values.forEach(value => {
+        const tmpCondition = typeof value === 'string' ? _allRoles.includes(value) : constructRoles(value);
+        roles.operation === 'AND' ? (iterationPerm &&= tmpCondition) : (iterationPerm ||= tmpCondition);
     });
 
     return iterationPerm;
--- packages/ui/src/elements/nexus-navigation/components/NavigationItems.test.js
+++ packages/ui/src/elements/nexus-navigation/components/NavigationItems.test.js
@@ -1,25 +1,8 @@
-import {can} from '@vubiquity-nexus/portal-utils/lib/ability';
 import {navigationPrimaryItems} from './NavigationItems';
-import {EVENT_MANAGEMENT} from '../constants';
-
-jest.mock('@vubiquity-nexus/portal-utils/lib/ability');
-
-const isEventManagement = item => item.id === EVENT_MANAGEMENT;
 
 describe('Nexus Navigation Items', () => {
-    it('contains all nav items, including event management when event_viewer is enabled', () => {
-        can.mockImplementation(() => true);
-
-        const hasEventMgmtBtn = navigationPrimaryItems('test', () => null).filter(isEventManagement).length > 0;
-
-        expect(hasEventMgmtBtn).toBeTruthy();
-    });
-
-    it('contains all nav items, except event management when event_viewer is disabled', () => {
-        can.mockImplementation(() => false);
-
-        const hasEventMgmtBtn = navigationPrimaryItems('test', () => null).filter(isEventManagement).length > 0;
-
-        expect(hasEventMgmtBtn).toBeFalsy();
+    it('should match snapshot', () => {
+        const items = navigationPrimaryItems('item', () => null);
+        expect(items).toHaveLength(6);
     });
 });
