19024c531cdf87d77fb6081c7029edb91c1ea84a
Marios Charalambous <mcharalambous@vubiquity.net>
2022-04-21 11:33:11 +0300

    VBQT-1374: Tooltip message on hover of User Avatar

--- packages/ui/src/elements/nexus-navigation/constants.js
+++ packages/ui/src/elements/nexus-navigation/constants.js
@@ -7,7 +7,7 @@ export const TITLE_METADATA = 'Title Metadata';
 export const METADATA = 'Metadata';
 export const SYNC_LOG = 'Sync-Log';
 export const FEEDBACK_HEADER = 'Share your thoughts';
-export const USER_AVATAR = 'View User,Tenant Selection, Logout';
+export const USER_AVATAR = 'View User, Tenant Selection, Logout';
 
 export const backgroundColor = '#424242';
 export const backArrowColor = '#42526E';
