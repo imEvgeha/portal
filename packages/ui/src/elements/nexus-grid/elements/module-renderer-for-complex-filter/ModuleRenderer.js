ebd02b4588528eabee09cc1d3d846681e5997e9c
Evgeniy Lyashenko <elyashenko@vubiquity.net>
2022-05-04 16:26:51 +0300

    Feature/port 5511 (#2640)
    * PORT-5510: Backbone for AudioLanguage created
    
    * PORT-5511: Backbone for Price filter created
    
    * PORT-5511: AudioType & Price filters logic updated to new components
    
    * PORT-5511: Code optimized
    
    * PORT-5511: Code refactored
    
    * PORT-5511: Code refactored
    
    * PORT-5511: Code optimzied
    
    * PORT-5511: Validation improved, save button created
    
    * PORT-5511: Code optimized

--- packages/ui/src/elements/nexus-grid/elements/module-renderer-for-complex-filter/ModuleRenderer.js
+++ packages/ui/src/elements/nexus-grid/elements/module-renderer-for-complex-filter/ModuleRenderer.js
@@ -1,58 +1,79 @@
-import React, {useCallback} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import {debounce} from 'lodash';
 import {Dropdown} from 'primereact/dropdown';
 import {InputText} from 'primereact/inputtext';
 import {MultiSelect} from 'primereact/multiselect';
-import FieldLabel from '../../../../../lib/elements/nexus-field-label/FieldLabel';
+import ControllerWrapper from '../../../../../lib/elements/nexus-react-hook-form/ControllerWrapper';
 
-const ModuleRenderer = ({item, selectedData, onChangeSelectedData}) => {
-    const value = selectedData?.[item.id] || null;
+const ModuleRenderer = ({item, formDetails}) => {
+    const {errors, control, register} = formDetails;
     const options = item?.options?.[0].items;
     const labelName = item.label.toLowerCase();
-
-    const onChange = e => {
-        onChangeSelectedData({...selectedData, [item.id]: e.value});
-    };
-
-    const debouncedOnChange = useCallback(
-        debounce((newValue, selectedData) => {
-            onChangeSelectedData({...selectedData, [item.id]: newValue.value});
-        }, 1000),
-        []
-    );
 
     const ItemTypesSeparator = () => {
         switch (item.type) {
             case 'multiselect':
                 return (
-                    <MultiSelect
-                        id={item.id}
-                        value={value}
-                        options={options}
-                        onChange={onChange}
-                        placeholder={`Select a ${labelName}`}
-                        appendTo="self"
-                    />
+                    <ControllerWrapper
+                        title={item.label}
+                        inputName={item.id}
+                        errors={errors[item.id]}
+                        required={item.required}
+                        control={control}
+                        register={register}
+                        additionalValidation={{
+                            minLength: {
+                                value: 1,
+                                message: `Min length is 1!`,
+                            },
+                        }}
+                    >
+                        <MultiSelect
+                            id={item.id}
+                            options={options}
+                            placeholder={`Select a ${labelName}`}
+                            appendTo="self"
+                        />
+                    </ControllerWrapper>
                 );
 
             case 'select':
                 return (
-                    <Dropdown
-                        value={value}
-                        options={options}
-                        onChange={onChange}
-                        placeholder={`Select a ${labelName}`}
-                        appendTo="self"
-                    />
+                    <ControllerWrapper
+                        title={item.label}
+                        inputName={item.id}
+                        errors={errors[item.id]}
+                        required={item.required}
+                        control={control}
+                        register={register}
+                    >
+                        <Dropdown
+                            id={item.id}
+                            options={options}
+                            placeholder={`Select a ${labelName}`}
+                            appendTo="self"
+                        />
+                    </ControllerWrapper>
                 );
 
             case 'text':
                 return (
-                    <InputText
-                        onChange={e => debouncedOnChange(e.target, selectedData)}
-                        placeholder={`Enter a ${labelName}`}
-                    />
+                    <ControllerWrapper
+                        title={item.label}
+                        inputName={item.id}
+                        errors={errors[item.id]}
+                        required={item.required}
+                        control={control}
+                        register={register}
+                        additionalValidation={{
+                            pattern: {
+                                value: /^[0-9]+$/,
+                                message: 'Please enter a valid number!',
+                            },
+                        }}
+                    >
+                        <InputText id={item.id} placeholder={`Enter a ${labelName}`} autoFocus />
+                    </ControllerWrapper>
                 );
 
             default:
@@ -62,12 +83,6 @@ const ModuleRenderer = ({item, selectedData, onChangeSelectedData}) => {
 
     return (
         <div className="p-field">
-            <FieldLabel
-                htmlFor={item.id}
-                label={item.label}
-                isRequired={item.required}
-                className="nexus-c-custom-complex-filter__label"
-            />
             <ItemTypesSeparator />
         </div>
     );
@@ -75,12 +90,7 @@ const ModuleRenderer = ({item, selectedData, onChangeSelectedData}) => {
 
 ModuleRenderer.propTypes = {
     item: PropTypes.object.isRequired,
-    selectedData: PropTypes.array,
-    onChangeSelectedData: PropTypes.func.isRequired,
-};
-
-ModuleRenderer.defaultProps = {
-    selectedData: [],
+    formDetails: PropTypes.object.isRequired,
 };
 
 export default ModuleRenderer;
