2dd6a7d8d8736ae972ca9024bc5e5851c9ee2544
Evgeniy Lyashenko <elyashenko@vubiquity.net>
2022-06-06 15:25:46 +0300

    PORT-5761: Headers updated for MAM APIs (#2752)

--- src/pages/manual-tasks/asset-management/assetManagementService.js
+++ src/pages/manual-tasks/asset-management/assetManagementService.js
@@ -1,34 +1,44 @@
-import {keycloak} from "@portal/portal-auth";
+import {keycloak} from '@portal/portal-auth';
 import {getConfig} from '@vubiquity-nexus/portal-utils/lib/config';
 
 export const fetchPosters = url => {
-    const headers = new Headers();
     const {token} = keycloak;
-    headers.append('Accept', 'application/json');
-    headers.append('Authorization', `Bearer ${token}`);
+    const options = {
+        headers: {
+            Accept: 'application/json',
+            Authorization: `Bearer ${token}`,
+        },
+    };
     return fetch(url, {
         method: 'GET',
-        headers,
+        ...options,
     }).then(response => response.json().then(resp => resp));
 };
 
 export const fetchPoster = poster => {
-    const headers = new Headers();
-    headers.append('Authorization', `token ${localStorage.getItem('token')}`);
-    return fetch(poster, {method: 'GET', headers}).then(res => res.blob());
+    const options = {
+        headers: {
+            Authorization: `token ${localStorage.getItem('token')}`,
+        },
+    };
+    return fetch(poster, {method: 'GET', ...options}).then(res => res.blob());
 };
 
 export const loginAssets = () => {
-    const headers = new Headers();
     const url = `${getConfig('gateway.kongUrl')}${getConfig(
         'gateway.service.kongVidispine'
     )}/token?seconds=1800&autoRefresh=true`;
 
-    headers.append('Accept', 'application/json');
-    headers.append('Authorization', `Basic ${btoa('admin:admin')}`);
+    const options = {
+        headers: {
+            Accept: 'application/json',
+            Authorization: `Basic ${btoa('admin:admin')}`,
+        },
+    };
+
     return fetch(url, {
         method: 'GET',
-        headers,
+        ...options,
     }).then(async response => {
         const {token} = await response.json();
         localStorage.setItem('token', token);
