4a1cd44bfb779e0f0ddde2928a5784fdbb46c2e9
Евгений Ляшенко <evgenijlasenko@MacBook-Air-Evgenij.local>
2022-09-07 13:07:59 +0300

    VBQT-5509: Code refactored

--- src/util/http/HttpService.js
+++ src/util/http/HttpService.js
@@ -36,7 +36,7 @@ export default class HttpService extends HttpHeaders {
 
     // constructHeaders() {}
 
-    constructBaseUrl(apiVersion, isItOldApi) {
+    constructBaseUrl(apiVersion, isOldApi) {
         let baseUrl = getConfig('gateway.kongUrl');
         const serviceReference = this.constructor.name;
 
@@ -45,7 +45,7 @@ export default class HttpService extends HttpHeaders {
             case 'TitleService':
             case 'TitleTerittorialService':
             case 'TitleEditorialService':
-                baseUrl = getApiURI('title', isItOldApi ? '' : '/titles', apiVersion === 'v1' ? 1 : 2);
+                baseUrl = getApiURI('title', isOldApi ? '' : '/titles', apiVersion === 'v1' ? 1 : 2);
                 break;
             case 'TitleConfigurationService':
                 baseUrl = getApiURI('title', '', apiVersion === 'v1' ? 1 : 2);
